0fd1690545226a66190ecc45f3adadc7b60069f0
Andrew Kolumbic <andrewkolumbic@Andrews-Mac-mini.local>
2018-04-27 14:11:53 -0700

    made variables for the carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -2,10 +2,12 @@ class Carousel {
 
 }
 
-let carousel = $();
+let carousel = $(".carousel");
+let buttonLeft = $(".left-button");
+let buttonRight = $(".right-button");
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
+    1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
     2. Those buttons are gonna need some click handlers.
     3. Think of how you would animate this compoennt using jQuery. We would like to see as much done as possible using jQuery
     4. Have fun!
