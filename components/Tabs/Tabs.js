c7b6402324435484949e7662491d2057d7f85608
mr-burton <taylormade800@gmail.com>
2018-04-27 08:55:46 -0700

    adding changes to file tabs.js

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -5,26 +5,28 @@ class TabCard {
   }
   selectCard(){
     // show the card
-    this.element.addClass()
+    this.element.addClass('');
   }
   deselectCard(){
     // hide the card
-    this.element;
+    this.element.removeClass('');
   }
 }
 
 class TabLink {
   constructor(element, parent){
     // Use a jQuery selector to assign this.element to the DOM reference
-    this.element;
+    this.element = $element;
     // assign this.parent to the parent parameter
-    this.parent;
+    this.parent = element.parent;
 
     // Note that we are calling getCards() on Tabs (The parent of TabLink) and passing the data attribute: data-tab, no need to update this line of code.
     this.cards = this.parent.getCards(this.element.data('tab'));
 
     // Using jQuery, map over the array of cards.  In your callback function, create new instances of the TabCard class that contain a card reference. TabCard(card) as an example.
-    this.cards;
+    this.cards.map((index, item) => {
+      return new TabCard(card);
+    });
 
     // You will need to create a click handler for the TabLink element that calls selectTab()
     this.element;
@@ -32,16 +34,16 @@ class TabLink {
 
   selectTab(){
     // use this.parent to call the updateActive() method and pass the `this` keyword as a parameter
-    this.parent;
+    this.parent.updateActive();
     // using a jQuery method, add a class to this.element named "active-tab"
-    this.element;
+    this.element.addClass('active-tab');
     // iterate over each card using the .each() method in jQuery. call the selectCard() method in your callback function
     this.cards;
   }
 
   deselectTab(){
     // use a jQuery method to remove the class "active-tab" from this.element
-    this.element;
+    this.element.removeClass('active-tab');
     // iterate over each card using the .each() method in jQuery. call the deselectCard() method in your callback function
     this.cards;
   }
