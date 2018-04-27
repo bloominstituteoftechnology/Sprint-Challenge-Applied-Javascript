feaa7f6b63556c5735d529a5933b46b4a1879d35
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-04-27 15:01:33 -0400

    Completed Carousel component

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,6 +1,7 @@
 class CarouselImage {
-  constructor(element){
+  constructor(element, position){
     this.element = $(element);
+    this.position = position;
   }
   showImage(){
     this.element.show();
@@ -25,7 +26,7 @@ class CarouselLink {
   }
   incrementPosition(){
     let currentPosition = this.parent.getPosition(),
-        lastPosition = this.parent.carouselLength - 1;
+        lastPosition = this.parent.images.length - 1;
 
     if(currentPosition === lastPosition){
       this.parent.updateActiveImage(0);
@@ -36,7 +37,7 @@ class CarouselLink {
   }
   decrementPosition(){
     let currentPosition = this.parent.getPosition(),
-        lastPosition = this.parent.carouselLength - 1;
+        lastPosition = this.parent.images.length - 1;
 
     if(currentPosition === 0){
       this.parent.updateActiveImage(lastPosition);
@@ -50,42 +51,25 @@ class CarouselLink {
 class Carousel {
   constructor(element){
     this.element = $(element);
-
-    this.leftBtn = this.element.find('.left-button');
-    this.leftBtn = new CarouselLink(this.leftBtn, this);
-    this.RightBtn = this.element.find('.right-button');
-    this.RightBtn = new CarouselLink(this.RightBtn, this);
-
+    this.leftBtn = new CarouselLink($('.left-button'), this);
+    this.RightBtn = new CarouselLink($('.right-button'), this);
     this.images = this.element.find('img');
-    this.images = this.images.map((index, image) => new CarouselImage(image));
-    this.carouselLength = this.images.length;
-
-    this.activePosition = 0;
-    this.activeImage = this.images[this.activePosition];
-
+    this.images = this.images.map((index, image) => new CarouselImage(image, index));
+    this.activeImage = this.images[0];
     this.init();
   }
   init(){
     this.activeImage.showImage();
   }
   getPosition(){
-    return this.activePosition;
+    return this.activeImage.position;
   }
   updateActiveImage(position){
     this.activeImage.hideImage();
-    this.activePosition = position;
-    this.activeImage = this.images[this.activePosition];
+    this.activeImage = this.images[position];
     this.activeImage.showImage();
   }
 }
 
-// Grab a reference to the carousel and use it to initlaize a new Carousel component
 let carousel = $('.carousel');
 carousel = new Carousel(carousel);
-
-/* If You've gotten this far, you're on your own! Although we will give you some hints:
-1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
-2. Those buttons are gonna need some click handlers.
-3. Think of how you would animate this compoennt using jQuery. We would like to see as much done as possible using jQuery
-4. Have fun!
-*/
