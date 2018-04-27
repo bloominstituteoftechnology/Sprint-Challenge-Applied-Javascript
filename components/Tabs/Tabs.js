5f17c019b7cfbb758cbbe1d1e1fca3bff48eef47
jesuarva <jean.ariza@jesuarva.com>
2018-04-27 18:18:22 +0200

    END TabCard constructor

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,7 +1,7 @@
 class TabCard {
   constructor(element){
     // Use a jQuery selector to assign this.element to the DOM reference
-    this.element;
+    this.element = $(element);
   }
   selectCard(){
     // show the card
@@ -22,9 +22,10 @@ class TabLink {
 
     // Note that we are calling getCards() on Tabs (The parent of TabLink) and passing the data attribute: data-tab, no need to update this line of code.
     this.cards = this.parent.getCards(this.element.data('tab'));
+    // console.log(this.cards);
 
     // Using jQuery, map over the array of cards.  In your callback function, create new instances of the TabCard class that contain a card reference. TabCard(card) as an example.
-    this.cards;
+    this.cards = this.cards.map( (i, card) => new TabCard(card) );
 
     // You will need to create a click handler for the TabLink element that calls selectTab()
     this.element;
