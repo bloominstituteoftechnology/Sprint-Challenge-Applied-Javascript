9aef1a8bd568bd5fb4c096b077786810d21fc6a2
Steve <shalomlandsman@gmail.com>
2018-04-27 13:39:37 -0400

    tab selection working but not filtering cards

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -24,29 +24,34 @@ class TabLink {
     this.cards = this.parent.getCards(this.element.data('tab'));
 
     // Using jQuery, map over the array of cards.  In your callback function, create new instances of the TabCard class that contain a card reference. TabCard(card) as an example.
-    this.cards.map((index, card)) => {
+    this.cards.map((index, card) => {
       new TabCard($(card));
-    }
+    });
 
     // You will need to create a click handler for the TabLink element that calls selectTab()
     this.element.click(() => {
-      ;
+      this.selectTab();
+    });
   }
 
-  selectTab(){
+  selectTab() {
     // use this.parent to call the updateActive() method and pass the `this` keyword as a parameter
-    this.parent;
+    this.parent.updateActive(this);
     // using a jQuery method, add a class to this.element named "active-tab"
-    this.element;
+    this.element.addClass("active-tab");
     // iterate over each card using the .each() method in jQuery. call the selectCard() method in your callback function
-    this.cards;
+    this.cards.each(() => {
+      selectCard;
+    });
   }
 
   deselectTab(){
     // use a jQuery method to remove the class "active-tab" from this.element
-    this.element;
+    this.element.removeClass("active-tab");
     // iterate over each card using the .each() method in jQuery. call the deselectCard() method in your callback function
-    this.cards;
+    this.cards.each(() => {
+      deselectCard();
+    });
   }
 }
 
@@ -81,11 +86,17 @@ class Tabs {
     // This method is meant to get all the cards from the HTML page.  
     // If the data supplied is 'all' then all of the cards should be returned. 
     // Otherwise, only cards matching the data attribute should be returned. 
+    if (data === 'all') {
+      return this.element.find(`.card`);
+    }
+    else {
+      return this.element.find(`.card[data-tab="${data}"]`);
+    }
   }
 }
 
 // Using jQuery, select the correct tabs component. Then initialize the Tabs class.
-let tabs = $();
-// tabs = new Tabs(tabs)
+let tabs = $('.tabs');
+tabs = new Tabs(tabs);
 
 
