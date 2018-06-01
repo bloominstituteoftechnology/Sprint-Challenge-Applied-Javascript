f7836c1505ad824b25feffcd9dc18abc0c1cdf48
Sukhada Gholba <sukhadagholba@gmail.com>
2018-06-01 08:27:29 -0700

    updated TabCard class to select and deselect display styles

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -69,15 +69,15 @@ class TabLink {
 class TabCard {
   constructor(element){
     // Assign this.element to the passed in element.
-    this.element;
+    this.element = this.element;
   }
   selectCard(){
     // Update the style of this.element to display = null
-    this.element;
+    this.element.style.display = null;
   }
   deselectCard(){
     // Update the style of this.element to display = "none"
-    this.element;
+    this.element.style.display = "none";
   }
 }
 
