22bb192cf9b10db1f8d4e31b8cb2368af3da55de
JJ Ashcraft <jj@jjashcraft.com>
2018-06-01 14:05:18 -0400

    carousel.js in progress

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,12 +1,50 @@
 class Carousel {
+  constructor (carousel) {
+    this.carousel = carousel;
+   this.slides = carousel.getElementsByTagName('img');
+   this.slides = Array.from(this.slides).map(slide => new Slide(slide));
+  this.activeSlide = this.slides[0];
+  this.init();
+  }
+
+
+
+
+init () {
+    this.activeSlide.displaySlide();
+}
+leftButton() {
 
 }
+rightButton() {
 
-let carousel = $();
+}
+    
+}
 
-/* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
-    2. Those buttons are gonna need some click handlers.
-    3. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
-    4. Have fun!
-*/
+
+class Slide {
+     constructor(slide) {
+         this.slide = slide;
+     }
+
+     displaySlide() {
+     this.slide.style.display = 'block';
+     }
+     hideSlide() {
+     this.slide.style.display = 'none';
+     }
+}
+
+let carousel = document.querySelector('.carousel');
+carousel = new Carousel(carousel);
+
+
+// // let carousel = $();
+
+// /* If You've gotten this far, you're on your own! Although we will give you some hints:
+//     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
+//     2. Those buttons are gonna need some click handlers.
+//     3. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
+//     4. Have fun!
+// */
