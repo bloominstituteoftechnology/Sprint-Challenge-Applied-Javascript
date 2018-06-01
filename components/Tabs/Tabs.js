37b167c2f1032fdfd687ea77002becacb90ea257
chainchompa <chainchompa@gmail.com>
2018-06-01 11:33:03 -0400

    added methods to Tabs class

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -13,25 +13,25 @@ class Tabs {
 
   init(){
     // Invoke the selectTab() method on activeTab so we can see the tab when the page loads.
-    this.activeTab;
+    this.activeTab.selectTab();
   }
 
   updateActive(tabElement){
     // Invoke the deselectTab() on activeTab to clear the styling on the tab
-    this.activeTab;
+    this.activeTab.deselectTab();
     // assign activeTab to tabElement to update it's apperance
-    this.activeTab;
+    this.activeTab = tabElement;
   }
 
   getCards(data){
     // Update the logic in the if statment to check if 'all' is equal to the passed in data.
-    if(data) {
+    if(data === this.element.querySelector('.tab[data-tab="all"]')) {
       // Return a reference to all the ".card" classes
-      return;
+      return document.querySelectorAll('.card');
     } else {
       // Return a reference to the data attributes of all the ".card" classes.  Hint: use the passed data value in getCards() to accomplish this.
-      return document.querySelectorAll(`.card`);
-    }
+      return document.querySelector(`.card[data-tab="${data}"]`)
+    } //maybe document should be this.element?
   }
 }
 
