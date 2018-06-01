7ac2d664bf4d6527bcfc8777dad96acaa439edbe
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-06-01 11:07:08 -0700

    playing with carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,25 @@
 class Carousel {
-
+  constructor() {
+    this.slides = document.querySelectorAll('.carousel img');
+    this.slides = Array.from(this.slides);
+    console.log(this.slides[0]);
+    this.slides.forEach(element => this.showImage(element));
+    // this.showImage(this.slides[0]);
+  }
+  showImage(element) {
+    // Update the style of this.element to display = null
+    element.style.display = 'show';
+    console.log(element.style.display);
+  }
+  hideImage() {
+    // Update the style of this.element to display = "none"
+    element.style.display = 'none';
+  }
 }
 
-let carousel = $();
+let carousel = new Carousel();
+document.addEventListener('DOMContentLoaded', () => carousel);
+// $(document).ready(() => carousel);
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
