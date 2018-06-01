bc29e296d68b4709995c36abe4b1a1a3277857df
Wdifulvio523 <w.difulvio523@gmail.com>
2018-06-01 17:33:42 -0400

    Created Carousel constructor class, made click handlers for leftClick and rightClick, created methods for leftClick and rightClick

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,21 +1,48 @@
 class Carousel {
     constructor(element){
         this.element = element;
-        console.log(this.element);  //why wont this work??
         this.left = this.element.querySelector(".left-button");
         this.right = this.element.querySelector(".right-button");
         
-        console.log(this.left);
-        console.log(this.right); //why wont these work??
+        //click handlers for left click and right click
+        this.left.addEventListener("click", () => {this.leftClick()
+        })
+        this.right.addEventListener("click", () => {this.rightClick()
+        })
+
+
+        // Adding images to element -- this is where it seems to break
+
+        this.images = this.element.querySelectorAll("img");
+        this.currentImage = 1;
+        this.init()
     }
 
+
+    init() {
+        this.images[this.currentImage].style.display = "block";
+    }
+
+    // LeftClick Method
+    leftClick() {
+        console.log("click");
+        if (this.currentImage === 0) return;
+        this.images[this.currentImage].style.display = "none"
+        this.currentImage = this.currentImage - 1;
+        this.images[this.currentImage].style.display = "block"
+    }
+    // RightClick Method
+    rightClick() {
+        if (this.currentImage === this.images.length -1) return;
+        this.images[this.currentImage].style.display = "none"
+        this.currentImage = this.currentImage + 1;
+        this.images[this.currentImage].style.display = "block" 
+    }
 }
 
 
-let carousel = $(".carousel");
-
-// carousel = Array.from(carousel).map(carouselRef => new Carousel(carouselRef));
-
+let carousel = document.querySelector(".carousel");
+carousel = new Carousel(carousel);
 
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
