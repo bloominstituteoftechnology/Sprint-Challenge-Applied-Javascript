b4dd0ff2993c7e07e2e9b03b1f226a48ea539628
sibhat <sibhat.temelso@gmail.com>
2018-06-01 11:01:57 -0700

    carousel, started but not finished

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,50 @@
 class Carousel {
+    constructor(element){
+        this.element = element;
+        this.rightBtn = this.element.querySelector('.right-button'); 
+        this.leftBtn = this.element.querySelector('.left-button');
+        this.imgs = this.element.querySelectorAll('img');
+        this.imgs = Array.from(this.imgs).map(img => (new Image(img, this)))
+        this.activeImg = imgs[0];
+        console.log(this.activeImg);
+        this.init();
+
+        this.rightBtn.addEventListner('click', ()=>{
+
+        });
+        this.leftBtn.addEventListner('click', ()=>{
+
+        });
+    }
+
+    init(){
+        this.activeImg.selectImg();
+  
+    }
+    updateActive(tabElement){
+        // Invoke the deselectTab() on activeTab to clear the styling on the tab
+        this.activeImg.deselectImg();
+        // assign activeTab to tabElement to update it's apperance
+        this.activeImg = tabElement;
+    }
 
 }
+class Image{
+    constructor(element, parent){
+        this.element = element;
+    }
+    selectImg(){
+        // Update the style of this.element to display = null
+        this.element.style.display = 'block';
+    }
+    deselectImg(){
+        // Update the style of this.element to display = "none"
+        this.element.style.display = 'none';
+    }
+}
 
-let carousel = $();
+let carousel = document.querySelectorAll('.carousel');
+carousel = Array.from(carousel).map(item => (new Carousel(item)));
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
