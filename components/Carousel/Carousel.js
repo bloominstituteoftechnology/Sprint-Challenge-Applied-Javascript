46b58dc62b7b26c1f961945f44bb64bd950918eb
JacobLayton <jlayjlay3@gmail.com>
2018-06-01 11:04:49 -0700

    ehh the stretch is tough

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,14 @@
 class Carousel {
-
+    constructor(element){
+        this.element = element;
+        this.img = this.element.querySelectorAll("img");
+        // console.log(this.img);
+    }
+    }
 }
 
-let carousel = $();
+let carousel = $(".carousel, left-button, right button");
+slides = 
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
