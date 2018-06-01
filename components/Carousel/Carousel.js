e388dee42348056e74ab7dc1def992421f917e82
Wdifulvio523 <w.difulvio523@gmail.com>
2018-06-01 13:48:03 -0400

    Attempted Stretch goal - Carousel. Failed miserably.

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,22 @@
 class Carousel {
+    constructor(element){
+        this.element = element;
+        console.log(this.element);  //why wont this work??
+        this.left = this.element.querySelector(".left-button");
+        this.right = this.element.querySelector(".right-button");
+        
+        console.log(this.left);
+        console.log(this.right); //why wont these work??
+    }
 
 }
 
-let carousel = $();
+
+let carousel = $(".carousel");
+
+// carousel = Array.from(carousel).map(carouselRef => new Carousel(carouselRef));
+
+
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
