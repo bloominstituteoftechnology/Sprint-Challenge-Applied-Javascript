a270b00ae08fa7207a9409abb3995763b16eda87
Austin Merando <ajmerando1@yahoo.com>
2018-06-01 10:56:15 -0700

    tabs fully functional

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -46,7 +46,7 @@ class TabLink {
     // Nothing to update here, notice we are accessing the parent's method getCards(), nothing to update here
     this.cards = this.parent.getCards(this.element.dataset.tab);
     // Map over the cards array and convert each card reference into a new TabCard object. Pass in the card object to the TabCard class.
-    this.cards = Array.from(this.cards).map(card => {return new TabCard(card)});
+    this.cards = Array.from(this.cards).map(card =>  new TabCard(card));
     // Add a click event that invokes selectTab
     this.element.addEventListener("click", () => {
       this.selectTab();
@@ -77,11 +77,11 @@ class TabCard {
   }
   selectCard(){
     // Update the style of this.element to display = null
-    this.element.display = null;
+    this.element.style.display = null;
   }
   deselectCard(){
     // Update the style of this.element to display = "none"
-    this.element.display = "none";
+    this.element.style.display = "none";
   }
 }
 
