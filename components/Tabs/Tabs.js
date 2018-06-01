0898f1731c2bc97caee3d564c629113b43b29f6b
Sukhada Gholba <sukhadagholba@gmail.com>
2018-06-01 08:16:37 -0700

    created a reference to all the tab  classes and assigned activeTab to the first item in the tabs array

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -2,11 +2,11 @@ class Tabs {
   constructor(element){
     this.element = element;
     //  create a reference to all the ".tab" classes
-    this.tabs;
+    this.tabs = this.element.querySelectorAll(".tab");
     // Notice that we are passing a new tab AND a reference to Tabs by using "this"
     this.tabs = Array.from(this.tabs).map( tab => new TabLink(tab, this) );
     // Assign activeTab to the first item in the tabs array
-    this.activeTab;
+    this.activeTab = this.tabs[0];
     // init is simply calling a custom method named init(), nothing to do here
     this.init();
   }
