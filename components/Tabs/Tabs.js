181afe3e1c62a0c63d2947be91b5ddc87d668aca
Joe Stanfield <jlstan06@hotmail.com>
2018-06-01 12:28:27 -0400

    finally have tabs working but now adding notes to file so I can understand what is going on

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -20,7 +20,7 @@ class Tabs {
     // Invoke the deselectTab() on activeTab to clear the styling on the tab
     this.activeTab.deselectTab();
     // assign activeTab to tabElement to update it's apperance
-    this.activeTab = this.tabElement;
+    this.activeTab = tabElement;
   }
 
   getCards(data){
@@ -47,21 +47,21 @@ class TabLink {
     this.cards = Array.from(this.cards).map(card => new TabCard(card, this));
     // this.tabs = Array.from(this.tabs).map( tab => new TabLink(tab, this) );
     // Add a click event that invokes selectTab
-    this.element.addEventListener('click', selectTab());
+    this.element.addEventListener('click', () => this.selectTab());
   }
 
   selectTab(){
     // Notice we are invoking updateActive on the parent class of TabLink, nothing to update here
     this.parent.updateActive(this);
     // Add a class of ".active-tab" to this.element
-    this.element.add("active-tab");
+    this.element.classList.add("active-tab");
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class, nothing to update here
     this.cards.forEach(card => card.selectCard());
   }
 
   deselectTab(){
     // Remove the class ".active-tab" from this.element
-    this.element;
+    this.element.classList.remove("active-tab");
     // Notice we are looping through the this.cards array and invoking deselectCard() from the TabCard class, nothing to update here
     this.cards.forEach( card => card.deselectCard());
   }
@@ -70,19 +70,19 @@ class TabLink {
 class TabCard {
   constructor(element){
     // Assign this.element to the passed in element.
-    this.element;
+    this.element = element;
   }
   selectCard(){
     // Update the style of this.element to display = null
-    this.element;
+    this.element.style.display = null;
   }
   deselectCard(){
     // Update the style of this.element to display = "none"
-    this.element;
+    this.element.style.display = 'none';
   }
 }
 
 // Create a reference to ".tabs"
-let tabs = document.querySelectorAll('.tabs');
+let tabs = document.querySelectorAll('.tabs');////this is grabbing .tabs from the less file tabs.less/////
 // Map over the array and convert each tab reference into a new Tabs object.  Pass in the tab object to the Tabs class.
-tabs = Array.from(tabs).map(tabs => new Tabs(tabs));
+tabs = Array.from(tabs).map(tabs => new Tabs(tabs));///this is turning a node list into an array list so we have access to more functions/////
