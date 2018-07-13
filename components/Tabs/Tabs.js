f1e6ffc986a2051483787b6159a17b16e513d2e8
Stephanie Banks <thisgirlcancode@gmail.com>
2018-07-13 12:56:28 -0500

    fixed the tab highlight. i had a . before active tab

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -46,21 +46,22 @@ class TabLink {
     // Map over the cards array and convert each card reference into a new TabCard object. Pass in the card object to the TabCard class.
     this.cards = Array.from(this.cards).map(card => new TabCard(card));
     // Add a click event that invokes selectTab
-    this.element.addEventListener('click', () => {this.selectTab()});
+    // this.element.addEventListener('click', () => {this.selectTab()});
+    this.element.addEventListener("click", this.selectTab.bind(this));
   }
 
   selectTab(){
     // Notice we are invoking updateActive on the parent class of TabLink, nothing to update here
     this.parent.updateActive(this);
     // Add a class of ".active-tab" to this.element
-    this.element.classList.add('.active-tab');
+    this.element.classList.add('active-tab');
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class, nothing to update here
     this.cards.forEach(card => card.selectCard());
   }
 
   deselectTab(){
     // Remove the class ".active-tab" from this.element
-    this.element.classList.remove('.active-tab');
+    this.element.classList.remove('active-tab');
     // Notice we are looping through the this.cards array and invoking deselectCard() from the TabCard class, nothing to update here
     this.cards.forEach( card => card.deselectCard());
   }
@@ -84,4 +85,4 @@ class TabCard {
 // Create a reference to ".tabs"
 let tabs = document.querySelectorAll('.tabs');
 // Map over the array and convert each tab reference into a new Tabs object.  Pass in the tab object to the Tabs class.
-tabs = Array.from(tabs).map(tabs => new Tabs(tabs));
+tabs = Array.from(tabs).map(tab => new Tabs(tab));
