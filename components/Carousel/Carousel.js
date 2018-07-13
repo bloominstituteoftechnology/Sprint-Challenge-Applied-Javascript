98e47d2ff0d3b5cf24e10a8d0669ece39ee8d284
William <kwontaewoongah@gmail.com>
2018-07-13 12:38:39 -0700

    finished carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,9 +1,54 @@
 class Carousel {
-
+    constructor(element) {
+        this.element=element;
+        this.images=this.element.querySelectorAll('img');
+        this.images=Array.from(this.images).map(img=>new ImageSlide(img,this));
+        this.leftButton=this.element.querySelector('.left-button').addEventListener('click', this.moveLeft.bind(this));
+        this.rightButton=this.element.querySelector('.right-button').addEventListener('click', this.moveRight.bind(this));
+        this.activeImg=this.images[0].element.dataset.number;
+        this.number=0;
+        this.init();
+    }
+    init() {
+        document.querySelector(`.carousel img[data-number="${this.activeImg}"]`).style.display='block';
+    }
+    
+    getSlides(data) {
+        return document.querySelectorAll(`.carousel img`); 
+    }
+    moveLeft(){
+        if (this.number>0) {
+            this.number-=1;
+        } else {
+            this.number=3;
+        }
+        document.querySelector(`.carousel img[data-number="${this.activeImg}"]`).style.display='none';
+        this.activeImg=this.images[this.number].element.dataset.number;
+        document.querySelector(`.carousel img[data-number="${this.activeImg}"]`).style.display='block';
+    }
+    moveRight(){
+        if (this.number<3) {
+            this.number+=1;
+        } else {
+            this.number=0;
+        }
+        document.querySelector(`.carousel img[data-number="${this.activeImg}"]`).style.display='none';
+        this.activeImg=this.images[this.number].element.dataset.number;
+        document.querySelector(`.carousel img[data-number="${this.activeImg}"]`).style.display='block';
+    }
+}
+class ImageSlide {
+     constructor(element,parent) {
+        this.element=element;
+        this.parent=parent;
+        this.slide= this.parent.getSlides(this.element.dataset.number);
+        this.slide= Array.from(this.slide).map(image=>new Image(image));
+    } 
 }
 
-let carousel = $();
+let carousel = document.querySelectorAll('.carousel');
 
+carousel=Array.from(carousel).map(carousel=>new Carousel(carousel));
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
     2. Those buttons are gonna need some click handlers.
--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -5,7 +5,6 @@ class Tabs {
     this.tabs=this.element.querySelectorAll('.tab');
     // Notice that we are passing a new tab AND a reference to Tabs by using "this"
     this.tabs = Array.from(this.tabs).map( tab => new TabLink(tab, this) );
-    
     // Assign activeTab to the first item in the tabs array
     this.activeTab=this.tabs[0];
     // init is simply calling a custom method named init(), nothing to do here
