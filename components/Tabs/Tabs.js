8400bcf93ebee042546f1e717ea7cac988180fad
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-07-13 14:32:19 -0500

    added this

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -2,7 +2,7 @@ class Tabs {
   constructor(element) {
     this.element = element;
     // create a reference to all the ".tab" classes
-    this.tabs = element.querySelectorAll(".tab");
+    this.tabs = this.element.querySelectorAll(".tab");
     // Notice that we are passing a new tab AND a reference to Tabs by using "this"
     //MAY HAVE TO ADD A RETURN STATEMENT TO LINE BELOW
     this.tabs = Array.from(this.tabs).map(tab =>  new TabLink(tab, this));
