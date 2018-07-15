f726f323bbadb8b7193a2f587332bd608223ebe6
szincone <sawyerzincone@gmail.com>
2018-07-15 17:15:04 -0600

    Last push, cleaned up and got rid of logs

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -7,8 +7,6 @@ class Carousel {
         this.rightCounter = 0;
         this.leftCounter = this.imgsArray.length;
         this.activeImg = this.imgsArray[this.leftCounter];
-        console.log(this.leftCounter)
-        
         this.imgsArray = Array.from(this.imgsArray);
         this.leftButton.addEventListener("click", () => { this.updateImageLeft() });
         this.rightButton.addEventListener("click", () => { this.updateImageRight() });
@@ -16,7 +14,6 @@ class Carousel {
     };
 
     init(){
-        // Invoke the selectTab() method on activeTab so we can see the tab when the page loads.
         this.activeImg = this.imgsArray[0];
         this.activeImg.style.display = "block";
     }
@@ -40,34 +37,10 @@ class Carousel {
         }
 
     }
-
-
-    // updateActive(tabElement){
-    // // Invoke the deselectTab() on activeTab to clear the styling on the tab
-    // this.activeImg.deselectImage();
-    // // assign activeTab to tabElement to update it's apperance
-    // this.activeImg = tabElement;
-    // }
-
-
-    // updateImage(){
-    //     // Notice we are invoking updateActive on the parent class of TabLink, nothing to update here
-    //     this.parent.updateActive(this);
-    //     console.log(this.parent)
-    //     // Add a class of ".active-tab" to this.element
-    //     this.element.classList.toggle("img");
-    //     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class, nothing to update here
-    //     this.cards.forEach(card => card.selectCard());
-    // }
-
-    // deselectImage() {
-    //     this
-    // }
 };
 
 let carousel = document.querySelectorAll(".carousel");
 carousel = Array.from(carousel).map( item => new Carousel(item));
-// console.log(carousel);
 
 
 
