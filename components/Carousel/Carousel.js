29544b892dc466150724c81bc9de42bae40076b4
Danny Vail <danny_vail@outlook.com>
2018-08-03 17:04:28 -0400

    Added slide animation to carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,73 +1,77 @@
 class CarouselImage {
-  constructor(element, position){
+  constructor(element, position) {
     this.element = $(element);
     this.position = position;
   }
-  showImage(){
-    this.element.show();
+  showImage(direction) {
+    this.element.show('slide', { direction: direction });
   }
-  hideImage(){
-    this.element.hide();
+  hideImage(direction) {
+    this.element.hide('slide', { direction: direction });
   }
 }
 
 class CarouselLink {
-  constructor(element, parent){
+  constructor(element, parent) {
     this.element = $(element);
     this.parent = parent;
     this.direction = this.getLinkDirection();
     this.element.click(() => this.getNewPosition());
   }
-  getLinkDirection(){
+  getLinkDirection() {
     return this.element.hasClass('left-button') ? 'dsc' : 'asc';
   }
-  getNewPosition(){
-    (this.direction === 'dsc') ? this.decrementPosition() : this.incrementPosition();
+  getNewPosition() {
+    this.direction === 'dsc'
+      ? this.decrementPosition()
+      : this.incrementPosition();
   }
-  incrementPosition(){
+  incrementPosition() {
     let currentPosition = this.parent.getPosition(),
-        lastPosition = this.parent.images.length - 1;
+      lastPosition = this.parent.images.length - 1;
 
-    if(currentPosition === lastPosition){
-      this.parent.updateActiveImage(0);
+    if (currentPosition === lastPosition) {
+      this.parent.updateActiveImage(0, 'right');
     } else {
       currentPosition++;
-      this.parent.updateActiveImage(currentPosition);
+      this.parent.updateActiveImage(currentPosition, 'right');
     }
   }
-  decrementPosition(){
+  decrementPosition() {
     let currentPosition = this.parent.getPosition(),
-        lastPosition = this.parent.images.length - 1;
+      lastPosition = this.parent.images.length - 1;
 
-    if(currentPosition === 0){
-      this.parent.updateActiveImage(lastPosition);
+    if (currentPosition === 0) {
+      this.parent.updateActiveImage(lastPosition, 'left');
     } else {
       currentPosition--;
-      this.parent.updateActiveImage(currentPosition);
+      this.parent.updateActiveImage(currentPosition, 'left');
     }
   }
 }
 
 class Carousel {
-  constructor(element){
+  constructor(element) {
     this.element = $(element);
     this.leftBtn = new CarouselLink($('.left-button'), this);
     this.RightBtn = new CarouselLink($('.right-button'), this);
     this.images = this.element.find('img');
-    this.images = this.images.map((index, image) => new CarouselImage(image, index));
+    this.images = this.images.map(
+      (index, image) => new CarouselImage(image, index)
+    );
     this.activeImage = this.images[0];
     this.init();
   }
-  init(){
-    this.activeImage.showImage();
+  init() {
+    this.activeImage.element.show();
   }
-  getPosition(){
+  getPosition() {
     return this.activeImage.position;
   }
-  updateActiveImage(position){
-    this.activeImage.hideImage();
+  updateActiveImage(position, direction) {
+    this.activeImage.hideImage(direction === 'left' ? 'left' : 'right');
     this.activeImage = this.images[position];
-    this.activeImage.showImage();
+    this.activeImage.showImage(direction === 'left' ? 'right' : 'left');
   }
 }
 
