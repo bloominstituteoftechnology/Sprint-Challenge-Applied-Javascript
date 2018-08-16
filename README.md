5cba0b1692c4b504e57ca5d260af834466f78ecb
Daniel Frehner <dfrehner1@gmail.com>
2018-08-16 17:47:42 -0600

    Completed Tabs.js instructions

--- README.md
+++ README.md
@@ -17,15 +17,15 @@ We've covered a lot this week. We've learned how to plug Javascript up to our HT
 We are going to create a Lambda Newspaper. The styling is modeled after the [LA Times Website](http://www.latimes.com). Open it up and you will notice all of the HTML and CSS has already been created and imported properly. Our job is going to be to add functionality to the tabs below the 'Lambda Times' logo. These tabs will act as filters for our articles, and when clicked on, should filter out all articles except those with that tag. 
 
 * Look through the HTML code paying particular attention to the Tabs component, and the Cards components. You will notice they share a data attribute. We will be using this data attribute to determine which cards should show when each tab is selected. 
-* Following the instructions in the `Tabs.js` file, complete the `Tabs`, `TabLink`, and `TabCard` class components. It will look and feel very similar to the last project we worked on, but with a twist. Now, instead of one `Item` to display, we will need to display a collection of `Cards`. Think about ways to iterate over an array and manipulate each item.
-* Once you get your `Tabs` component working properly add a couple more articles yourself and check out how it works.
+* Following the instructions in the `Tabs.js` file, complete the `TabLink`, and `TabCard` class components. It will look and feel very similar to the last project we worked on, but with a twist. Now, instead of one `Item` to display, we will need to display a collection of `Cards`. Think about ways to iterate over an array and manipulate each item.
+* Once you get your `Tab` component working properly add a couple more articles yourself and check out how it works.
 
 ![Working Sprint Challenge Gif](./Sprint-Challenge.gif "Example of working project")
 
 
 ## Stretch Challenge
-Your stretch challenge is to write the functionality of a `Carousel` component. You have the HTML and CSS already in place, simply uncomment the HTML in the `index.html` file. This is an advanced challenge, so you are not expected to be able to complete it. If you begin and don't finish, you should still submit with what you have. You may reference the `Tabs` component for assistance.
+Your stretch challenge is to write the functionality of a `Carousel` component. You have the HTML and CSS already in place, simply uncomment the HTML in the `index.html` file. This is an advanced challenge, so you are not expected to be able to complete it. If you begin and don't finish, you should still submit with what you have. You may reference the `Tabs.js` file for assistance.
 * Complete the carousel functionality in `Carousel.js`
 * If you complete the Carousel, add functionality so that the carousel slides when the buttons are pressed instead of just appearing.
-* Create an 'infinte loop' carousel. In which as long as you click on an arrow, the array of images will loop over iteself.
+* Create an 'infinite loop' carousel. In which as long as you click on an arrow, the array of images will loop over iteself.
 * If you have finished the above, play around with the styling on all the components, and understand how each is built.
--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,68 +1,33 @@
-class Tabs {
-  constructor(element){
-    this.element = element;
-    // create a reference to all the ".tab" classes
-    this.tabs;
-    // Notice that we are passing a new tab AND a reference to Tabs by using "this"
-    this.tabs = Array.from(this.tabs).map( tab => new TabLink(tab, this) );
-    // Assign activeTab to the first item in the tabs array
-    this.activeTab;
-    // init is simply calling a custom method named init(), nothing to do here
-    this.init();
-  }
-
-  init(){
-    // Invoke the selectTab() method on activeTab so we can see the tab when the page loads.
-    this.activeTab;
-  }
-
-  updateActive(tabElement){
-    // Invoke the deselectTab() on activeTab to clear the styling on the tab
-    this.activeTab;
-    // assign activeTab to tabElement to update it's apperance
-    this.activeTab;
-  }
-
-  getCards(data){
-    // Update the logic in the if statment to check if 'all' is equal to the passed in data.
-    if(data) {
-      // Return a reference to all the ".card" classes
-      return;
-    } else {
-      // Return a reference to the data attributes of all the ".card" classes.  Hint: use the passed data value in getCards() to accomplish this.
-      return document.querySelectorAll(`.card`);
-    }
-  }
-}
-
 class TabLink {
-  constructor(element, parent){
+  constructor(element){
     // assign this.element to the element reference
     this.element;
-    // assign this.parent to the parent reference
-    this.parent;
-    // Nothing to update here, notice we are accessing the parent's method getCards(), nothing to update here
-    this.cards = this.parent.getCards(this.element.dataset.tab);
-    // Map over the cards array and convert each card reference into a new TabCard object. Pass in the card object to the TabCard class.
+    // Get the tab data attribute and save the value here
+    this.tabData = ; 
+    // Find all element with the .card class in the app that correspond to the tab data attribute
+    // If the data is 'all' then select all cards regardless of their data attribute
+    if(){
+      this.cards = ;
+    } else {
+      this.cards = ;
+    }
+
+    // Map over the cards array and convert each card element into a new instance of the TabCard class. 
+    // Pass in the card object to the TabCard class.
     this.cards = Array.from(this.cards).map();
     // Add a click event that invokes selectTab
     this.element.addEventListener();
   }
 
   selectTab(){
-    // Notice we are invoking updateActive on the parent class of TabLink, nothing to update here
-    this.parent.updateActive(this);
+    // Select all elements with the .tab class on them
+    const tabs = document.querySelector();
+    // Iterate through the NodeList removing the .active-tab class from each element
+    tabs.forEach()
     // Add a class of ".active-tab" to this.element
     this.element;
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class, nothing to update here
     this.cards.forEach(card => card.selectCard());
-  }
-
-  deselectTab(){
-    // Remove the class ".active-tab" from this.element
-    this.element;
-    // Notice we are looping through the this.cards array and invoking deselectCard() from the TabCard class, nothing to update here
-    this.cards.forEach( card => card.deselectCard());
   }
 }
 
@@ -72,16 +37,19 @@ class TabCard {
     this.element;
   }
   selectCard(){
+    // Select all of the elements with the .card class on them
+    const cards = ;
+    // Iterate through the NodeList setting the display style each one to 'none'
+
     // Update the style of this.element to display = null
     this.element;
   }
-  deselectCard(){
-    // Update the style of this.element to display = "none"
-    this.element;
-  }
+
 }
 
-// Create a reference to ".tabs"
+// Create a reference to all ".tab" classes
 let tabs = document.querySelectorAll();
-// Map over the array and convert each tab reference into a new Tabs object.  Pass in the tab object to the Tabs class.
+// Map over the array and convert each tab reference into a new TabLink object.  Pass in the tab object to the Tabs class.
 tabs = Array.from(tabs).map()
+
+//Once you are complete, call the .select method on the first tab
