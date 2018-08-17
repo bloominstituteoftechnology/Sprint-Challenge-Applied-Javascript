7d3689444a35d63d6883bc66fbc85bcdbc83aa9f
Clint Kunz <clintkunz@gmail.com>
2018-08-17 14:14:34 -0600

    changed display to flex

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -49,7 +49,7 @@ class TabCard {
   }
   selectCard(){
     // Update the style of this.element to display = null
-    this.element.style.display = null;
+    this.element.style.display = "flex";
   }
 
 }
