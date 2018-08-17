11eabb574b5408ed64b8526f0fb8ee0abb902286
James Cassidy <cassidyjamesw@gmail.com>
2018-08-17 11:21:12 -0400

    test

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,13 +1,13 @@
 class TabLink {
   constructor(element){
     // assign this.element to the element reference
-    this.element;
+    this.element = element;
     // Get the tab data attribute and save the value here
-    this.tabData = ; 
+    this.tabData = this.element.dataset.tab;
     // Find all elements with the .card class in index.html that correspond to the tab data attribute
     // If the data is 'all' then select all cards regardless of their data attribute
-    if(){
-      this.cards = ;
+    if(this.tabData === 'all'){
+      this.cards = document.querySelectorAll('.card');
     } else {
       this.cards = ;
     }
