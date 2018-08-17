b4c778de10f0882ab5f9da94dabfda6e1a2891e0
Joseph Chretien-Golden <joechre@gmail.com>
2018-08-17 13:12:33 -0500

    lunch

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,9 +1,9 @@
 class TabLink {
   constructor(element){
-    // assign this.element to the element reference
-    this.element;
+    
+    this.element = element;
     // Get the tab data attribute and save the value here
-    this.tabData = ; 
+    this.tabData = this.element.dataset.tab; 
     // Find all elements with the .card class in index.html that correspond to the tab data attribute
     // If the data is 'all' then select all cards regardless of their data attribute
     if(){
@@ -13,20 +13,23 @@ class TabLink {
     }
 
     // Map over the cards array and convert each card element into a new instance of the TabCard class. 
-    // Pass in the card object to the TabCard class.
-    this.cards = Array.from(this.cards).map();
+    // Pass in the card object to the TabCard class. check Tabcard!!!
+    this.cards = Array.from(this.cards).map(card => new TabCard(card));
     // Add a click event that invokes selectTab
-    this.element.addEventListener();
+    this.element.addEventListener('click', this.selectTab);
   }
 
   selectTab(){
 
     // Select all elements with the .tab class on them
-    const tabs = document.querySelectorAll();
+    const tabs = document.querySelectorAll('.tab');
     // Iterate through the NodeList removing the .active-tab class from each element
-    tabs.forEach()
+    tabs.forEach( tab => {
+      tab.classList.remove('.active-tab')
+    });
+   
     // Add a class of ".active-tab" to this.element
-    this.element;
+    this.element.classList.add(".active-tab");
 
 
     // Select all of the elements with the .card class on them
@@ -41,7 +44,7 @@ class TabLink {
 class TabCard {
   constructor(element){
     // Assign this.element to the passed in element.
-    this.element;
+    this.element = element;
   }
   selectCard(){
     // Update the style of this.element to display = null
