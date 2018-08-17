b3643f1ebe95c0ee25faaf5f4fda59370d54d566
lndubose <lndubose13@gmail.com>
2018-08-17 13:00:12 -0500

    Worked on stretch

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,34 @@
 class Carousel {
+    constructor(element) {
+        //Buttons
+        this.element = element;
+        
+
+        this.leftButton = this.element.querySelector(".left-button");
+        this.rightButton = this.element.querySelector(".right-button");
+        
+        //Images
+        this.images = this.element.querySelectorAll(".carousel img");    
+
+        this.leftButton.addEventListener("click", () => { this.buttonClick() });
+
+        this.rightButton.addEventListener("click", () => { this.buttonClick() });
+    }
+
+    buttonClick() {
+        this.images.forEach(image => {
+            image.classList.toggle("display");
+        })
+        console.log(this.images);
+        //this.images.classList.add("display");      
+    }
 
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelectorAll(".carousel");
+
+carousel = Array.from(carousel).map(button => new Carousel(button))
+
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -2,8 +2,10 @@ class TabLink {
   constructor(element){
     // assign this.element to the element reference
     this.element = element;
+
     // Get the tab data attribute and save the value here
     this.tabData = this.element.dataset.tab; 
+
     // Find all elements with the .card class in index.html that correspond to the tab data attribute
     // If the data is 'all' then select all cards regardless of their data attribute
     if(this.tabData === "all"){
