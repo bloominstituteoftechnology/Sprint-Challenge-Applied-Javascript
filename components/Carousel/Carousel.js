b4826f26d26c9232c8c7997f4bbdbe69422a27b2
A2i <abdulfatah.a.ahmad@gmx.com>
2018-08-17 13:23:59 -0400

    DOM Sprint Challenge A Ahmad

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,12 @@
 class Carousel {
-
+    constructor(element) {
+        this.element = element;
+        this.images = this.element.querySelectorAll('img');
+        
+}
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector(".carousel");
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
