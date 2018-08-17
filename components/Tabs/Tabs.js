49bf2cf9bc0c8909a19312ecfa81b178d5135e59
mparedes003 <michelle.g.paredes@gmail.com>
2018-08-17 08:45:23 -0700

    Created Reference to .tab classes. Mapped over array and converted each tab reference in a new Tablink object. Assigned this. element to element reference. got data attribute and saved the value in this.tabData.

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,16 +1,16 @@
 class TabLink {
-  constructor(element){
+  constructor(tabLinks){
     // assign this.element to the element reference
-    this.element;
+    this.element = tabLinks;
     // Get the tab data attribute and save the value here
-    this.tabData = ; 
+    this.tabData = this.element.dataset.tab; 
     // Find all elements with the .card class in index.html that correspond to the tab data attribute
     // If the data is 'all' then select all cards regardless of their data attribute
-    if(){
-      this.cards = ;
-    } else {
-      this.cards = ;
-    }
+    // if(){
+    //   this.cards = ;
+    // } else {
+    //   this.cards = ;
+    // }
 
     // Map over the cards array and convert each card element into a new instance of the TabCard class. 
     // Pass in the card object to the TabCard class.
@@ -30,7 +30,7 @@ class TabLink {
 
 
     // Select all of the elements with the .card class on them
-    const cards = ;
+    // const cards = ;
     // Iterate through the NodeList setting the display style each one to 'none'
     cards.forEach()
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class, nothing to update here
@@ -51,8 +51,8 @@ class TabCard {
 }
 
 // Create a reference to all ".tab" classes
-let tabs = document.querySelectorAll();
+let tabs = document.querySelectorAll('.tab');
 // Map over the array and convert each tab reference into a new TabLink object.  Pass in the tab object to the Tabs class.
-tabs = Array.from(tabs).map()
+tabs = Array.from(tabs).map(tabLinks => new TabLink(tabLinks));
 
 //Once you are complete, call the .select method on the first tab
