25449fd274746d52f52f1862f2cb2bffdf82287f
Daniel Frehner <dfrehner1@gmail.com>
2018-08-16 18:47:26 -0600

    Finished Tabs.js instructions

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -6,7 +6,9 @@ let carousel = document.querySelector();
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
-    2. Those buttons are gonna need some click handlers.
-    3. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
-    4. Have fun!
+    2. You will need to grab a reference to all of the images
+    3. Create a current index
+    4. Those buttons are gonna need some click handlers.
+    5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
+    6. Have fun!
 */
