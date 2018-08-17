083985d89fe3460c5cd75390f4461f22fbf2dc44
Tristen Neu <tristenneu@Tristens-MacBook-Pro.local>
2018-08-17 12:53:52 -0500

    Sprint Challenge Complete

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -5,7 +5,6 @@ class TabLink {
     // Get the tab data attribute and save the value here
     this.tabData = this.element.dataset.tab;
     // Find all elements with the .card class in index.html that correspond to the tab data attribute
-    this.cards = document.querySelectorAll(`.card[data-tab="${this.tabData}"]`);
     // If the data is 'all' then select all cards regardless of their data attribute
     if (this.tabData === "all") {
       this.cards = document.querySelectorAll(".card");
@@ -19,16 +18,18 @@ class TabLink {
     // Pass in the card object to the TabCard class.
     this.cards = Array.from(this.cards).map(card => new TabCard(card));
     // Add a click event that invokes selectTab
-    this.element.addEventListener("click", () => this.selectTab());
+    this.element.addEventListener("click", () => {
+      this.selectTab();
+    });
   }
 
   selectTab() {
     // Select all elements with the .tab class on them
     const tabs = document.querySelectorAll(".tab");
     // Iterate through the NodeList removing the .active-tab class from each element
-    tabs.forEach(tab => tab.classList.remove(".active-tab"));
+    tabs.forEach(tab => tab.classList.remove("active-tab"));
     // Add a class of ".active-tab" to this.element
-    this.element.classList.add(".active-tab");
+    this.element.classList.add("active-tab");
 
     // Select all of the elements with the .card class on them
     const cards = document.querySelectorAll(".card");
@@ -46,7 +47,7 @@ class TabCard {
   }
   selectCard() {
     // Update the style of this.element to display = null
-    this.element.style.display = "null";
+    this.element.style.display = null;
   }
 }
 
@@ -56,4 +57,4 @@ let tabs = document.querySelectorAll(".tab");
 tabs = Array.from(tabs).map(tab => new TabLink(tab));
 
 //Once you are complete, call the .select method on the first tab
-tab[0].selectTab();
+tabs[0].selectTab();
