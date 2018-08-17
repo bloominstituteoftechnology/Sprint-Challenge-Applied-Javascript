219be8e5ee471baa5b0c6a676c7bf80ec201c27d
easyra <ezra.rr.davis@gmail.com>
2018-08-17 09:27:19 -0700

    'upate

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,20 @@
+
+
 class Carousel {
+    constructor(element) {
+        this.element = element
+        this.leftBtn = element.querySelector('.left-button')
+        console.log(this.leftBtn);
+    }
+
 
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector(".carousel");
+
+carousel = new Carousel(carousel)
+
+
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
