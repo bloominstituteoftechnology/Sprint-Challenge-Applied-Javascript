706e7b21ca890a8f7bf875adc605a2062b1936bc
Anthony Greb <agreb255@gmail.com>
2018-08-17 16:12:41 -0400

    making some changes

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -3,9 +3,15 @@ class TabLink {
     // assign this.element to the element reference
     this.element = element;
     // Get the tab data attribute and save the value here
-    this.tabData = this.element.dataset.tab; 
+    this.data = this.element.dataset.tab; 
     // Find all elements with the .card class in index.html that correspond to the tab data attribute
     // If the data is 'all' then select all cards regardless of their data attribute
+    if(this.data === "all") {
+      this.cards = document.querySelectorAll(".card");
+    } else {
+      this.cards = this.element.querySelectorAll(`.card[data-tab = "${this.data}"]`);
+    }
+    
     this.cards = document.querySelectorAll(".card");
 
     
@@ -15,13 +21,7 @@ class TabLink {
   }
 
   selectTab(){
-    let data = event.currentTarget.cards.TabLink;
-    if(data === "all") {
-      this.cards = document.querySelectorAll(".card");
-    } else {
-      this.cards = this.element.querySelectorAll(`.card[data-tab = "${data}"]`);
-    }
-
+    
     // Map over the cards array and convert each card element into a new instance of the TabCard class. 
     // Pass in the card object to the TabCard class.
     this.cards = Array.from(this.cards).map(card => new TabCard(card));
@@ -31,9 +31,6 @@ class TabLink {
     tabs.forEach(card => card.remove(".active-tab"));
     // Add a class of ".active-tab" to this.element
     this.element.classList.add("active-tab");
-
-    //  Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    // Array.from(links).forEach( item => item.classList.remove('tabs-link-selected'));
 
     // Select all of the elements with the .card class on them
     const cards = document.querySelectorAll(".card");
@@ -47,11 +44,11 @@ class TabLink {
 class TabCard {
   constructor(element){
     // Assign this.element to the passed in element.
-    this.element = tabs;
-  }
+    this.element = element;
+  }                 
   selectCard(){
     // Update the style of this.element to display = null
-    this.element.display = null;
+    this.element.style.display = null;
   }
 
 }
@@ -62,4 +59,4 @@ let tabs = document.querySelectorAll(".tab");
 tabs = Array.from(tabs).map(tab => new TabLink(tab));
 
 //Once you are complete, call the .select method on the first tab
-cards[0].selectCard();
+tabs[0].selectCard();
