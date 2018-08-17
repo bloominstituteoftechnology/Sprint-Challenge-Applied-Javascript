9549f1d984a3de2df5079d35191d80dd7647c32c
Lucas Beemer <lucasbeemer@Lukes-MBP.hsd1.ca.comcast.net>
2018-08-17 08:39:23 -0700

    answers completed and some edits to tabs js file

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,9 +1,9 @@
 class TabLink {
-  constructor(element){
+  constructor(tab){
     // assign this.element to the element reference
-    this.element;
+    this.element = tab;
     // Get the tab data attribute and save the value here
-    this.tabData = ; 
+    this.tabData = = this.element.dataset.tab;
     // Find all elements with the .card class in index.html that correspond to the tab data attribute
     // If the data is 'all' then select all cards regardless of their data attribute
     if(){
@@ -24,13 +24,13 @@ class TabLink {
     // Select all elements with the .tab class on them
     const tabs = document.querySelectorAll();
     // Iterate through the NodeList removing the .active-tab class from each element
-    tabs.forEach()
+    tabs.forEach(tab => tab.classList.remove('active-tab'));
     // Add a class of ".active-tab" to this.element
-    this.element;
+    this.element.classList.add('active-tab');
 
 
     // Select all of the elements with the .card class on them
-    const cards = ;
+    const cards = document.querySelectorAll('.card');
     // Iterate through the NodeList setting the display style each one to 'none'
     cards.forEach()
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class, nothing to update here
@@ -51,8 +51,20 @@ class TabCard {
 }
 
 // Create a reference to all ".tab" classes
-let tabs = document.querySelectorAll();
+let tabs = document.querySelectorAll('.tab');
 // Map over the array and convert each tab reference into a new TabLink object.  Pass in the tab object to the Tabs class.
-tabs = Array.from(tabs).map()
+tabs = Array.from(tabs).map( tab => new TabLink(tab));
 
 //Once you are complete, call the .select method on the first tab
+
+  <div class="tabs">
+    <div class="topics">
+      <span class="title">TRENDING TOPICS:</span>
+      <div data-tab="all" class="tab active-tab">ALL</div>
+      <div data-tab="javascript" class="tab">JAVASCRIPT</div>
+      <div data-tab="technology" class="tab">TECHNOLOGY</div>
+      <div data-tab="node" class="tab">NODE.JS</div>
+      <div data-tab="jquery" class="tab">jQUERY</div>
+      <div data-tab="bootstrap" class="tab">BOOTSTRAP</div>
+    </div>
+  </div>
