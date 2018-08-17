bda0d0c976a5345b33151ba306a8e0059cacfbfc
Jeremy Boggs <nicocchilover@gmail.com>
2018-08-17 12:41:21 -0400

    fixed the z-index of the image carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,70 @@
 class Carousel {
+    constructor(element) {
+        this.element = element;
 
+        this.leftBtn = document.querySelector('.left-button');
+        this.rightBtn = document.querySelector('.right-button');
+
+        this.images = document.querySelectorAll('.carousel img');
+        this.currentIndex = 0;
+        this.imgCount = this.images.length;
+
+        this.leftBtn.addEventListener('click', () => {this.selectLeft();});
+        this.rightBtn.addEventListener('click', () => {this.selectRight();});
+
+        this.images[0].style.display = 'block';
+
+        images.forEach( elem => {
+        })
+    }
+
+    selectLeft() {
+        // Decrease current index
+        this.currentIndex--;
+
+        // Check if at the end of the images and then loop to beginning
+        if (this.currentIndex <= 0 - 1) {
+            // Reset currentIndex to the last image
+            this.currentIndex = 3;
+        }
+
+        // Loop though the images and set the image index to the current index
+        for (let i = 0; i < this.imgCount; i++) {
+            if (this.currentIndex === i) {
+                this.images[i].style.display = 'block';
+            }
+            else {
+                this.images[i].style.display = 'none';
+            }
+        }
+    }
+
+    selectRight() {
+        // Increase current index
+        this.currentIndex++;
+
+        // Check if at the end of the images and then loop to beginning
+        if (this.currentIndex > this.imgCount - 1) {
+            // Reset currentIndex to the first image
+            this.currentIndex = 0;
+            
+        }
+
+        // Loop though the images and set the image index to the current index
+        for (let i = 0; i < this.imgCount; i++) {
+
+            if (this.currentIndex === i) {
+                this.images[i].style.display = 'block';
+            }
+            else {
+                this.images[i].style.display = 'none';
+            }
+        }
+    }
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector('.carousel');
+carousel = new Carousel(carousel);
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
