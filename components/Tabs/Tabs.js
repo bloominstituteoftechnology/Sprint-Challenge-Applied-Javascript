40ffa5054faa63fa399740cf46dd72adc6d0836e
Max <maxkajiwara@gmail.com>
2018-08-17 10:13:27 -0700

    I'm an idiot. null isn't a string.

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -45,7 +45,7 @@ class TabCard {
 
   selectCard(){
     // Update the style of this.element to display = null
-    this.element.style.display = 'block';
+    this.element.style.display = null;
   }
 
 }
