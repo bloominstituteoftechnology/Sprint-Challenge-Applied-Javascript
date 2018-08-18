95f13440f9a221a0c3f8ddd152a61bc6896af5fd
Max <maxkajiwara@gmail.com>
2018-08-18 15:22:40 -0700

    Carousel working as intended. Minor stack order failure at loop point if you spam the buttons.

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -2,7 +2,9 @@ class Carousel {
     constructor(element) {
         this.element = element;
         this.leftButton = element.querySelector('.left-button');
+        this.leftButton.style.zIndex = 2;
         this.rightButton = element.querySelector('.right-button');
+        this.rightButton.style.zIndex = 2;
         this.images = element.querySelectorAll('img');
         this.index = 0;
         this.leftButton.addEventListener('click',
@@ -13,21 +15,22 @@ class Carousel {
 
     left() {
         const previousImage = this.images[this.index];
+        previousImage.style.zIndex = 1;
         this.index = (this.index === 0) ?
             this.images.length -1 : this.index - 1;
         this.images[this.index].style.display = 'block';
-        TweenMax.from(this.images[this.index], .5, {x: -1200});
-        TweenMax.from(previousImage, .5, {x: -1200});
-        previousImage.style.display = 'none';
+        TweenMax.to(previousImage, .5, {x: 1200});
+        setTimeout(() => (previousImage.style.zIndex = 0, previousImage.style.display = 'none', TweenMax.set(previousImage, {x: 0})), 500);
     };
     right() {
         const previousImage  = this.images[this.index];
+        previousImage.style.zIndex = 0;
         this.index = (this.index === this.images.length -1) ?
             0 : this.index + 1;
+        this.images[this.index].style.zIndex = 1;
         this.images[this.index].style.display = 'block';
         TweenMax.from(this.images[this.index], .5, {x: 1200});
-        TweenMax.from(previousImage, .5, {x: 1200});
-        previousImage.style.display = 'none';
+        setTimeout(() => previousImage.style.display = 'none', 500);
     };
 }
 
