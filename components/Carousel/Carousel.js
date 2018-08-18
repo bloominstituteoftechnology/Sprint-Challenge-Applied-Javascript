c32494599e230d97a08e9e0c492562af784a688f
Michael Agard <mdagard@gmail.com>
2018-08-17 22:14:15 -0400

    Cleaned up the code and documented every line to learn the logic

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,44 +1,47 @@
+// Class for Carousel to house the mainCarousel
 class Carousel {
+    // Defined the constructor as carouselComponents for creating and initializing the object with a class 
     constructor(carouselComponents) {
+        // Defined the constructor to reference as this.carouselComponents
         this.carouselComponents = carouselComponents;
-        this.carouselLeftButton = this.carouselComponents.querySelector('.left-button')
-        this.carouselRightButton = this.carouselComponents.querySelector('.right-button')
-        this.carouselImages = this.carouselComponents.querySelectorAll('img')
-        // this.carouselImages[i].style.display = 'flex';
+        // Defined the .left-button CSS class as this.carouselLeftButton
+        this.carouselLeftButton = this.carouselComponents.querySelector('.left-button');``
+        // Defined the .right-button CSS class as this.carouselRightButton
+        this.carouselRightButton = this.carouselComponents.querySelector('.right-button');
+        // Defined the HTML img element within this.carouselComponents as this.carouselImages
+        this.carouselImages = this.carouselComponents.querySelectorAll('img');
+        // Added an event listener on 'click' for this.carouselRightButton to invoke this.nextImage()
         this.carouselRightButton.addEventListener('click', () => this.nextImage());
+        // Added an event listener on 'click' for this.carouselLeftButton to invoke this.nextImage()
         this.carouselLeftButton.addEventListener('click', () => this.lastImage());
-        // this.carouselImages = this.carouselComponents.querySelectorAll('img')
     }
+    // This method is created to advance through the images
     nextImage(){
-        i++;
-        if (i > 3) {i = 0};
-        Array.from(this.carouselImages).forEach(image => image.style.display = 'none')
-        this.carouselImages[i].style.display = 'flex'
-        console.log(`displaying image ${i}`)
+        // Ternery operation that checks if the iterator 'i' evaluates to 3. If it is then 'i' evaluates to 0. If not, add 1 to 'i' 
+        (i === 3) ? i = 0 : i++;
+        // Turns this.carouselImages to an array and sets the display of the image's CSS display value to 'none'
+        Array.from(this.carouselImages).forEach(image => image.style.display = 'none');
+        // Assigns value of 'flex' to the iterated this.carouselImages variable
+        this.carouselImages[i].style.display = 'flex';
+        // Debug Code to see which index of the image is being displayed
+        // console.log(`displaying image ${i}`);
     }
     lastImage(){
-        // debugger;
-        // i--;
+        // Ternery operation that checks if the iterator 'i' evaluates to 0. If it is then 'i' evaluates to 3. If not, subtract 1 to 'i'
         (i === 0) ? i = 3 : i--;
-        Array.from(this.carouselImages).forEach(image => image.style.display = 'none')
-        this.carouselImages[i].style.display = 'flex'
-        console.log(`displaying image ${i}`)
+        // Turns this.carouselImages to an array and sets the display of the image's CSS display value to 'none'
+        Array.from(this.carouselImages).forEach(image => image.style.display = 'none');
+        // Assigns value of 'flex' to the iterated this.carouselImages variable
+        this.carouselImages[i].style.display = 'flex';
+        // Debug Code to see which index of the image is being displayed
+        // console.log(`displaying image ${i}`);
     }
 }
+// Assigns 'i' to evaluate to 0, the beginning of the image carousel
 let i = 0;
+// Assigns mainCarousel to the first CSS class that is 'carousel'
 let mainCarousel = document.querySelector('.carousel');
+// Feeds mainCarousel to the class constructor 'Carousel'
 mainCarousel = new Carousel(mainCarousel);
-// mainCarousel = Array.from(mainCarousel).map( carouselComponents => new Carousel(carouselComponents))
+// Sets the CSS's 'display' style attribute on the 0 index number to 'flex'
 mainCarousel.carouselImages[i].style.display = 'flex';
-
-
-// buttons = Array.from(buttons).map( carouselButtons)
-
-/* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
-    2. You will need to grab a reference to all of the images
-    3. Create a current index
-    4. Those buttons are gonna need some click handlers.
-    5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
-    6. Have fun!
-*/
