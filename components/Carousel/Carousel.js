57763a283e32ec987a5a9abd3b27ad91f30398f4
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-09-05 16:07:06 +0800

    Did the following:
    1. Did the Stretch Challenge for this repo, which is to provide functionality of the Carousel component.
    2. Uncommented the lines of code in the index.html file that will enable the script for the Carousel as well as its component.
    3. Implemented Carousel.js.

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,41 @@
 class Carousel {
+    constructor(element) {
+        this.element = $(element);
 
+        this.leftButton = element.find(".left-button");
+        this.leftButton.click(() => {this.back()});
+
+        this.rightButton = element.find(".right-button");
+        this.rightButton.click(() => {this.forward()});
+
+        this.images = element.find("img");
+        this.currentImage = $(this.images[0]);
+        this.index = 0;
+
+        this.init();
+    }
+
+    init() {
+        this.currentImage.show();
+    }
+
+    back() {
+        this.index = this.index === 0 ? this.images.length-1 : this.index-1;
+        this.currentImage.hide();
+        this.currentImage = $(this.images[this.index]);
+        this.currentImage.fadeIn();
+    }
+
+    forward() {
+        this.index = this.index === this.images.length-1 ? 0 : this.index+1;
+        this.currentImage.hide();
+        this.currentImage = $(this.images[this.index]);
+        this.currentImage.fadeIn();
+    }
 }
 
-let carousel = $();
+let carousel = $(".carousel");
+carousel = new Carousel(carousel);
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
