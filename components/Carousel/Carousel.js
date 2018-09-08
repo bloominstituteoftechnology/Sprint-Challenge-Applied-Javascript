36b7f157f07601206ae4d0d641878539b70da0df
TexaCaliCoder <c.roth.lambda@gmail.com>
2018-09-08 12:54:24 -0500

    Building Stretch carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,11 +1,19 @@
 class Carousel {
+constructor(el){
+    this.el = el;
+    this.left = document.querySelector('.left-button');
+    this.right = document.querySelector('.right-button');
+    
+}
 
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector('.carousel')
+carousel = Array.from(carousel).map((carousel=> new Carousel(carousel)))
+
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
+    1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
     2. You will need to grab a reference to all of the images
     3. Create a current index
     4. Those buttons are gonna need some click handlers.
