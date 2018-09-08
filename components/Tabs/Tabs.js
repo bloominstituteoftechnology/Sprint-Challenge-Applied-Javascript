348d832b9371dcdcd3489acbd708fd6867bed877
meganfontenot <meganfontenot517@yahoo.com>
2018-09-08 12:01:09 -0500

    worked on TabCard and reviewed the rest of the Tabs.js file

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,4 +1,4 @@
-class TabLink {
+class TabLink{
   constructor(element) {
     // assign this.element to the element reference
     this.element = element;
@@ -6,17 +6,19 @@ class TabLink {
     this.tabData = this.element.dataset.tab;
     // Find all elements with the .card class in index.html that correspond to the tab data attribute
     // If the data is 'all' then select all cards regardless of their data attribute
-    if (this.tabData === 'all') {
-      this.cards = document.querySelectorAll('.card');
+
+    if (this.tabData === 'all'){
+      this.cards = document.querySelectorAll('.card'); 
     } else {
       this.cards = document.querySelectorAll(`.card[data-tab="${this.tabData}"]`);
-    
+    }
 
     // Map over the cards array and convert each card element into a new instance of the TabCard class. 
     // Pass in the card object to the TabCard class.
     this.cards = Array.from(this.cards).map(cardsItem => new TabCard(cardsItem));
     // Add a click event that invokes selectTab
     this.element.addEventListener('click', () => this.selectTab());
+
   }
 
   selectTab(){
@@ -38,21 +40,22 @@ class TabLink {
   }
 }
 
-class TabCard {
-  constructor(element){
+class TabCard{
+  constructor(element) {
     // Assign this.element to the passed in element.
-    this.element;
+    this.element= element;
   }
   selectCard(){
     // Update the style of this.element to display = null
-    this.element;
+    this.element.style.display= null;
   }
 
 }
 
 // Create a reference to all ".tab" classes
-let tabs = document.querySelectorAll();
+let tabs = document.querySelectorAll(".tab");
 // Map over the array and convert each tab reference into a new TabLink object.  Pass in the tab object to the Tabs class.
-tabs = Array.from(tabs).map()
+tabs = Array.from(tabs).map(tabItems => new TabLink(tabItems))
 
 //Once you are complete, call the .select method on the first tab
+tabs[0].selectTab();
