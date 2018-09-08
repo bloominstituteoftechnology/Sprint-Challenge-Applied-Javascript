f9a19b9cc20844fdf892403261f3a5352d0616c1
cferrell2 <cferrell12182@gmail.com>
2018-09-08 14:15:45 -0400

    Add Carousel component functionality - Carousel.js

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,39 @@
 class Carousel {
+    constructor(element){
+        this.element = element;
+        this.images = this.element.querySelectorAll('img');
+        this.leftButton = this.element.querySelector('.left-button');
+        this.rightButton = this.element.querySelector('.right-button');
+        this.imgIndex = 0;
 
+        //add event listener to display correct image on click
+        this.leftButton.addEventListener('click', () => { this.showImgLeft() });
+        this.rightButton.addEventListener('click', () => { this.showImgRight() });
+    }
+
+    showImgLeft(){
+        this.imgIndex = (this.imgIndex === 0) ? 3 : (this.imgIndex - 1);
+        this.images.forEach(function(img){
+            img.style.display = 'none';
+        });
+        this.images[this.imgIndex].style.display = 'block';
+    }
+
+    showImgRight(){
+        this.imgIndex = (this.imgIndex === 3) ? 0 : (this.imgIndex + 1);
+        this.images.forEach(function (img) {
+            img.style.display = 'none';
+        });
+        this.images[this.imgIndex].style.display = 'block';
+    }
 }
 
-let carousel = document.querySelector();
+//grab carousel container div
+let carousel = document.querySelector('.carousel');
+//create Carousel Object/component
+carouselObj = new Carousel(carousel);
+//display first image on startup
+carouselObj.images[0].style.display = 'block';
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
