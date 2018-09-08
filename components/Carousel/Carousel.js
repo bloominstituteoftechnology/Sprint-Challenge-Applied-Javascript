f6e0997abf5ef1734147dab07cfa8c5fbf6dc46e
Abigayle Franklin <abifanklin@gmail.com>
2018-09-08 13:57:35 -0500

    Carousel mostly functioning

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,14 +1,48 @@
 class Carousel {
+    constructor(element){
+      this.element = element;
+      this.left = element.querySelector('.left-button');
+      this.right = element.querySelector('.right-button');      
+      this.imgs = element.querySelectorAll('img');
+      this.images = Array.from(this.imgs);
+      this.images.forEach((img) => {
+          img.style.display = 'none';
+      })
+      this.right.addEventListener('click', () => {this.clickRight()});
+      this.left.addEventListener('click', () => {this.clickLeft()});
+    }
+  
+    clickRight(){
+      for (let i=0; i < this.images.length; i++) {
+        if (this.images[i].style.display === 'block') {
+            if (this.images[i+1]) {
+                this.images[i].style.display = 'none';
+                this.images[++i].style.display = 'block';
+            } else {
+                this.images[i].style.display = 'none';
+                this.images[0].style.display = 'block';
+            }
+        }
+      }
+    }
 
-}
+    clickLeft(){
+        for (let i=0; i < this.images.length; i++) {
+          if (this.images[i].style.display === 'block') {
+            if (i > 0) {
+                this.images[i].style.display = 'none';
+                this.images[i-1].style.display = 'block';
+            } else {
+                this.images[0].style.display = 'none';
+                this.images[3].style.display = 'block';
+            }
+          }
+        }
+      }
+  }
+  
 
-let carousel = document.querySelector();
-
-/* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
-    2. You will need to grab a reference to all of the images
-    3. Create a current index
-    4. Those buttons are gonna need some click handlers.
-    5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
-    6. Have fun!
-*/
+  
+  let carousel = document.querySelectorAll('.carousel');
+  carousel = Array.from(carousel).map(((carousel) => new Carousel(carousel)));
+  document.querySelector('img').style.display = 'block';
