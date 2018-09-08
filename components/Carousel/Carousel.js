d32da4fd75715d972330f66bc8910bf37d224b33
Jesse Anderson <jesseanderson@Jesses-MacBook-Pro.local>
2018-09-08 12:08:48 -0500

    Progress on Carousel stretch goal

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,47 @@
 class Carousel {
+  constructor(element) {
+    this.element = element;
 
+    this.currentIndex = 0;
+
+    this.leftBtn = document.querySelector('.left-button');
+    console.log(this.leftBtn);
+    this.rightBtn = document.querySelector('.right-button');
+
+    this.images = this.element.querySelectorAll('img');
+    this.images = Array.from(this.images);
+
+    this.leftBtn.addEventListener('click', () => {
+      this.moveLeft();
+    });
+
+    this.rightBtn.addEventListener('click', () => {
+      this.moveRight();
+    });
+  }
+
+  moveLeft() {
+    console.log('left button clicked');
+    if (this.currentIndex !== 0) {
+      this.currentIndex -= 1;
+    }
+    console.log(this.currentIndex);
+  }
+
+  moveRight() {
+    console.log('right button clicked');
+    if (this.currentIndex !== this.images.length - 1) {
+      this.currentIndex += 1;
+    }
+    console.log(this.currentIndex);
+  }
+
+  showImg() {}
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector('.carousel');
+
+carousel = new Carousel(carousel);
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
