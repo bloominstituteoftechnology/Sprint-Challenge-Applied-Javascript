b807c989a752b690edf7c81776e7161464b854b6
leelichtsinn <lee.lichtsinn@gmail.com>
2018-09-08 10:11:36 -0700

    added .tab class reference to selectTab method removed .active-tab class from tabs nodeList added .active-tab class to this.element

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -23,11 +23,11 @@ class TabLink {
   selectTab(){
 
     // Select all elements with the .tab class on them
-    const tabs = document.querySelectorAll();
+    const tabs = document.querySelectorAll('.tab');
     // Iterate through the NodeList removing the .active-tab class from each element
-    tabs.forEach()
+    tabs.forEach( tab => tab.classlist.remove('active-tab'));
     // Add a class of ".active-tab" to this.element
-    this.element;
+    this.element.classlist.add('active-tab');
 
 
     // Select all of the elements with the .card class on them
