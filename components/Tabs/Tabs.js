3e416b8baf332b3bc4a18881eec36bc0219abb4f
FreeKeyBoy <jjhutton81@gmail.com>
2018-09-08 10:41:46 -0700

    Finished file Tabs.js. Moving on to debugging.

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -6,17 +6,17 @@ class TabLink {
     this.tabData= this.element.dataset.tab; 
     // Find all elements with the .card class in index.html that correspond to the tab data attribute
     // If the data is 'all' then select all cards regardless of their data attribute
-    if(){
-      this.cards = ;
+    if(this.element=== document.querySelector('tabs[data-tab="all"]')){
+      this.cards = cards.style.display= "";
     } else {
-      this.cards = ;
+      this.cards = card.selectCard();
     }
 
     // Map over the cards array and convert each card element into a new instance of the TabCard class. 
     // Pass in the card object to the TabCard class.
-    this.cards = Array.from(this.cards).map();
+    this.cards = Array.from(this.cards).map(card => new TabLink(card));
     // Add a click event that invokes selectTab
-    this.element.addEventListener();
+    this.element.addEventListener('click', () => {this.selectTab() });
   }
 
   selectTab(){
@@ -45,11 +45,11 @@ class TabLink {
 class TabCard {
   constructor(element){
     // Assign this.element to the passed in element.
-    this.element;
+    this.element= element;
   }
   selectCard(){
     // Update the style of this.element to display = null
-    this.element;
+    this.element= this.element.style.display= "null";
   }
 
 }
@@ -60,3 +60,4 @@ let tabs = document.querySelectorAll();
 tabs = Array.from(tabs).map()
 
 //Once you are complete, call the .select method on the first tab
+tabs[0].select();
