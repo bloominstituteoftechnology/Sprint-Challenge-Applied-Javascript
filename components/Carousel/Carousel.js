f7569477c0882640a1c263dac56d6c086e660359
Jess <jessdev83@gmail.com>
2018-09-08 13:50:34 -0400

    Stretch goals
    carousel works. working on transitions

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,14 +1,47 @@
 class Carousel {
-
+    constructor(el) {
+        this.el = el;
+        this.rightButton = document.querySelector('.right-button');
+        this.leftButton = document.querySelector('.left-button');
+        this.count = 0
+        this.rightButton.addEventListener('click', () => this.rightClick());
+        this.leftButton.addEventListener('click', () => this.leftClick());
+    }
+    start() {
+        const images = document.querySelectorAll('img');
+        images.forEach((img) => {
+            img.style.display = "none";
+        })
+        images[0].style.display = "block";
+    }
+    rightClick() {
+        const images = document.querySelectorAll('.carousel img');
+        images.forEach((img) => {
+            img.style.display = "none";
+        })
+        if (this.count < images.length - 1) {
+            this.count += 1;
+            images[this.count].style.display = "block";
+        } else {
+            this.count = 0;
+            images[this.count].style.display = "block";
+        }
+    }
+    leftClick() {
+        const images = document.querySelectorAll('.carousel img');
+        images.forEach((img) => {
+            img.style.display = "none";
+        })
+        if (this.count > 0) {
+            this.count -= 1;
+            images[this.count].style.display = "block";
+        } else {
+            this.count = images.length - 1;
+            images[this.count].style.display = "block";
+        }
+    }
 }
 
-let carousel = document.querySelector();
-
-/* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
-    2. You will need to grab a reference to all of the images
-    3. Create a current index
-    4. Those buttons are gonna need some click handlers.
-    5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
-    6. Have fun!
-*/
+let carousel = document.querySelector('.carousel');
+carousel = new Carousel();
+carousel.start()
