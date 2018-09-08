9c9f061eec96aed920a44d64616a5c56d6def88a
FreeKeyBoy <jjhutton81@gmail.com>
2018-09-08 09:59:41 -0700

    About half way done with file Tabs.js.

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,9 +1,9 @@
 class TabLink {
   constructor(element){
     // assign this.element to the element reference
-    this.element;
+    this.element= element;
     // Get the tab data attribute and save the value here
-    this.tabData = ; 
+    this.tabData= this.element.dataset.tab; 
     // Find all elements with the .card class in index.html that correspond to the tab data attribute
     // If the data is 'all' then select all cards regardless of their data attribute
     if(){
@@ -22,17 +22,21 @@ class TabLink {
   selectTab(){
 
     // Select all elements with the .tab class on them
-    const tabs = document.querySelectorAll();
+    const tabs = document.querySelectorAll(".tab");
     // Iterate through the NodeList removing the .active-tab class from each element
-    tabs.forEach()
+    tabs.forEach(function(element){
+      tabs.classList.remove(".active-tab");
+    })
     // Add a class of ".active-tab" to this.element
-    this.element;
+    this.element.classList.add(".active-tab");
 
 
     // Select all of the elements with the .card class on them
-    const cards = ;
+    const cards= document.querySelectorAll("card");
     // Iterate through the NodeList setting the display style each one to 'none'
-    cards.forEach()
+    cards.forEach(function(element){
+      cards.style.display= "none";
+    })
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class, nothing to update here
     this.cards.forEach(card => card.selectCard());
   }
