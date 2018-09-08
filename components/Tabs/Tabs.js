5b1ae8f1bcc379de54950d6b36feceb8b16cc80d
Jess <jessdev83@gmail.com>
2018-09-08 16:58:34 -0400

    Correction
    corrected selectTab function. Had unnecessary '.' before class names in classList.add() and remove()

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -25,10 +25,10 @@ class TabLink {
         const tabs = document.querySelectorAll('.tab');
         // Iterate through the NodeList removing the .active-tab class from each element
         tabs.forEach((tab) => {
-                tab.classList.remove('.active-tab')
+                tab.classList.remove('active-tab')
             })
             // Add a class of ".active-tab" to this.element
-        this.element.classList.add('.active-tab');
+        this.element.classList.add('active-tab');
 
 
         // Select all of the elements with the .card class on them
