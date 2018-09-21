36c7c8d4c3fc3a6bdd58e3ed2c32add5a0df3678
Troy Amlee <troyamlee19@gmail.com>
2018-09-21 13:12:47 -0500

    Troy Amlee [sprint] completed some stretch

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,53 @@
 class Carousel {
+constructor(caroElements) {
+    this.caroElements = caroElements;
+    console.log(this.caroElements);
+    let i = 0;
+    this.right = document.querySelector('.right-button');
+    this.left = document.querySelector('.left-button');
+    this.images = document.querySelectorAll('.carousel img');
+    console.log(this.images)
+    Array.from(this.images).map(caroimage => new CaroImages(caroimage));
+    
+    this.right.addEventListener('click', ()=>{
+        this.clickRight();
+     })
+
+     this.left.addEventListener('click', ()=>{
+        this.clickLeft();
+     })
+
+    }
+
+    clickRight() {
+        console.log("does right work?");
+
+        
+    }
+
+    clickLeft() {
+        console.log("does left work?");
+        let i = 0;
+        this.images[i].style.display = "flex";
+    }
 
 }
 
-let carousel = document.querySelector();
+class CaroImages {
+    constructor(caroElements) {
+        this.caroElements = caroElements;
+    }
+    selectImageRight(){
+        this.element.style.display = "block"
+    }
+
+    selectImageLeft(){
+        this.element.style.display = "block"
+    }
+}
+
+let carousel = document.querySelectorAll('.carousel');
+carousel = Array.from(carousel).map(caroElements => new Carousel(caroElements));
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
