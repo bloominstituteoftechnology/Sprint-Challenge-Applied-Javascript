c5bd64fb75cceac766ca1ebb387d8cedf73008b8
Chad Jemmett <chad.jemmett@gmail.com>
2018-09-21 11:46:45 -0600

    the main project is done. I was able to do a little bit of stretch. I can show one picture in the carosuel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,9 +1,27 @@
 class Carousel {
+  constructor(carousel) {
+
+  this.carousel = carousel;
+  this.leftButton = this.carousel.querySelector('.left-button');
+  this.rightButton = this.carousel.querySelector('.right-button');
+  this.pictures = this.carousel.querySelectorAll('img');
+  this.currentPicture = 3;
+  this.leftButton.addEventListener('click', () => {
+    this.pictures[this.currentPicture].style.display = 'inline';
+});
+  }
+
+showPicture() {
+  this.pictures[1].style.display.inline;
+  // this.pictures[this.currentPicture].style.display.inline;
+  
 
 }
 
-let carousel = document.querySelector();
-
+}
+let carousel = document.querySelector('.carousel');
+carousel = new Carousel(carousel);
+// carousel = Array.from(carousel).map(item => new Carousel(carousel));
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
     2. You will need to grab a reference to all of the images
