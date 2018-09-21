35157f5eab4947a42836c151ff9f28bba9e196a8
Jasmine Galang <jsmn.glng@gmail.com>
2018-09-21 09:53:23 -0700

    Made carousel working

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,58 @@
 class Carousel {
+    constructor(carousel) {
+        this.carousel = carousel;
+        this.leftBtn = this.carousel.querySelector('.left-button');
+        this.rightBtn = this.carousel.querySelector('.right-button');
+        this.images = this.carousel.querySelectorAll('img');
+        this.index = 0;
+
+        this.leftBtn.addEventListener('click', (event) => this.previous(event));
+        this.rightBtn.addEventListener('click', (event) => this.next(event));
+
+        this.activeImage();
+    }
+
+    activeImage() {
+        this.images[0].classList.add('show-img');
+    }
+
+    previous() {
+        if (this.index === 0) {
+            // index = 4 - 1
+            this.index = this.images.length - 1;
+        } else {
+            // index = indexValueGreaterThanZero - 1
+            this.index = this.index - 1;
+        }
+
+        // hide all images
+        this.images.forEach(image => image.classList.remove('show-img'));
+
+        // show image currently being viewed
+        this.images[this.index].classList.add('show-img');
+    }
+
+    next() {
+        // if index is on the last image
+        if (this.index === this.images.length-1) {
+            // go back to the first image
+            this.index = 0;
+        } else {
+            // go to the next image
+            this.index = this.index + 1;
+        }
+
+        // hide all images
+        this.images.forEach(image => image.classList.remove('show-img'));
+
+        // show image currently being viewed
+        this.images[this.index].classList.add('show-img');
+    }
 
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector('.carousel');
+carousel = new Carousel(carousel);
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
