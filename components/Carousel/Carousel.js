ba15872339b554a7c51baa72fd758636def6073f
SavannahGreuel <smallidgesavannah@gmail.com>
2018-09-21 11:14:17 -0600

    added content and started on carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,9 +1,10 @@
 class Carousel {
-
-}
-
-let carousel = document.querySelector();
-
+    constructor(element){
+        this.element = element;
+       
+ }	}
+ let carousel = document.querySelector();	let carousel = document.querySelector('.carousel');
+carousel = new Carousel(carousel);
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
     2. You will need to grab a reference to all of the images
