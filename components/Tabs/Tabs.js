6385cbaa8325468da48003f5451fd4d4bfc72c8a
Jake Thomas <jmaxt12@gmail.com>
2018-09-21 12:04:45 -0600

    Couldn't get anything to work... Not sure how close my code is

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -5,14 +5,14 @@ class TabLink {
     // Get the tab data attribute and save the value here
     this.tabData = this.element.dataset.tab; 
     // Find all elements with the .card class in index.html that correspond to the tab data attribute. If the data is 'all' then select all cards regardless of their data attribute
-    if(this.element === '.card'){
-      this.cards = ;
-    } else {
-      this.cards = ;
-    }
+    // if(this.element === '.card'){
+    //   display.this.cards;
+    // // } else {
+    // //   this.cards = ;
+    // }
 
     // Map over the cards array and convert each card element into a new instance of the TabCard class. Pass in the card object to the TabCard class.
-    this.cards = Array.from(this.cards).map( link => new TabCard(link));
+    this.cards = Array.from(this.cards).map( link => new Tab(link));
     // Add a click event that invokes selectTab
     this.element.addEventListener('click', () => { this.selectTab() });
   }
@@ -53,7 +53,7 @@ class TabCard {
 // Create a reference to all ".tab" classes
 let tabs = document.querySelectorAll('.tab');
 // Map over the array and convert each tab reference into a new TabLink object.  Pass in the tab object to the Tabs class.
-tabs = Array.from(tabs).map( tabs => new TabLink(tab));
+tabs = Array.from(tabs).map(tabs => new TabLink(tabs));
 
 //Once you are complete, call the .select method on the first tab
 tabs[0].select();
