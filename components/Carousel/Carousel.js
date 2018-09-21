1a421dac9fa557713dcdff6584ffc9b9c91f9f3d
Edward J <edjeong99@gmail.com>
2018-09-21 12:35:30 -0400

    implement carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,41 @@
 class Carousel {
+    constructor(element){
+        this.element = element;
+        this.currentPic = 0;
+        this.imgs = this.element.querySelectorAll('img');
+        this.maxPicNum = this.imgs.length;
+        this.imgs[this.currentPic].style.display = "block";
+
+        this.element.querySelector('.left-button').addEventListener('click', () => this.leftClicked());
+        this.element.querySelector('.right-button').addEventListener('click', () => this.rightClicked());
+    }
+
+    rightClicked() {
+        let newNum = this.currentPic + 1;
+        if (newNum == this.maxPicNum )
+            newNum = 0;
+        this.display(newNum, this.currentPic);
+        this.currentPic = newNum;            
+    }
+
+    leftClicked(){
+        let newNum = this.currentPic - 1;
+        if (newNum == -1 )
+            newNum = this.maxPicNum - 1;
+        this.display(newNum, this.currentPic);
+        this.currentPic = newNum;   
+    }
+
+    display(newNum, currentNum){
+    this.imgs[currentNum].style.display = 'none';
+    this.imgs[newNum].style.display = "block";
+
+    }
 
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector('.carousel');
+carousel = new Carousel(carousel);
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
@@ -12,3 +45,4 @@ let carousel = document.querySelector();
     5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
     6. Have fun!
 */
+
