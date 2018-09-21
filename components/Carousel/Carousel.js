5f4220eaf63950a87c8c1d086150c9a34adc709a
theryanclausen <theryanclausen@gmail.com>
2018-09-21 11:34:54 -0700

    mvp and carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,14 +1,62 @@
-class Carousel {
 
+
+
+class CarouselIMG {
+  constructor(element) {
+    this.element = element;
+    this.data = this.element.dataset.pic;
+
+
+  }
+  goLastPic() {
+    
+    this.element.classList.remove("current-img");
+
+    if (this.data - 1 === 0) {
+      this.lastPicData = 4;
+    } else {
+      this.lastPicData = this.data - 1;
+    }
+    this.lastPic = document.querySelector(`[data-pic= '${this.lastPicData}']`);
+    this.lastPic.classList.toggle("current-img");
+  }
+  goNextPic() {
+
+    this.element.classList.toggle("current-img");
+    if (Number(this.data) + 1 === 5) {
+      this.nextPicData = 1;
+    } else {
+      this.nextPicData = Number(this.data) + 1;
+    }
+    this.nextPic = document.querySelector(`[data-pic= '${this.nextPicData}']`);
+    this.nextPic.classList.toggle("current-img");
+  }
 }
 
-let carousel = document.querySelector();
+class Arrows {
+    constructor(element){
+        this.element = element;
+        this.isLeft = (this.element.classList.contains('left-button'));
+        this.element.addEventListener('click', () =>{
+            let currentIMG = document.querySelector('.current-img');
+            currentIMG = new CarouselIMG(currentIMG)
+            if (this.isLeft){
+                currentIMG.goLastPic();
+            }
+            else{
+                currentIMG.goNextPic();
+            }
+        })
+    }
+    
+}
 
-/* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
-    2. You will need to grab a reference to all of the images
-    3. Create a current index
-    4. Those buttons are gonna need some click handlers.
-    5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
-    6. Have fun!
-*/
+left = document.querySelector(".left-button");
+left = new Arrows(left);
+right = document.querySelector(".right-button");
+right = new Arrows(right);
+
+let carousel = document.querySelectorAll(".carouselIMG");
+carousel = Array.from(carousel).map(x => new CarouselIMG(x));
+
+
--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -54,15 +54,15 @@ class TabCard {
 
 }
 let cards = document.querySelectorAll('.card');
-console.log(cards)
+
 cards = Array.from(cards).map(x => new TabCard(x));
-console.log(cards)
+
 // Create a reference to all ".tab" classes
 let tabs = document.querySelectorAll('.tab');
 
 // Map over the array and convert each tab reference into a new TabLink object.  Pass in the tab object to the Tabs class.
 tabs = Array.from(tabs).map(x => new TabLink(x))
-console.log(tabs)
+
 
 
 //Once you are complete, call the .select method on the first tab
