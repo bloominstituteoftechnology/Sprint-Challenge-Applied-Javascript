f5398c61b049c7ff8bd222706e0e4ba35661d4d6
Michael Checo <checomichael2@gmail.com>
2018-09-21 13:03:53 -0400

    completed stretch assignment by adding carousel functionality

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,44 @@
 class Carousel {
-
+    constructor(element){
+        //assign element reference
+        this.element = element;
+        //assign reference to both left and right buttons
+        this.leftButton = element.querySelector('.left-button');
+        this.rightButton = element.querySelector('.right-button');
+        //reference to grab all images
+        this.img = element.querySelectorAll('.carousel img');
+        // console.log(this.img)
+        //Counter to be able through go through images
+        this.counter = 0;
+        //add click events for both buttons
+        this.leftButton.addEventListener('click', () => {this.goLeft()});
+        this.rightButton.addEventListener('click', () => {this.goRight()});
+    }
+      goLeft() {
+          this.img[this.counter].style.display = 'none';
+          if(this.counter > 0) {
+            this.counter -= 1;
+            this.img[this.counter].style.display = 'block';
+        } else {
+            this.counter = this.img.length - 1;
+            this.img[this.counter].style.display = 'block';
+        }
+      }
+      goRight() {
+        this.img[this.counter].style.display = 'none'
+        if(this.counter < this.img.length - 1){
+            this.counter += 1;
+            this.img[this.counter].style.display = 'block'
+        } else {
+            this.counter = 0;
+            this.img[this.counter].style.display = 'block'
+        }
+    }
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector('.carousel');
+carousel =  new Carousel(carousel);
+carousel.img[0].style.display = 'block';
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
