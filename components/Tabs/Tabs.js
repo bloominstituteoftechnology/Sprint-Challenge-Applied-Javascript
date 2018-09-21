5e3956b7c40d6ae25d23bf2858237aefffe86cb1
Brandon Vilaychith <blvilaychith@outlook.com>
2018-09-21 10:07:39 -0500

    Converted tab references into a new TabLink object

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -53,6 +53,6 @@ class TabCard {
 // Create a reference to all ".tab" classes
 let tabs = document.querySelectorAll('.tab');
 // Map over the array and convert each tab reference into a new TabLink object.  Pass in the tab object to the Tabs class.
-tabs = Array.from(tabs).map()
+tabs = Array.from(tabs).map(item => new TabLink(item));
 
 //Once you are complete, call the .select method on the first tab
