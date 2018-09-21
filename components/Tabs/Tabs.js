40972cb6b2336122c24636b78aa2e39f55f6c113
Unknown <zachary.arney@gmail.com>
2018-09-21 11:14:25 -0500

    Created reference to all .tab classes and mapped to make new TabLink obj

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -49,8 +49,8 @@ class TabCard {
 }
 
 // Create a reference to all ".tab" classes
-let tabs = document.querySelectorAll();
+let tabs = document.querySelectorAll('.tab');
 // Map over the array and convert each tab reference into a new TabLink object.  Pass in the tab object to the Tabs class.
-tabs = Array.from(tabs).map()
+tabs = Array.from(tabs).map(tab => new TabLink(tab));
 
 //Once you are complete, call the .select method on the first tab
