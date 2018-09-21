0a49f493a17a53ef1fd745147b479f35a3370099
Daniel Ferrer <danfrrr13@gmail.com>
2018-09-21 12:13:12 -0400

    fixed the error, but my IF statement which assigns values to this.cards is not working correctly yet

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -5,11 +5,11 @@ class TabLink {
     // Get the tab data attribute and save the value here
     this.tabData = this.element.dataset.tab; 
     // Find all elements with the .card class in index.html that correspond to the tab data attribute. If the data is 'all' then select all cards regardless of their data attribute
-    if(this.element.dataset.tab ='all'){
-      this.cards = document.querySelectorAll('.card'); 
-    } else {
+    // if(this.element.tabData = 'all') {
+    //   this.cards = document.querySelectorAll('.card'); 
+    // } else {
       this.cards = document.querySelectorAll(`.card[data-tab='${this.tabData}']`);
-    }
+    // }
     //
     // Map over the cards array and convert each card element into a new instance of the TabCard class. Pass in the card object to the TabCard class.
     this.cards = Array.from(this.cards).map(card => new TabCard(card));
@@ -37,9 +37,7 @@ class TabLink {
     // Select all of the elements with the .card class on them
     const cards = document.querySelectorAll('.card');
     // Iterate through the NodeList setting the display style each one to 'none'
-    cards.forEach(card => {
-      card.style.display = 'none';
-    });
+    cards.forEach(card => card.style.display = 'none');
     //console.log(cards);
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class, nothing to update here
     this.cards.forEach(card => card.selectCard());
@@ -54,7 +52,7 @@ class TabCard {
   }
   selectCard(){
     // Update the style of this.element to display = null
-    this.element.style.display = 'null';
+    this.element.style.display = 'flex';
   }
 
 }
@@ -62,8 +60,6 @@ class TabCard {
 // Create a reference to all ".tab" classes
 let tabs = document.querySelectorAll('.tab');
 // Map over the array and convert each tab reference into a new TabLink object.  Pass in the tab object to the Tabs class.
-tabs = Array.from(tabs).map(tab => {
-  new TabLink(tab);
-})
+tabs = Array.from(tabs).map(tab => new TabLink(tab));
 
 //Once you are complete, call the .select method on the first tab
