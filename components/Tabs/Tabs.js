0f2b5ca3d04017a95c05a7b1c4c895522d7663c5
Katy Hollobaugh <khollobaugh@gmail.com>
2018-09-21 11:59:10 -0400

    finished selectTab method

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -20,17 +20,19 @@ class TabLink {
   selectTab(){
 
     // Select all elements with the .tab class on them
-    const tabs = document.querySelectorAll();
+    const tabs = document.querySelectorAll('.tab');
     // Iterate through the NodeList removing the .active-tab class from each element
-    tabs.forEach()
+    tabs.forEach((tab) => {tab.classList.remove('.active-tab');
+  });
     // Add a class of ".active-tab" to this.element
-    this.element;
-
+    this.element.classlist.add('.active-tab');
 
     // Select all of the elements with the .card class on them
-    const cards = ;
+    const cards = document.querySelectorAll('.card');
+
     // Iterate through the NodeList setting the display style each one to 'none'
-    cards.forEach()
+    cards.forEach((card) => {card.classlist.style = 'display: none;'});
+
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class, nothing to update here
     this.cards.forEach(card => card.selectCard());
   }
