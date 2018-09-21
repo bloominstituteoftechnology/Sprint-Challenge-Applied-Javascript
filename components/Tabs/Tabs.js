0b0129cbb720397d2cdc3ae0b1334853158136f0
Hunter315 <hunter.mckay.smith@gmail.com>
2018-09-21 09:51:39 -0600

    fixed style.display = 'null' to style = 'display:null'

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -5,7 +5,7 @@ class TabLink {
     // Get the tab data attribute and save the value here
     this.tabData = this.element.dataset.tab; 
     // Find all elements with the .card class in index.html that correspond to the tab data attribute. If the data is 'all' then select all cards regardless of their data attribute
-    if(this.tabData === 'all'){
+    if(`${this.tabData}` === 'all'){
       this.cards = document.querySelectorAll('.card');
     } else {
       this.cards = document.querySelectorAll(`.card[data-tab="${this.tabData}"]`) ;
@@ -24,7 +24,7 @@ class TabLink {
     // Select all elements with the .tab class on them
     const tabs = document.querySelectorAll('.tab');
     // Iterate through the NodeList removing the .active-tab class from each element
-    tabs.forEach( tab => {tab.classList.remove('active-tab')});
+    tabs.forEach( tab => tab.classList.remove('active-tab'));
     // Add a class of ".active-tab" to this.element
     this.element.classList.add('active-tab');
 
@@ -32,7 +32,7 @@ class TabLink {
     // Select all of the elements with the .card class on them
     const cards = document.querySelectorAll('.card');
     // Iterate through the NodeList setting the display style each one to 'none'
-    cards.forEach(card => card.style.display = 'none');
+    cards.forEach(card => card.style = 'display:none');
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class, nothing to update here
     this.cards.forEach(card => card.selectCard());
   }
@@ -45,7 +45,7 @@ class TabCard {
   }
   selectCard(){
     // Update the style of this.element to display = null
-    this.element.style.display = 'null';
+    this.element.style = 'display:null';
   }
 
 }
