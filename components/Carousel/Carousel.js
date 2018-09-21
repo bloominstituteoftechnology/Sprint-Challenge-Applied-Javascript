09d21bba50997de289057201a0faa954e4dde114
ssmodish <smodish@hotmail.com>
2018-09-21 14:04:14 -0400

    Time is up right now - working on sprint

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,52 @@
 class Carousel {
+    constructor(element){
+        // assign this.element to the element reference
+        this.element = element;
 
+        // find all the images in this carousel
+        this.images = this.element.getElementsByTagName('img');
+        
+        // convert this.images into a real array
+        this.images = Array.from(this.images);
+
+        let currentIndex = 0;
+
+        this.leftButton = this.element.querySelectorAll('.left-button');
+        this.rightButton = this.element.querySelectorAll('.right-button');
+        Array.from(this.leftButton).map(button => addEventListener('click', (event => {this.leftSelected(event)})));
+        Array.from(this.rightButton).map(button => addEventListener('click', (event => {this.rightSelected(event)})));
+    }
+
+
+    leftSelected(){
+        currentIndex -= 1;
+        if(currentIndex < 0){
+            currentIndex = this.images.length - 1;
+        }
+        nextImage(currentIndex);
+    }
+
+    rightSelected(){
+        currentIndex += 1;
+        if(currentIndex > this.images.length - 1){
+            currentIndex = 0;
+        }
+        nextImage(currentIndex);
+    }
+
+    nextImage(index){
+        // set display on all images to null
+        this.images.map(image => image.style.display = '');
+
+        //set current image to display
+        this.images[index].style.display = 'block'; 
+    }
 }
 
-let carousel = document.querySelector();
+let carousels = document.querySelectorAll('.carousel');
+carousels = Array.from(carousels).map(carousel => new Carousel(carousel));
+
+
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
