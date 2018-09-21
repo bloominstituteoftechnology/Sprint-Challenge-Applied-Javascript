611bd4ee33e14aec5ce3cfa63837d8a131f1db3b
rytwalker <rytwalker@gmail.com>
2018-09-21 11:25:04 -0400

    Ran tabs through constructor function

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,9 +1,9 @@
 class TabLink {
   constructor(element){
     // assign this.element to the element reference
-    this.element;
+    this.element = element;
     // Get the tab data attribute and save the value here
-    this.tabData = ; 
+    this.tabData = this.element.dataset.tab; 
     // Find all elements with the .card class in index.html that correspond to the tab data attribute. If the data is 'all' then select all cards regardless of their data attribute
     if(){
       this.cards = ;
@@ -49,8 +49,8 @@ class TabCard {
 }
 
 // Create a reference to all ".tab" classes
-let tabs = document.querySelectorAll();
+let tabs = document.querySelectorAll('.tab');
 // Map over the array and convert each tab reference into a new TabLink object.  Pass in the tab object to the Tabs class.
-tabs = Array.from(tabs).map()
+tabs = Array.from(tabs).map(tab => new TabLink(tab));
 
 //Once you are complete, call the .select method on the first tab
