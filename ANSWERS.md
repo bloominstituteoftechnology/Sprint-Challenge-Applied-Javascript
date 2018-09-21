6e08d528ce5a7b2d12eb52bc6a14065276dd6142
Brian G <brian.gregorius@gmail.com>
2018-09-21 11:26:34 -0400

    first update

--- ANSWERS.md
+++ ANSWERS.md
@@ -1,6 +1,18 @@
 ### Questions:
 1. What is the DOM?
+ It gives us an interface that with which we can interact with and manipulate the page, changing document structure, content, and styling.
+
 2. What is an event?
+Every interaction a user has with a site is known as an event, a click, moving the mouse, scrolling the page, pressing a key on the keyboard, these are all events on the page and the browser can detect all of them.
+
 3. What is an event listener?
+
+An event listener are specific elements to listen to specific events and fires a callback when that event occurs.
+
 4. Why would we convert a NodeList into an Array?
+
+to get access to all of our array methods
+
 5. What is a component? 
+
+A component is HTML, CSS, and JavaScript brought together for reuse in a website or application.
--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,9 +1,10 @@
 class TabLink {
   constructor(element){
     // assign this.element to the element reference
-    this.element;
+    this.element = element;
     // Get the tab data attribute and save the value here
-    this.tabData = ; 
+    this.tabData = document.querySelector(`.card[data-tab='${this.element}]`);
+    console.log(this.tabData); 
     // Find all elements with the .card class in index.html that correspond to the tab data attribute. If the data is 'all' then select all cards regardless of their data attribute
     if(){
       this.cards = ;
@@ -49,8 +50,9 @@ class TabCard {
 }
 
 // Create a reference to all ".tab" classes
-let tabs = document.querySelectorAll();
+let tabs = document.querySelectorAll('.tab');
+console.log(tabs);
 // Map over the array and convert each tab reference into a new TabLink object.  Pass in the tab object to the Tabs class.
-tabs = Array.from(tabs).map()
+tabs = Array.from(tabs).map(tabs => new TabLink (tab));
 
 //Once you are complete, call the .select method on the first tab
