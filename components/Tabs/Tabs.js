49d27316c84c951272f9f776b29ea6c52597ad5c
Yusuf Nafey <yusufnafey@yahoo.com>
2018-09-21 09:46:41 -0700

    found error (had '' around null), completed project and tested with new tabs

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -9,14 +9,12 @@ class TabLink {
       this.cards = document.querySelectorAll('.card')
     } else {
       this.cards = document.querySelectorAll(`.card[data-tab="${this.tabData}"]`)
-      // ********** add/remove 'All' **********
     }
 
     // Map over the cards array and convert each card element into a new instance of the TabCard class. Pass in the card object to the TabCard class.
     this.cards = Array.from(this.cards).map(card => new TabCard(card))
     // Add a click event that invokes selectTab
-    this.element.addEventListener('click', () => {this.selectTab()});
-    // ********** add/remove curly braces **********
+    this.element.addEventListener('click', () => this.selectTab());
   }
 
   selectTab(){
@@ -25,7 +23,6 @@ class TabLink {
     const tabs = document.querySelectorAll('.tab')
     // Iterate through the NodeList removing the .active-tab class from each element
     tabs.forEach((tab) => tab.classList.remove('active-tab'))
-    // ********** add/remove plural tab/tabs (second and third) **********
     // Add a class of ".active-tab" to this.element
     this.element.classList.add('active-tab')
 
@@ -35,7 +32,7 @@ class TabLink {
     // Iterate through the NodeList setting the display style each one to 'none'
     cards.forEach((card) => card.style.display = 'none')
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class, nothing to update here
-    this.cards.forEach((card) => card.selectCard());
+    this.cards.forEach((card) => card.selectCard())
   }
 }
 
@@ -46,7 +43,7 @@ class TabCard {
   }
   selectCard(){
     // Update the style of this.element to display = null
-    this.element.style.display = 'null'
+    this.element.style.display = null
   }
 
 }
@@ -55,5 +52,5 @@ class TabCard {
 let tabs = document.querySelectorAll('.tab');
 // Map over the array and convert each tab reference into a new TabLink object.  Pass in the tab object to the Tabs class.
 tabs = Array.from(tabs).map(tab => new TabLink(tab))
-
 //Once you are complete, call the .select method on the first tab
+tabs[0].selectTab()
