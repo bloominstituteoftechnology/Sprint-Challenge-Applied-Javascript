5a42ca60ad82ea3038a2d495448ef8303b0eb334
cembreyfarquhar <cembreyfarquhar@gmail.com>
2018-09-21 12:10:01 -0500

    finished carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,19 +1,43 @@
 class Carousel {
-    constructor(carousel){
-        this.carousel = carousel;
-        console.log(this.carousel);
-        this.imageArr = Array.from(this.carousel.querySelectorAll('.carousel img'));
-        console.log(this.imageArr);
-        this.leftButton = this.carousel.querySelector('.left-button');
-        this.rightButton = this.carousel.querySelector('.right-button');
-                
-    }
+  constructor(carousel) {
+    this.carousel = carousel;
+    console.log(this.carousel);
+    this.imageArr = Array.from(this.carousel.querySelectorAll(".carousel img"));
+    console.log(this.imageArr);
+    this.leftButton = this.carousel.querySelector(".left-button");
+    this.rightButton = this.carousel.querySelector(".right-button");
+    let imageIndex = 1;
+
+    this.rightButton.addEventListener("click", () => {
+      this.displayImage(this.imageArr[imageIndex]);
+      imageIndex++;
+      if (imageIndex === this.imageArr.length) {
+        imageIndex = 0;
+      }
+    });
+    this.leftButton.addEventListener("click", () => {
+      this.displayImage(this.imageArr[imageIndex]);
+      imageIndex--;
+      if (imageIndex < 0) {
+        imageIndex = this.imageArr.length - 1;
+      }
+    });
+  }
+
+  displayImage(image) {
+    this.imageArr.forEach(img => {
+        img.style.display = "none";
+    });
+    image.style.display = "inline";
+    image.style.zIndex = "-2";
+  }
 }
 
-let carousel = document.querySelector('.carousel');
-const images = Array.from(document.querySelectorAll('.carousel img'));
+let carousel = document.querySelector(".carousel");
 
 carousel = new Carousel(carousel);
+
+carousel.displayImage(document.querySelector(".carousel img"));
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
