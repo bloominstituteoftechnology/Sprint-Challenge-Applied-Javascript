23bdef50e3f41e2cb7d3486d9e1263cbb3d95354
mm2079 <vayne3252@gmail.com>
2018-09-21 12:54:43 -0400

    2nd Commit

--- ANSWERS.md
+++ ANSWERS.md
@@ -1,6 +1,11 @@
 ### Questions:
 1. What is the DOM?
+   DOM refers to Document Object Model which is a web api that allows changing of elements( or nodes ) on a webpage through a document like structure.
 2. What is an event?
+   Event is a functionality that allows the user to interact with elements.
 3. What is an event listener?
+   Event listener is a function that takes two arguments: type of event and function that interacts when that certain even occurs.
 4. Why would we convert a NodeList into an Array?
+   NodeList is an HTML Collection which is an Array-like object. Unlike an Array it doesn't contain the useful functionalities such as map, reduce, etc.
 5. What is a component? 
+   Components are classes that are resposible of handling only one job and implemented in such a way that they are reusable.
--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -3,12 +3,12 @@ class TabLink {
     // assign this.element to the element reference
     this.element = element;
     // Get the tab data attribute and save the value here
-    this.tabData = element.getAttribute( "data-tab" ); 
+    this.tabData = this.element.dataset.tab; 
     // Find all elements with the .card class in index.html that correspond to the tab data attribute. If the data is 'all' then select all cards regardless of their data attribute
-    if( element.className === "card" && tabData ){
+    if( element.className === "all" ){
       this.cards = document.querySelectorAll( ".card" ) ;
     } else {
-      this.cards = document.querySelectorAll( ".all" );
+      this.cards = document.querySelectorAll( `.card[data-tab='${this.tabData}']` );
     }
 
     // Map over the cards array and convert each card element into a new instance of the TabCard class. Pass in the card object to the TabCard class.
@@ -16,7 +16,7 @@ class TabLink {
       return new TabCard( tabElm );
     } );
     // Add a click event that invokes selectTab
-    this.element.addEventListener( "click", this.selectTab );
+    this.element.addEventListener( "click", (event) => {this.selectTab(event)} );
     
   }
 
@@ -25,18 +25,18 @@ class TabLink {
     // Select all elements with the .tab class on them
     const tabs = document.querySelectorAll( ".tab" );
     // Iterate through the NodeList removing the .active-tab class from each element
-    tabs.forEach( (tabElm) =>{
-      return tabElm.classList.remove( ".active-tab" );
-    });
+    Array.from(tabs).forEach( (tabElm) =>
+      tabElm.classList.remove( ".active-tab" ));
+    
     // Add a class of ".active-tab" to this.element
-    this.element.setAttribute( ".active-tab" );
+    this.element.classList.add('active-tab');
 
 
     // Select all of the elements with the .card class on them
     const cards = document.querySelectorAll( ".card" );
     // Iterate through the NodeList setting the display style each one to 'none'
     cards.forEach( (cardElm) =>{
-      return cardElm.style.display = "none";
+      cardElm.style.display = "none";
     } )
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class, nothing to update here
     this.cards.forEach(card => card.selectCard());
@@ -51,7 +51,7 @@ class TabCard {
   }
   selectCard(){
     // Update the style of this.element to display = null
-    this.element = element.style.display = null;
+    this.element.style.display = null;
   }
 
 }
