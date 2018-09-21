1410bd58ceb1218732e80dbe47e4e5caf2d8de33
Trevor Lewis <tenzingtitan@gmail.com>
2018-09-21 10:53:46 -0700

    fixed

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,63 +1,12 @@
-// class Carousel {
-//     constructor(element) {
-//         this.element = element;
-
-//         this.leftButton = element.querySelector('.left-button');
-//         this.rightButton = element.querySelector('.right-button');
-
-//         this.images = this.element.querySelectorAll('img');
-//         this.currentIndex = 0;
-//         this.images[this.currentIndex].style.display = 'block';
-
-//         this.leftButton.addEventListener('click', () => {
-//             this.lastImage();
-//         })
-
-//         this.rightButton.addEventListener('click', () => {
-//             this.nextImage();
-//         })
-//     }
-
-//     lastImgae() {
-//         if ((this.currentIndex - 1) < 0) {
-//             this.currentIndex = this.images.length - 1;
-//         } else {
-//             this.currentIndex -=1;
-//         }
-//         thisimages.forEach(function(e) {
-//             each.style.display = 'none';
-//         })
-//         this.images[this.currentIndex].style.display = 'block';
-//     }
-
-//     lastImage() {
-//         if ((this.currentIndex + 1) < 0) {
-//             this.currentIndex = this.images.length - 1;
-//         } else {
-//             this.currentIndex +=1;
-//         }
-//         thisimages.forEach(function(e) {
-//             each.style.display = 'none';
-//         })
-//         this.images[this.currentIndex].style.display = 'block';
-//     }
-// }
-
-// let carousel = document.querySelectorAll('.carousel');
-// carousel = Array.from(carousel).map(carousel => new Carousel(carousel));
-
-
 class Carousel {
     constructor(element) {
         this.element = element;
-        // Get left and right buttons from carousel element
+
         this.leftButton = element.querySelector('.left-button');
         this.rightButton = element.querySelector('.right-button');
-        // Get all images within carousel element
+
         this.images = this.element.querySelectorAll('img');
-        // Set default index to 0
         this.currentIndex = 0;
-        // Show first image by default
         this.images[this.currentIndex].style.display = 'block';
 
         this.leftButton.addEventListener('click', () => {
@@ -65,51 +14,36 @@ class Carousel {
         })
 
         this.rightButton.addEventListener('click', () => {
-                this.nextImage();
+            this.nextImage();
         })
     }
-    
 
     lastImage() {
-        // Shift index -1 on left click, wraparound if index < 0
         if ((this.currentIndex - 1) < 0) {
             this.currentIndex = this.images.length - 1;
         } else {
-            this.currentIndex -= 1;
+            this.currentIndex -=1;
         }
-        // hide all images
-        this.images.forEach(function (each) {
+        thisimages.forEach(function(e) {
             each.style.display = 'none';
         })
-        // display image of current index
         this.images[this.currentIndex].style.display = 'block';
-        let currentImg = this.images[this.currentIndex];
-        TweenMax.fromTo(currentImg, 0.5, { ease: Back.easeOut, x: 1200 }, { ease: Back.easeOut, x: 0 });
     }
 
-
-    nextImage() {
-        // Shift index +1 on right click, wraparound if index > images.length - 1
-            if ((this.currentIndex + 1) > this.images.length - 1) {
-                this.currentIndex = 0;
-            } else {
-                this.currentIndex += 1;
-            }
-        
-        // hide all images
-        this.images.forEach(function (each) {
+    lastImage() {
+        if ((this.currentIndex + 1) < 0) {
+            this.currentIndex = this.images.length - 1;
+        } else {
+            this.currentIndex +=1;
+        }
+        thisimages.forEach(function(e) {
             each.style.display = 'none';
         })
-        // display image of current index
         this.images[this.currentIndex].style.display = 'block';
-        let currentImg = this.images[this.currentIndex];
-        TweenMax.fromTo(currentImg, 0.5, { ease: Back.easeOut, x: -1200 }, { ease: Back.easeOut, x: 0 });
     }
 }
 
-
 let carousel = document.querySelectorAll('.carousel');
-
 carousel = Array.from(carousel).map(carousel => new Carousel(carousel));
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
