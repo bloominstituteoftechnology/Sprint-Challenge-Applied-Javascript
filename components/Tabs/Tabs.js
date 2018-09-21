355e0e73e70aa9b426cd174e805e7ee37c661805
IAN CAMERON <iancameron@IANs-MacBook-Pro.local>
2018-09-21 11:30:05 -0600

    push it

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -35,7 +35,7 @@ class TabLink {
     const cards = document.querySelectorAll('.card');
     // Iterate through the NodeList setting the display style each one to 'none'
     cards.forEach( card => {
-      card.classList.toggle('');
+      this.card.classList.toggle('');
     })
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class, nothing to update here
     this.cards.forEach(card => card.selectCard());
