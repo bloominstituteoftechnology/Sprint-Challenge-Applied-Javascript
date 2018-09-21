38c2f5cbf942549a3852ed57beb491aeb7291484
zacharyarney <zachary.arney@gmail.com>
2018-09-21 12:06:48 -0500

    Changed empty string to null

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -48,7 +48,7 @@ class TabCard {
   }
   selectCard() {
     // Update the style of this.element to display = null
-    this.element.style.display = '';
+    this.element.style.display = null;
   }
 }
 
@@ -56,6 +56,5 @@ class TabCard {
 let tabs = document.querySelectorAll('.tab');
 // Map over the array and convert each tab reference into a new TabLink object.  Pass in the tab object to the Tabs class.
 tabs = Array.from(tabs).map(tab => new TabLink(tab));
-
 //Once you are complete, call the .select method on the first tab
 tabs[0].selectTab();
