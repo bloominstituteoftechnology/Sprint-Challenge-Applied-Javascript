5dd67a4c909e23a7951dee22ca380d851a204313
Ryan Boris <neodynia@icloud.com>
2018-09-21 13:48:09 -0400

    Work on carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,45 @@
 class Carousel {
+    constructor(element){
+      this.element = element;
+      this.leftArrow = document.querySelector('.left-button');
+      this.rightArrow = document.querySelector('.right-button');
+      this.data = this.element.dataset.pict;
+      this.dataItem = document.querySelector(`.picture[data-pict='${this.data}']`);
+  
+      this.leftArrow.addEventListener('click', () => this.goBack())
+      this.rightArrow.addEventListener('click', () => this.goForward())
+  
+    }
+    goBack() {
+      this.dataItem.classList.toggle('hide');
+      if (this.data - 1 === 0) {
+        this.lastPicData = 4 
+      }
+      else {
+        this.lastPicData = this.data - 1
+      }
+      this.lastPic = document.querySelector( `.picture[data-pict= '${this.lastPicData}']`)
+      this.lastPic.classList.toggle('hide')
+    }
 
-}
+    goForward() {
+      this.element.classList.toggle('hide');
+      if (Number(this.data) + 1 === 5){
+        this.nextPicData = 1;
+      }
+      else{
+        this.nextPicData = Number(this.data) + 1;
+      }
 
-let carousel = document.querySelector();
+      this.nextPic = document.querySelector(`.picture[data-pict= '${this.nextPicData}']`);
+      this.nextPic.classList.toggle('hide');
+    }
+  }
+  
+  let images = document.querySelectorAll('.picture')
+  
+  images = Array.from(images).map( image => new Carousel(image) );
+
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
