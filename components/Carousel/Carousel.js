3caf7e4352d2ee74c79932362db4e942e4934570
Carlos Lantigua <clantigua954@gmail.com>
2018-09-22 12:08:50 -0400

    completed carousel, working on animation bugs

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -3,37 +3,41 @@ class Carousel {
         this.element = element;
         this.buttonLeft = document.querySelector('.left-button');
         this.buttonRight = document.querySelector('.right-button');
-        this.images = document.querySelectorAll('.carousel img');
-        this.imgData = this.element.dataset.img;
-
-        this.leftButton.addEventListener('click', () => {
-            this.slideLeft();
-        })
-        this.rightButton.addEventListener('click', () => {
-            this.slideRight();
-        })
+        this.buttonLeft.addEventListener('click', () => this.leftShowImage())
+        this.buttonRight.addEventListener('click', () => this.rightShowImage())
+        this.images = Array.from(this.element.querySelectorAll('img'));
+        this.currentIndex = 0;
+        this.rightShowImage();
     }
 
-    slideLeft() {
-
+    leftShowImage() {
+        this.currentIndex === 0 ? this.currentIndex = 3 : this.currentIndex -= 1;
+        this.images.forEach(img => {
+            img.classList.remove('img-display');
+            img.classList.add('img-none');
+        })
+        this.images[this.currentIndex].classList.remove('img-none');
+        this.images[this.currentIndex].classList.add('img-display');
+        TweenMax.from('.img-none', 0, { opacity: 0 });
+        TweenMax.to('.img-display', 0.2, { opacity: 1 });
     }
-    slideRight() {
-
+    rightShowImage() {
+        this.currentIndex === 3 ? this.currentIndex = 0 : this.currentIndex += 1;
+        this.images.forEach(img => {
+            img.classList.remove('img-display');
+            img.classList.add('img-none');
+        })
+        this.images[this.currentIndex].classList.remove('img-none');
+        this.images[this.currentIndex].classList.add('img-display');
+        TweenMax.from('.img-none', 0, { opacity: 0 });
+        TweenMax.to('.img-display', 0.2, { opacity: 1 });
     }
 }
-// class MainImage {
-//     constructor(element) {
-//         this.element = element;
-//     }
-//     slideLeft() {
-//         this.element.style.display = null;
-//     }
-// }
 
 
 let carousel = document.querySelector('.carousel');
 // console.log(carousel);
-carousel = Array.from(carousel).map(tab => new Carousel(tab));
+carousel = new Carousel(carousel);
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
     2. You will need to grab a reference to all of the images
