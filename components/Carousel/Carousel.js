f6abdfa1eae52131ce9090ab2131c460b2248928
unknown <quan.nguyen199712@gmail.com>
2018-09-22 14:19:09 -0400

    added somewhat of a stretch. still not working as expected

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,18 +1,38 @@
 class Carousel {
+    constructor(element){
+        this.element = element;
+        this.leftButton = element.querySelector('.left-button');
+        this.rightButton = element.querySelector('.right-button');
+        this.images = element.querySelectorAll('.img');
+        this.currentIndex = 0;
+        this.images[0].style.display = 'block';
+        this.leftButton.addEventListener('click', () => {this.leftButtonClick()})
+        this.rightButton.addEventListener('click', () => { this.rightButtonClick() })
+    }
 
+    leftButtonClick(){
+        this.images[this.currentIndex].style.display = 'none'
+        if(this.currentIndex === 0){
+            this.currentIndex = this.images.length - 1
+        } else {
+            this.currentIndex = this.currentIndex - 1
+        }
+        this.images[this.currentIndex].style.display = 'block'
+    }
+
+    rightButtonClick(){
+        this.images[this.currentIndex].style.display = 'none'
+        if (this.currentIndex === this.images.length - 1) {
+            this.currentIndex = 0
+        } else {
+            this.currentIndex = this.currentIndex + 1
+        }
+        this.images[this.currentIndex].style.display = 'block'
+    }
 }
 
 let carousel = document.querySelector('.carousel');
-let rightButton= carousel.querySelector('.right-button');
-let leftButton= carousel.querySelector('.left-button');
-let img = carousel.querySelectorAll('.img');
-let currentIndex=0;
-img[currentIndex].style.display='block';
-rightButton.addEventListener('click',(event)=>{
-    for(currentIndex; currentIndex<4; currentIndex++){
-
-    }
-})
+carousel = new Carousel(carousel);
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
     2. You will need to grab a reference to all of the images
