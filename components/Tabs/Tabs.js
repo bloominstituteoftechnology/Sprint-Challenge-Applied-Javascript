16e1138238762f9721985ecfb64418ec3ef00995
Joe Bugajski <yogurtmystery@gmail.com>
2018-09-22 16:33:04 -0500

    Breakthrough! Getting selectTab event handler to console.log on click!

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -12,41 +12,41 @@ class TabLink {
     }
 
     // Map over the cards array and convert each card element into a new instance of the TabCard class. Pass in the card object to the TabCard class.
-    this.cards = Array.from(this.cards).map( card => new TabCard(card));
+    this.cards = Array.from(this.cards).map( element => new TabCard(element));
     // Add a click event that invokes selectTab
-    this.element.addEventListener('click', () => {
-      this.selectTab(event)
-    });
-  }
+     this.tabLinkElement.addEventListener('click', (event) => {
+       console.log("You've made it this far, don't quit now");
+      this.selectTab(event);
+     });
+   }
+   selectTab(event){
+    console.log("inside selectTab event handler");
+     // Select all elements with the .tab class on them
+     const tabs = document.querySelectorAll(".tab");
+     // Iterate through the NodeList removing the .active-tab class from each element
+     tabs.forEach( tab => tab.classList.remove("active-tab"));
+     // Add a class of ".active-tab" to this.element
+     this.element.classList.add("active-tab");
 
-  selectTab(){
 
-    // Select all elements with the .tab class on them
-    const tabs = document.querySelectorAll(".tab");
-    // Iterate through the NodeList removing the .active-tab class from each element
-    tabs.forEach()
-    // Add a class of ".active-tab" to this.element
-    this.element;
+     // Select all of the elements with the .card class on them
+     const cards = document.querySelectorAll(".card");
+     // Iterate through the NodeList setting the display style each one to 'none'
+     cards.forEach( card => card.style.display = "none")
+     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class, nothing to update here
+     this.cards.forEach(card => card.selectCard());
+   }
+ }
 
-
-    // Select all of the elements with the .card class on them
-    const cards = ;
-    // Iterate through the NodeList setting the display style each one to 'none'
-    cards.forEach()
-    // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class, nothing to update here
-    this.cards.forEach(card => card.selectCard());
-  }
-}
-
-class TabCard {
-  constructor(element){
-    // Assign this.element to the passed in element.
-    this.element;
-  }
-  selectCard(){
-    // Update the style of this.element to display = null
-    this.element;
-  }
+ class TabCard {
+   constructor(element){
+     // Assign this.element to the passed in element.
+     this.element = element;
+   }
+   selectCard(){
+     // Update the style of this.element to display = null
+     this.element.style.display = null;
+   }
 
 }
 
