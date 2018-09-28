ccdb8167e7cbf8e04282c58325a6b49d6642330e
Jake Thomas <jmaxt12@gmail.com>
2018-09-28 09:34:05 -0600

    Working on finishing TabLink Class

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,9 +1,9 @@
 class TabLink {
   constructor(element){
     // assign this.element to the element reference
-    this.element;
+    this.element = element;
     // Get the tab data attribute and save the value here
-    this.tabData = ; 
+    this.tabData = this.element.dataset.tab; 
     // Find all elements with the .card class in index.html that correspond to the tab data attribute. If the data is 'all' then select all cards regardless of their data attribute
     if(){
       this.cards = ;
@@ -12,9 +12,11 @@ class TabLink {
     }
 
     // Map over the cards array and convert each card element into a new instance of the TabCard class. Pass in the card object to the TabCard class.
-    this.cards = Array.from(this.cards).map();
+    this.cards = Array.from(this.cards).map( card => new TabCard(card));
     // Add a click event that invokes selectTab
-    this.element.addEventListener();
+    this.element.addEventListener( 'click', () => {
+      this.selectTab();
+    });
   }
 
   selectTab(){
@@ -39,7 +41,7 @@ class TabLink {
 class TabCard {
   constructor(element){
     // Assign this.element to the passed in element.
-    this.element;
+    this.element = element;
   }
   selectCard(){
     // Update the style of this.element to display = null
@@ -49,8 +51,8 @@ class TabCard {
 }
 
 // Create a reference to all ".tab" classes
-let tabs = document.querySelectorAll();
+let tabs = document.querySelectorAll('.tab');
 // Map over the array and convert each tab reference into a new TabLink object.  Pass in the tab object to the Tabs class.
-tabs = Array.from(tabs).map()
+tabs = Array.from(tabs).map( tab => new TabLink(tab))
 
 //Once you are complete, call the .select method on the first tab
