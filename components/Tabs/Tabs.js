99283df6a411e3a5d4349c77998ba9348863e198
2thumbstech <2thumbstech@gmail.com>
2018-09-29 20:08:24 -0600

    Making progress

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -2,50 +2,41 @@ class TabLink {
   constructor(element){
     // assign this.element to the element reference
     this.element = element;
-    // Get the tab data attribute and save the value here
-    this.tabData = this.element.tabData; 
+// Get the tab data attribute and save the value here
+this.tabData = element.dataset.tab ;
     // Find all elements with the .card class in index.html that correspond to the tab data attribute. If the data is 'all' then select all cards regardless of their data attribute
-    if ($ (this).tabData == '.all'){
-      this.cards = '.card' ;
+    if (this.tabData === 'all'){
+      this.cards = document.querySelectorAll('.card') ;
     } else {
-      
-    }
-
-    // Map over the cards array and convert each card element into a new instance of the TabCard class. Pass in the card object to the TabCard class.
-    cards = Array.from(cards).map(cards => {
-      return new TabCard (TabCard);
-
-      
-    // Add a click event that invokes selectTab
-    this.element.addEventListener('click', ()=>{
   
- this. selectTab();
-    });
-  });
-
-  selectTab();{
-
-    // Select all elements with the .tab class on them
+      this.cards = document.querySelectorAll(`.card[data-tab ='${this.tabData}']`) ;
+    }
+    // Map over the cards array and convert each card element into a new instance of the TabCard class. Pass in the card object to the TabCard class.
+    this.cards = Array.from(this.cards).map(cards => new TabCard(cards, this));
+    // Add a click event that invokes selectTab
+    this.element.addEventListener('click', () => this.selectTab());{
+   selectTab();
+  }
+// Select all elements with the .tab class on them
     const tabs = document.querySelectorAll('.tab');
     // Iterate through the NodeList removing the .active-tab class from each element
-    tabs.forEach( link =>{
-      tabs.classList.remove('active-tab')
-    });
+    tabs.forEach(element =>
+      element.classList.remove('active-tab'));
+
     // Add a class of ".active-tab" to this.element
-    this.element.classList.add('.activity-tab');
+    this.element.classList.add('active-tab');
 
 
-    // Select all of the elements with the .card class on them
-    const cards = 
-    document.querySelectorAll('.card');
+    //  Select all of the elements with the .card class on them
+    const cards = ('.card') ; { 
+      document.querySelectorAll('.card');
+    
     // Iterate through the NodeList setting the display style each one to 'none'
-    cards.forEach(link =>{
-      tabs.classList.style.display = 'none';
-    })
-  
-    // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class, nothing to update here
+    cards.forEach(card => {
+      card.style.display = 'none';
+ //Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class, nothing to update here
     this.card.forEach(card => card.selectCard());
-  }
+  });
 }
 
 class TabCard {
@@ -54,27 +45,24 @@ class TabCard {
     this.element = element;
   }
    
-selectCard(){
+selectCard()
+}
     // Update the style of this.element to display = null
     this.element ();{
       document.querySelector(card).style.display='none';
     };
-  }
-
-
 
 // Create a reference to all ".tab" classes
-let tabs = document.querySelectorAll('.tab')
+let tabs = this.element.querySelectorAll(".tab");{
 // Map over the array and convert each tab reference into a new TabLink object.  Pass in the tab object to the Tabs class.
-tabs= Array.from(tabs).map (tab  => new TabLink (tab));
-
-//   let dropdowns = document.querySelectorAll('.dropdown');
-// dropdowns = Array.from(dropdowns).map( dropdown => new Dropdown(dropdown));
-
-
+tabs = Array.from.map; () => new TabLink (tabs);
+}
 //Once you are complete, call the .select method on the first tab
-tab[0].select()
-
+this.activeTab;{
+this.activeTab.selectTab();
+}
 function newFunction() {
   return this;
+    }
+  }
 }
