6d371495c1b5f0ddcda7f329b787774414903bb9
Chris Honda <christopher.honda92@gmail.com>
2018-10-06 16:21:32 -0700

    Fixed typos in Carousel.js

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -5,8 +5,8 @@ class Carousel {
 let carousel = $();
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
+    1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
     2. Those buttons are gonna need some click handlers.
-    3. Think of how you would animate this compoennt using jQuery. We would like to see as much done as possible using jQuery
+    3. Think of how you would animate this component using jQuery. We would like to see as much done as possible using jQuery
     4. Have fun!
 */
