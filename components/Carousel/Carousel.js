4b239e0f6ec1ecadf0b1552b5150453ed0f87ae6
Latifah President <latifahpresident@Latifahs-MacBook-Pro.local>
2018-10-26 17:26:53 -0500

    fixed tabs so that each tab displays the correct cards

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,14 +1,42 @@
 class Carousel {
+    constructor(element){
+        this.element = element;
+        this.leftButton = element.querySelector('.left-button');
+        this.rightButton = element.querySelector('.right-button');
+        this.images = element.querySelectorAll('img');
+        this.currentIndex = 0;
+        this.images[0].style.display = 'block';
+        this.leftButton.addEventListener('click', () => {this.leftButtonClick()})
+        this.rightButton.addEventListener('click', () => { this.rightButtonClick() })
+    }
 
+    leftButtonClick(){
+        this.images[this.currentIndex].style.display = 'none'
+        if(this.currentIndex === 0){
+            this.currentIndex = this.images.length - 1
+        } else {
+            this.currentIndex = this.currentIndex - 1
+        }
+        this.images[this.currentIndex].style.display = 'block'
+    }
+
+    rightButtonClick(){
+        this.images[this.currentIndex].style.display = 'none'
+        if (this.currentIndex === this.images.length - 1) {
+            this.currentIndex = 0
+        } else {
+            this.currentIndex = this.currentIndex + 1
+        }
+        this.images[this.currentIndex].style.display = 'block'
+    }
 }
 
-let carousel = document.querySelector();
-
+let carousel = document.querySelector('.carousel');
+carousel = new Carousel(carousel);
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
-    2. You will need to grab a reference to all of the images
-    3. Create a current index
-    4. Those buttons are gonna need some click handlers.
-    5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
-    6. Have fun!
+    1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
+    2. Make sure you are importing the carousel.js file properlly in your index.html file, go and check right now. 
+    3. Those buttons are gonna need some click handlers.
+    4. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
+    5. Have fun!
 */
--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -4,23 +4,23 @@ class TabLink {
     this.tabElement = tabElement;
     
     // Get the `data-tab` value from this.tabElement and store it here
-     this.tabData = this.tabElement.dataset.tab ; 
+     this.tabData = this.tabElement.dataset.tab ; //checks all the data-tab for matching values
     
     // We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task:    
     
     // Check to see if this.tabData is equal to 'all'
     if(this.tabData === 'all'){
       // If `all` is true, select all cards regardless of their data attribute values
-      this.cards = document.querySelectorAll('.cards');
+      this.cards = document.querySelectorAll('.card');
+      //this.cards = document.querySelectorAll( `.card[data-tab ='javascript']`);
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
-      this.cards = this.tabElement.querySelector(`.card[data-tab ='${this.tabData}']`) ;
+      this.cards = document.querySelectorAll(`.card[data-tab ='${this.tabData}']`);
     }
     
-
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
     this.cards = Array.from(this.cards).map(card => new TabCard(card));
-
+      console.log(this.cards)
     // Add a click event that invokes this.selectTab
      this.tabElement.addEventListener('click', () => this.selectTab());
   }
@@ -28,13 +28,13 @@ class TabLink {
   selectTab(){
 
     // Select all elements with the .tab class on them
-     const tabs = document.querySelectorAll('.tab class');
+     const tabs = document.querySelectorAll('.tab');
     
     // Iterate through the NodeList removing the .active-tab class from each element
      tabs.forEach(tab => tab.classList.remove('active-tab'));
 
     // Select all of the elements with the .card class on them
-     const cards = document.querySelectorAll('.card') ;
+     const cards = document.querySelectorAll('.card');
 
     // Iterate through the NodeList setting the display style each one to 'none'
     cards.forEach(card => card.style.display = 'none')
