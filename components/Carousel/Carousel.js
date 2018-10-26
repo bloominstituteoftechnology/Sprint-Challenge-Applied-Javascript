0ca9257c3cb1f81c07948df88a97c8bac13d3c29
steelworx <eileencuevas62@gmail.com>
2018-10-26 13:34:28 -0400

    added carousel functionality in carousel.js

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,65 @@
 class Carousel {
+    constructor(element) {
+        this.element = element;
 
+        this.leftButton = document.querySelector('.left-button');
+        this.rightButton = document.querySelector('.right-button');
+        this.images = document.querySelectorAll('.carousel img');
+        this.index = Array.from(this.images).length - 1;
+
+        this.leftButton.addEventListener('click', () => {
+            this.cycleLeft();
+        })
+
+        this.rightButton.addEventListener('click', () => {
+            this.cycleRight();
+        })
+    }
+
+    cycleLeft () {
+        if (this.index  === 3) {
+            this.images[3].style.display = 'none';
+            this.index = 2;
+            this.images[2].style.display = 'inline';
+        } else if (this.index  === 2) {
+            this.images[2].style.display = 'none';
+            this.index = 1;
+            this.images[1].style.display = 'inline';
+        } else if (this.index  === 1) {
+            this.images[1].style.display = 'none';
+            this.index = 0;
+            this.images[0].style.display = 'inline';
+        } else {
+            this.images[0].style.display = 'none';
+            this.index = 3;
+            this.images[3].style.display = 'inline';
+        }
+    }
+
+    cycleRight() {
+        if (this.index  === 3) {
+            this.images[3].style.display = 'none';
+            this.index = 0;
+            this.images[0].style.display = 'inline';
+        } else if (this.index  === 0) {
+            this.images[0].style.display = 'none';
+            this.index = 1;
+            this.images[1].style.display = 'inline';
+        } else if (this.index  === 1) {
+            this.images[1].style.display = 'none';
+            this.index = 2;
+            this.images[2].style.display = 'inline';
+        } else {
+            this.images[2].style.display = 'none';
+            this.index = 3;
+            this.images[3].style.display = 'inline';
+        }
+    }
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector('.carousel');
+
+carousel = new Carousel(carousel);
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
