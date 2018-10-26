afb2c8cab768479d1790ec9592a8914757b9d19c
brooks <bpoltl1@gmail.com>
2018-10-26 13:00:47 -0500

    stretch attempt

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,38 @@
 class Carousel {
+    constructor(element){
+        this.element = element;
+        this.lButton = document.querySelector('.carousel .left-button')
+        this.rButton = document.querySelector ('.carousel .right-button');
+        this.imgs = document.querySelectorAll('.carousel img');
+        this.imgs = Array.from(this.imgs)
+        this.start = document.querySelector('.active-img');
+        this.current = this.imgs[0];
+        this.start.style.display = 'block';
+        console.log(this.imgs)
+
+        console.log(this.lButton)
+        console.log(this.rButton)
+        this.lButton.addEventListener('click',()=>this.leftClick())
+        this.rButton.addEventListener ('click',()=>this.rightClick());
+
+        
+    }
+    leftClick(){
+        console.log(this.start)
+        this.current.classList.remove('.active-img')
+        console.log (this.start);
+
+    }
+    rightClick() {
+        console.log('right')
+    }
+
 
 }
+let carousel= document.querySelectorAll('.carousel');
 
-let carousel = document.querySelector();
+// Map over the array and convert each tab reference into a new TabLink object.  Pass in the tab object to the Tabs class.  After you finish this line of code, it's time to build out your TabLink class at the top of the page!
+carousel = new Carousel(carousel);
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
