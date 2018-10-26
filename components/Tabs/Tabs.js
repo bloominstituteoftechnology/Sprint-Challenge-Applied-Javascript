af55a8511f2bfe54fa6c91d03bce5ba93455e7fb
1@$@1github <asaofdiamonds@gmail.com>
2018-10-26 11:23:29 -0600

    wrote all code but is not functional, starting debug process

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -30,33 +30,37 @@ class TabLink {
   selectTab(){
 
     // Select all elements with the .tab class on them
-     const tabs = document.querySelectorAll();
+     const tabs = document.querySelectorAll('.tab');
     
     // Iterate through the NodeList removing the .active-tab class from each element
-     tabs.forEach()
+     tabs.forEach( tab => {
+       tab.classList.remove('.active-tab'); // **removed dot from class if doesn't work add back**
+     })
 
     // Select all of the elements with the .card class on them
-    // const cards = ;
+    const cards = document.querySelectorAll('.card');
 
     // Iterate through the NodeList setting the display style each one to 'none'
-    // cards.forEach()
+    cards.forEach(card => {
+      card.style.display = 'none';
+    })
     
     // Add a class of ".active-tab" to this.tabElement
-    // this.tabElement;
+    this.tabElement.classList.add('.active-tab'); //  *** again removed dot from active-tab if it doesn't work replace it.
   
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class. Just un-comment the code and study what is happening here.
-    // this.cards.forEach(card => card.selectCard());
+     this.cards.forEach(card => card.selectCard());
   }
 }
 
 class TabCard {
   constructor(cardElement){
     // Assign this.cardElement to the cardElement DOM reference
-    // this.cardElement;
+     this.cardElement = cardElement;
   }
   selectCard(){
     // Update the style of this.cardElement to display = "flex"
-    // this.cardElement;
+    this.cardElement.style.display = 'flex';
   }
 
 }
