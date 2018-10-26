9fec1bfe8faf9a34664e118dcde433d783e225a8
David Freitag <freitagdavid@outlook.com>
2018-10-26 14:03:34 -0400

    got the right arrow almost working.

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,6 +1,29 @@
-class Carousel {}
+class Carousel {
+    constructor(carousel) {
+        this.carousel = carousel;
+        this.carouselImages = this.carousel.querySelectorAll('img');
+        this.carouselLButton = this.carousel.querySelector('.left-button');
+        this.carouselRButton = this.carousel.querySelector('.right-button');
+        this.carouselLButton.addEventListener('click', () => this.prevImage(this.activeImage));
+        this.carouselRButton.addEventListener('click', () => this.nextImage(this.activeImage));
+        this.activeImage = this.carouselImages[0];
+        this.activeImageIndex = 0;
+        this.displayImage(this.activeImage);
+    }
+    prevImage(currentImage) {}
+    nextImage(currentImage) {
+        currentImage.style.display = 'none';
+        this.activeImage = this.carouselImages[++this.activeImageIndex];
+        this.activeImageIndex += 1;
+        this.displayImage(this.activeImage);
+    }
+    displayImage(nextImage) {
+        nextImage.style.display = 'block';
+    }
+}
 
-let carousel = document.querySelector();
+let carousels = document.querySelectorAll('.carousel');
+carousels = Array.from(carousels).map(carousel => new Carousel(carousel));
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
