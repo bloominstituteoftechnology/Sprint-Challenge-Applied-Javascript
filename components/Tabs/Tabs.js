53dc9cf2a7749fe45ad05a27a830ecfa2caa613e
Trai Lynne <trailynne@outlook.com>
2018-10-26 11:00:06 -0600

    TabCard: selectCard() -> Update the display of this.cardElement to flex

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,27 +1,16 @@
 class TabLink {
     constructor(tabElement) {
-        // assign this.tabElement to the tabElement DOM reference
-        this.tabElement = tabElement;
 
-        // Get the `data-tab` value from this.tabElement and store it here
+        this.tabElement = tabElement;
         this.tabData = this.tabElement.dataset.tab;
 
-        // We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task:    
-
         // Check to see if this.tabData is equal to 'all'
-        if (this.tabData === 'all') {
-            // If `all` is true, select all cards regardless of their data attribute values
-            this.cards = document.querySelectorAll('.card');
-        } else {
-            // else if `all` is false, only select the cards with matching this.tabData values
-            this.cards = document.querySelectorAll(`.card[data-tab='${this.tabData}']`);
-        }
+        this.tabData === 'all' ? this.cards = document.querySelectorAll('.card') : this.cards = document.querySelectorAll(`.card[data-tab='${this.tabData}']`);
 
-
-        // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
+        // Convert to Array and TabCard objects
         this.cards = Array.from(this.cards).map(card => new TabCard(card));
 
-        // Add a click event that invokes this.selectTab
+        // Click Events
         this.tabElement.addEventListener('click', () => this.selectTab());
     }
 
@@ -58,7 +47,7 @@ class TabCard {
     }
     selectCard() {
         // Update the style of this.cardElement to display = "flex"
-        // this.cardElement;
+        this.cardElement.style.display = 'flex';
     }
 
 }
