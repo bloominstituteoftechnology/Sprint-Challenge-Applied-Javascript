54a4165649ee142347badbaf4fd4d358f1c71ab6
dyiar <austin.hl.blake@gmail.com>
2018-10-26 12:00:02 -0600

    brute forced through mvp and stretch

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,64 @@
 class Carousel {
+    constructor(element) {
+        this.element = element;
+
+        this.right = this.element.querySelector('.right-button');
+        this.left = this.element.querySelector('.left-button');
+
+        this.images = this.element.querySelectorAll('.carousel img');
+
+        this.index = 0;
+        this.images[this.index].style.display = "block";
+
+        this.right.addEventListener('click', () => (this.rightClick()));
+        this.left.addEventListener('click', () => (this.leftClick()));
+
+
+    }
+    // clickSides(n) {
+    //     if (this.element == right) {
+    //         showSlides(index += n)
+    //     } else {
+    //         showSlides(index -= n)
+    //     };
+    //     console.log('click');
+    // }
+
+    // plusSlides(n) {
+    //     this.showSlides(index += n);
+    // }
+
+    // showSlides(n) {
+    //     if (n > images.length) {
+    //         index = 1
+    //     };
+    //     if (n < 1) {
+    //         index = images.length
+    //     };
+    //     for (let i=0; i<images.length; i++) {
+    //         images[i].style.display = "none";
+    //     };
+    //     images[index-1].style.display = "block";
+    // }
+
+    leftClick() {
+        this.images.forEach(element => element.style.display = "none");
+        this.index -1 < 0 ? this.images[this.images.length-1].style.display = "block" : this.images[this.index-1].style.display = "block";
+        this.index - 1 < 0 ? this.index = this.images.length - 1 : this.index --;
+    }
+
+    rightClick() {
+        this.images.forEach(element => element.style.display = 'none');
+        this.index +1 > this.images.length -1 ? this.images[0].style.display = "block" : this.images[this.index+1].style.display = "block";
+        this.index + 1 > this.images.length -1 ? this.index = 0 : this.index ++;
+        // TweenLite.to(this.element, 3, {className: "img", ease:Power1.easeOut});
+    }
 
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelectorAll('.carousel');
+
+carousel = carousel.forEach(element => new Carousel(element));
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
