28a7bc6974f6d3d347c771f31f2a965d54f77ee5
Trai Lynne <trailynne@outlook.com>
2018-10-26 10:39:29 -0600

    TabCard: selectCard() -> Update the display of this.cardElement to flex

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -58,7 +58,7 @@ class TabCard {
     }
     selectCard() {
         // Update the style of this.cardElement to display = "flex"
-        // this.cardElement;
+        this.cardElement.style.display = 'flex';
     }
 
 }
