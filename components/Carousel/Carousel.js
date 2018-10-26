1bd8aaa9126268742848695a2dc853b438480349
BrandCam <BrandCam>
2018-10-26 12:38:44 -0500

    cleaned up code

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,46 +1,44 @@
 class Carousel {
-    constructor(carousel){
-        this.carousel = carousel;
-        
-        this.rightButton = document.querySelector('.right-button');
-        this.leftButton = document.querySelector('.left-button');
-        this.pictures = document.querySelectorAll('.carousel-img');
-        this.slideIndex = 1;
-        Array.from(this.pictures).map(picture => new Picture(picture));
-        
-        this.rightButton.addEventListener('click',() => this.toggel(1));
-        this.leftButton.addEventListener('click',() => this.toggel(-1));
+  constructor(carousel) {
+    this.carousel = carousel;
+
+    this.rightButton = document.querySelector(".right-button");
+    this.leftButton = document.querySelector(".left-button");
+    this.pictures = document.querySelectorAll(".carousel-img");
+    this.slideIndex = 1;
+    Array.from(this.pictures).map(picture => new Picture(picture));
+
+    this.rightButton.addEventListener("click", () => this.toggel(1));
+    this.leftButton.addEventListener("click", () => this.toggel(-1));
+  }
+  toggel(n) {
+    this.show((this.slideIndex += n));
+  }
+  show(n) {
+    if (n > this.pictures.length) {
+      this.slideIndex = 1;
     }
-    toggel(n){  
-        this.show(this.slideIndex += n);
+    if (n < 1) {
+      this.slideIndex = this.pictures.length;
     }
-    show(n){
-        if (n > this.pictures.length) {
-            this.slideIndex = 1
-        } 
-        if (n < 1) {
-            this.slideIndex = this.pictures.length
-        }
-        for (let i = 0; i < this.pictures.length; i++) {
-          this.pictures[i].style.display = "none"; 
-        }
-        this.pictures[this.slideIndex-1].style.display = "block"; 
+    for (let i = 0; i < this.pictures.length; i++) {
+      this.pictures[i].style.display = "none";
     }
+
+    this.pictures[this.slideIndex - 1].style.display = "block";
+  }
 }
 
-class Picture{
-    constructor(picture){
-        this.picture = picture;
-
-        
-    }
-
+class Picture {
+  constructor(picture) {
+    this.picture = picture;
+  }
 }
 
-let carousel = document.querySelector('.carousel');
+let carousel = document.querySelector(".carousel");
 
- new Carousel(carousel);
-//console.log(carousel);
+new Carousel(carousel);
+
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
     2. You will need to grab a reference to all of the images
