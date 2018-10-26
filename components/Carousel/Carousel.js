20779ad956667af97f31e58c39e40caf493524d6
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2018-10-26 12:14:43 -0500

    Carousel Stretch Component working properly

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,45 @@
 class Carousel {
+    constructor(element){
+        this.element = element;
+        
+        this.leftBtn = this.element.querySelector('.left-button');
+        this.rightBtn = this.element.querySelector('.right-button');
+
+        this.currentIndex = 0;
+
+        this.images = Array.from(element.querySelectorAll('img'))
+        console.log(this.images);
+
+        this.images[0].style.display = "block";
+
+        this.leftBtn.addEventListener('click', () => this.leftImage());
+        this.rightBtn.addEventListener('click', () => this.rightImage());
+    }
+    leftImage(){
+        if(this.currentIndex < 1)
+            this.currentIndex = this.images.length ;
+        this.currentIndex--;
+        this.images.forEach((img) => img.style.display = "none");
+        console.log(this.currentIndex);
+        this.images[this.currentIndex].style.display = "block";
+        
+    }
+    rightImage(){
+        if(this.currentIndex == this.images.length -1)
+            this.currentIndex = -1;
+        this.currentIndex++;
+        this.images.forEach((img) => img.style.display = "none");
+        this.images[this.currentIndex].style.display = "block";
+        
+    }
 
 }
 
-let carousel = document.querySelector();
+
+let carousel = document.querySelector('.carousel');
+carousel = new Carousel( carousel);
+
+
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
