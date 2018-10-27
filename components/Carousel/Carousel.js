e647bd082fc363d4904ee5f21ec01d63c9b63b37
Dennis Orbison <orbison.dennis@gmail.com>
2018-10-26 21:03:25 -0700

    refactored carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,63 +1,44 @@
 class Carousel {
     constructor(items) {
         this.items = items;
+        this.current = 0;
+        this.load = true;
+
         this.leftButton = this.items.querySelector('.left-button');
         this.rightButton = this.items.querySelector('.right-button');
-        this.images = items.querySelectorAll('img');
+        this.images = this.items.querySelectorAll('img');
 
-        this.images.forEach((element, i) => {
-            if (element.classList.value === 'current') this.current = i;
-        });
+        this.leftButton.addEventListener('click', () => { this.leftClick() });
+        this.rightButton.addEventListener('click', () => { this.rightClick(event) });
+        console.log('beginning')
+    }
 
-        if (event.target === this.leftButton) {
-            if (this.current > 0) this.current--
-
-            if (this.current === 0) this.leftButton.textContent = '';
-            if (this.current > 0) this.rightButton.textContent = '>';
-
-
-            this.images.forEach(element => {
-                element.classList.remove('current');
-            })
-
-            this.images[this.current].classList.remove('image-gone');
-            if (this.current > 0) this.images[this.current - 1].classList.add('image-gone');
-
-            setTimeout(() => {
-                this.images[this.current].classList.add('current');
-            }, 2000)
-
-
-        } else if (event.target === this.rightButton) {
-            if (this.current < this.images.length - 1) this.current++
-
-            if (this.current === this.images.length - 1) this.rightButton.textContent = '';
-            if (this.current < this.images.length - 1) this.leftButton.textContent = '<';
-
-
-            this.images.forEach(element => {
-                element.classList.remove('current');
-            })
-            if (this.images[this.current].classList.value === 'image-gone') this.images[this.current].classList.remove('image-gone');
-
-            this.images[this.current - 1].classList.add('image-gone');
-
-
+    leftClick() {
+        console.log('there')
+        if (this.current > 0) this.current--;
+        if (this.load = true) this.load = false;
+        if (this.images[this.current]) {
             this.images[this.current].classList.add('current');
+            this.images[this.current + 1].classList.remove('current');
+        }
+    }
 
-
+    rightClick() {
+        console.log('here')
+        if (this.current < this.images.length - 1) this.current++;
+        if (this.load = true) this.load = false;
+        if (this.images[this.current]) {
+            this.images[this.current].classList.add('current');
+            this.images[this.current - 1].classList.remove('current');
         }
 
     }
 
 }
 
-let carousel = document.querySelector('.carousel');
+const carousel = new Carousel(document.querySelector('.carousel'));
 
-carousel.addEventListener('click', () => {
-    return new Carousel(carousel);
-})
-
+console.log(carousel)
 // carousel.querySelector('left-button').addEventListener('click', (event) => {
 //     return new Carousel(event);
 // })
@@ -74,3 +55,44 @@ carousel.addEventListener('click', () => {
     5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
     6. Have fun!
 */
+
+// if (event.target === this.leftButton) {
+//     if (this.current > 0) this.current--
+
+//     if (this.current === 0) this.leftButton.textContent = '';
+//     if (this.current > 0) this.rightButton.textContent = '>';
+
+
+//     this.images.forEach(element => {
+//         element.classList.remove('current');
+//     })
+
+//     if (this.current < this.images.length - 1) this.images[this.current].classList.remove('image-gone');
+//     if (this.current > 0) this.images[this.current - 1].classList.add('image-gone');
+
+//     setTimeout(() => {
+//         this.images[this.current].classList.add('current');
+//     }, 2000)
+
+
+// } else if (event.target === this.rightButton) {
+//     if (this.current < this.images.length - 1) this.current++
+
+//     if (this.current === this.images.length - 1) this.rightButton.textContent = '';
+//     if (this.current < this.images.length - 1) this.leftButton.textContent = '<';
+
+//     if (this.current > 0) this.images[this.current - 1].classList.add('image-gone');
+
+//     // this.images.forEach(element => {
+//     //     element.classList.remove('current');
+//     // })
+
+//     if (this.images[this.current].classList.value === 'image-gone') this.images[this.current].classList.remove('image-gone');
+
+
+
+
+//     this.images[this.current].classList.add('current');
+
+
+// }
