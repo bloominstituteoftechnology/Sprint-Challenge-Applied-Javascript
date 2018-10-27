6b90288968fd3d92539a190784417e5e9d0e7d00
kevinmarker1101 <kmarker1101@icloud.com>
2018-10-27 09:56:11 -0500

    formating

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -3,25 +3,25 @@ class Carousel {
         this.currentIndex = 0;
         this.carousel = carousel;
         this.imgs = Array.from(this.carousel.querySelectorAll('img'));
-		this.leftButton = document.querySelector('.left-button');
+        this.leftButton = document.querySelector('.left-button');
         this.rightButton = document.querySelector('.right-button');
         this.leftButton.addEventListener('click', () => this.shiftLeft());
-		this.rightButton.addEventListener('click', () => this.shiftRight());
+        this.rightButton.addEventListener('click', () => this.shiftRight());
     }
-    
+
     shiftLeft() {
-		this.currentIndex === 0 ? (this.currentIndex = 3) : (this.currentIndex -= 1);
-		this.imgs.forEach(img => img.classList.replace('img-show', 'img-null'));
-		this.imgs[this.currentIndex].classList.replace('img-null', 'img-show');
+        this.currentIndex === 0 ? (this.currentIndex = 3) : (this.currentIndex -= 1);
+        this.imgs.forEach(img => img.classList.replace('img-show', 'img-null'));
+        this.imgs[this.currentIndex].classList.replace('img-null', 'img-show');
     }
-    
+
     shiftRight() {
-		this.currentIndex === 3 ? (this.currentIndex = 0) : (this.currentIndex += 1);
-		this.imgs.forEach(img => img.classList.replace('img-show', 'img-null'));
-		this.imgs[this.currentIndex].classList.replace('img-null', 'img-show');
-	}
+        this.currentIndex === 3 ? (this.currentIndex = 0) : (this.currentIndex += 1);
+        this.imgs.forEach(img => img.classList.replace('img-show', 'img-null'));
+        this.imgs[this.currentIndex].classList.replace('img-null', 'img-show');
+    }
 }
-    
+
 
 
 let carousel = document.querySelector('.carousel');
