c25e2899638a24740a5d939477a43c60d21758c1
Rachel Jefferies <rachel_lynne_t@yahoo.com>
2018-11-02 14:14:41 -0400

    fixed error line 54

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -34,6 +34,7 @@ class TabLink {
 
     // Select all elements with the .tab class on them
     const tabs = document.querySelectorAll(".tabs");
+    console.log(tabs);
     
     // Iterate through the NodeList removing the .active-tab class from each element
     tabs.forEach( item => {
@@ -41,18 +42,16 @@ class TabLink {
     })
 
     // Select all of the elements with the .card class on them
-    const cards = querySelectorAll(".card");
-    console.log(querySelectorAll(".card"));
+    const cards = document.querySelectorAll(".card");
+  
 
     // Iterate through the NodeList setting the display style each one to 'none'
     cards.forEach( card => {
       card.style.display = "none";
-
-      console.log(this.selectTab())
     });
     
     // Add a class of ".active-tab" to this.tabElement
-    this.tabElement = this.tabElement.classList.add(".active-tab");
+    this.tabElement.classList.add(".active-tab");
   
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class. Just un-comment the code and study what is happening here.
     this.cards.forEach(card => card.selectCard());
