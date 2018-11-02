5809a57f3094726622bacceee0f9ac94a6727900
chrisgins <christiansgina@gmail.com>
2018-11-02 09:16:43 -0600

    map over/convert this.cards element into a new instance of the TabCard

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -16,12 +16,10 @@ class TabLink {
       // else if `all` is false, only select the cards with matching this.tabData values
       this.cards = document.querySelectorAll(`.card[data-tab="${this.tabData}"]`) ;
     }
-    
 
-    
-
+  
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
-    // this.cards = Array.from(this.cards).map();
+     this.cards = Array.from(this.cards).map( card => new TabCard(card));
 
     // Add a click event that invokes this.selectTab
     // this.tabElement.addEventListener();
