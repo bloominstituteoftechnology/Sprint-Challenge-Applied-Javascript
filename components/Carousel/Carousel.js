f8c0fb63b08bffe2d5a55872b4f5fd40f4283c27
1@$@1github <asaofdiamonds@gmail.com>
2018-11-02 11:42:09 -0600

    starting over again on carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,19 +1,62 @@
+/* If You've gotten this far, you're on your own! Although we will give you some hints:
+    1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
+    2. You will need to grab a reference to all of the images
+    3. Create a current index
+    4. Those buttons are gonna need some click handlers.
+    5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
+    6. Have fun!
+*/
 class Carousel {
-    //need constructor and properties
-    constructor(imgElement) {
-        this.imgElement = imgElement;
-        this.pic = pic;
-        console.log(pic);
-    }
-    // method goes here
+    
 }
 
+
+
+
+
+
+// class Carousel {
+//     //need constructor and properties
+//     constructor(imgElement) {
+//         this.imgElement = imgElement;
+//         this.box = document.querySelector('.carousel');
+//         this.next = box.querySelector('.right-button');
+//         this.prev = box.querySelector('.left-button');
+//         this.items = box.querySelectorAll('.carousel img');
+//         this.counter = 0;
+//         this.amount = items.length;
+//         this.current = items[0];
+//         box.classList.add('active');
+//     }
+//         navigate(direction) {
+//             current.classList.remove('current');
+//             counter = counter + direction;
+//             if (direction === -1 &&
+//                 counter < 0) {
+//                 counter = amount - 1;
+//             }
+//             if (direction === 1 &&
+//                 !items[counter]) {
+//                 counter = 0;
+//             }
+//             current = items[counter];
+//             current.classList.add('current');
+//         }
+//         next.addEventListener('click', () => {
+//             this.navigate(1);
+//         });
+//         prev.addEventListener('click',() => {
+//             navigate(-1);
+//         });
+//         navigate(0);
+//     })();
+
 // going to select all images under .carousel
-let pic = document.querySelectorAll('.carousel .pics');
+// let pic = document.querySelectorAll('.carousel .pics');
 
 // create an array from the NodeList so can iterate
-pic = Array.from(pic).map(link => new Carousel (link));
-console.log(pic);
+// pic = Array.from(pic).map(link => new Carousel (link));
+// console.log(pic);
 
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
