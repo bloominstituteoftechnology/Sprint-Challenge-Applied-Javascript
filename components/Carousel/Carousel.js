e9297a8b81b565e69080c48e227d7c7109a092c6
chrisgins <christiansgina@gmail.com>
2018-11-02 11:46:02 -0600

    Stretchy stuff

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,55 +1,64 @@
 class Carousel {
     constructor(element) {
-        //reference the DOM element
+        // assign this.element to the element reference
         this.element = element;
-        //grabbed reference to the carousel for left buttons
-        this.leftButton = this.element.querySelector('.left-button')
-        //grabbed reference to the carousel for right buttons
-        this.rightButton = this.element.querySelector('.right-button')
-        //grabbed a reference to all of the images
-        this.images = this.element.querySelectorAll('img')
-        //Set the index
-        this.indexArr = [0,1,2,3]
-        this.index = 0
-        //Add event listener to execute script on click
-        this.rightButton.addEventListener('click', () => this.scrollRight())
-        this.leftButton.addEventListener('click', () => this.scrollLeft())
+        let leftbutton = document.querySelector('.left-button');
+        this.leftbutton = leftbutton;
+        let rightbutton = document.querySelector('.right-button');
+        this.rightbutton = rightbutton;
+        let carouselImgs = document.querySelectorAll('.carousel img');
+        this.carouselImgs = carouselImgs;
+        let index = 0;
+        this.index = index;
+        this.leftbutton.addEventListener('click', (event) => {
+            // Call the select method you define below
+            this.CarouselScrollLeft(event);
+        });
+        this.rightbutton.addEventListener('click', (event) => {
+            // Call the select method you define below
+            this.CarouselScrollRight(event);
+        });
+    }
+    CarouselScrollLeft(event) {
+        if (this.index > 0) {
+            this.carouselImgs[this.index].style.display = 'none';
+            //decrement through index
+            this.index = this.index - 1;
+            this.carouselImgs[this.index].style.display = 'flex';
+        }
+        else {
+            this.carouselImgs[this.index].style.display = 'flex';
+        }
 
-  }
-  scrollRight() {
-      //iterate through the classList adding/removing images (toggle on/off)
-    this.images.forEach ( item => item.classList.remove('carousel-inactive-right') )
-    this.images.forEach ( item => item.classList.remove('carousel-active-right') )
-    this.images.forEach( item  => item.classList.remove('carousel-inactive-left') )
-    this.images.forEach ( item => item.classList.remove('carousel-active-left') )
-    
-    //increment the value of the index
-    this.index++
-
-    this.index %= 4;
-
-    this.index == 0 ?
-    this.images[3].classList.toggle('carousel-inactive-right')
-    :
-    this.images[this.index-1].classList.toggle('carousel-inactive-right')
-
-    this.images[this.index].classList.add('carousel-active-right')
-    console.log('ind:', this.index);
-  }
-
-    
-
-
+    }
+    CarouselScrollRight(event) {
+        if (this.index < 3) {
+            this.carouselImgs[this.index].style.display = 'none';
+            //increment through index
+            this.index = this.index + 1;
+            this.carouselImgs[this.index].style.display = 'flex';
+        }
+        else {
+            this.carouselImgs[this.index].style.display = 'flex';
+        }
+    }
 }
 
-//set the carousel reference
-let carousel = document.querySelector('.carousel');
-//Changed line of code to convert nodes to array - this works better for me here.
+
+
+let carousel = document.querySelectorAll('.carousel');
+
 carousel = Array.from(carousel).map(link => new Carousel(link));
+
+carousel[0].CarouselScrollLeft();
+
+
+
+
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
-    2. You will need to grab a reference to all of the images
+    2. You will need to grab a reference to all of the Img
     3. Create a current index
     4. Those buttons are gonna need some click handlers.
     5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
