55f20364708def7fcfe4aede4f61ae17f35049db
Rachel Jefferies <rachel_lynne_t@yahoo.com>
2018-11-02 12:36:00 -0400

    completed Add a click event that invokes this.selectTab

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -20,10 +20,13 @@ class TabLink {
 
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
 
-    this.cards = Array.from(this.cards).map( card => new TabCard);
+    this.cards = Array.from(this.cards).map( card => new TabCard(card));
 
     // Add a click event that invokes this.selectTab
-    // this.tabElement.addEventListener();
+    
+    this.tabElement.addEventListener('click', () => {
+      this.selectTab();
+    });
   }
 
   selectTab(){
