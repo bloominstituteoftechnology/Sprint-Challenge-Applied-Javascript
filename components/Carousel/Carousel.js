99a2a7ef743a26924c7fa0160005a8a1e497c256
1@$@1github <asaofdiamonds@gmail.com>
2018-11-02 11:17:48 -0600

    ugh this Carousel is so frustraiting, I though I knoew what I was doing but I clearly don't

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,11 +1,23 @@
 class Carousel {
-
+    //need constructor and properties
+    constructor(imgElement) {
+        this.imgElement = imgElement;
+        this.pic = pic;
+        console.log(pic);
+    }
+    // method goes here
 }
 
-let carousel = document.querySelector();
+// going to select all images under .carousel
+let pic = document.querySelectorAll('.carousel .pics');
+
+// create an array from the NodeList so can iterate
+pic = Array.from(pic).map(link => new Carousel (link));
+console.log(pic);
+
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
+    1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
     2. You will need to grab a reference to all of the images
     3. Create a current index
     4. Those buttons are gonna need some click handlers.
