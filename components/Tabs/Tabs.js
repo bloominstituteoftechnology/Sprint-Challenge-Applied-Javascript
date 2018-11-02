4850a6813d7c58d21e4fc859b9d06fbc57689363
chrisgins <christiansgina@gmail.com>
2018-11-02 08:57:50 -0600

    Create a reference to all ".tab" classes.

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -60,7 +60,8 @@ class TabCard {
 }
 
 // First step! Create a reference to all ".tab" classes.
-// let tabs = document.querySelectorAll();
+
+let tabs = document.querySelectorAll('.tab');
 
 // Map over the array and convert each tab reference into a new TabLink object.  Pass in the tab object to the Tabs class.  After you finish this line of code, it's time to build out your TabLink class at the top of the page!
 // tabs = Array.from(tabs).map()
