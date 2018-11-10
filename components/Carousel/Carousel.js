6cbec8d846f4af9cfe0011fe32b97c00b28bc53e
echopecho <pbsmithtbn@gmail.com>
2018-11-10 12:09:50 -0600

    Complete carousel functionality
    I went through the first part of the stretch task to complete the carousel. Because I thought it was important and not too challenging I added the infinite loop as part of the base functionality. The code is a little wet, but I was unsure if things would work so I kept it simple. If there is time, I will go back and dry up the code a bit.

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,42 @@
 class Carousel {
+  constructor(carouselElement) {
+    this.carouselElement = carouselElement;
 
+    
+    this.leftButton = this.carouselElement.querySelector('.left-button');
+    this.rightButton = this.carouselElement.querySelector('.right-button');
+
+    this.images = document.querySelectorAll('.carousel img');
+
+    this.index = 0;
+
+    console.log(this.leftButton, this.rightButton)
+
+    this.leftButton.addEventListener('click', () => this.left());
+    this.rightButton.addEventListener('click', () => this.right());
+  }
+
+  right() {
+    this.images[this.index].style.display = 'none';
+    this.index++;
+    if(this.index >= this.images.length) {
+      this.index = 0;
+    }
+    this.images[this.index].style.display = 'block';
+  }
+
+  left() {
+    this.images[this.index].style.display = 'none';
+    this.index--;
+    if(this.index <= 0) {
+      this.index = this.images.length - 1;
+    }
+    this.images[this.index].style.display = 'block';
+  }
 }
 
-let carousel = document.querySelector();
+let carousel = new Carousel(document.querySelector('.carousel'));
+
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
