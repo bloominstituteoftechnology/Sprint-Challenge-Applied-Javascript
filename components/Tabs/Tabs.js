0dc6783ccd19d80f59e1c6f873a1579aaeb9b78c
rberriz <rberriz@icloud.com>
2018-11-10 14:54:42 -0500

    Final

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,25 +1,21 @@
 class TabLink {
   constructor(tabElement){
     // assign this.tabElement to the tabElement DOM reference
-    // this.tabElement;
     this.tabElement = tabElement;
     // Get the `data-tab` value from this.tabElement and store it here
     this.tabData = this.tabElement.dataset.tab; 
     
-    
     if(this.tabData === 'all'){
       // If `all` is true, select all cards regardless of their data attribute values
-      this.cards = tabs ;
+      this.cards = document.querySelectorAll('.card');
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
-      this.cards = this.tabData;
+      this.cards = document.querySelectorAll(`.card[data-tab='${this.tabData}']`);
     }
     
 
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
-    this.cards = Array.from(this.cards).map(function(){
-      return new Tabcard(cards)
-    });
+    this.cards = Array.from(this.cards).map(card => new TabCard(card));
 
     // Add a click event that invokes this.selectTab
     this.tabElement.addEventListener('click', () => {this.selectTab()} );
@@ -75,8 +71,8 @@ class TabCard {
 */
 let tabs = document.querySelectorAll('.tab');
 
-tabs.forEach(function(tabs) {
-  return new Tabcard(tabs)
-})
+tabs.forEach(function(tab) {
+  return new TabLink(tab)
+});
 
 
