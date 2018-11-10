62d280a5b10faa400b6f9473a0a59e7264fb2f34
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2018-11-10 14:00:59 -0600

    Referenced some objects for stretch.

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -3,9 +3,17 @@ class Carousel {
 }
 
 let carousel = document.querySelector('carousel');
+let leftBtn = document.querySelector('left-button');
+let rightBtn = document.querySelector('right-button');
+let CarouselImg = document.querySelectorAll('Carousel-Img');
+
+CarouselImg[0].textContent = siteContent.nav["./assets/carousel/mountains.jpeg"]
+CarouselImg[1].textContent = siteContent.nav["./assets/carousel/computer.jpeg"]
+CarouselImg[2].textContent = siteContent.nav["./assets/carousel/trees.jpeg"]
+CarouselImg[3].textContent = siteContent.nav["./assets/carousel/turntable.jpeg"]
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
+    1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
     2. You will need to grab a reference to all of the images
     3. Create a current index
     4. Those buttons are gonna need some click handlers.
