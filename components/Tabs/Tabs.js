958613eb661600d2e8fca72a63ec534214f3d605
ThomasUtsey <tomutsey@yahoo.com>
2018-11-10 13:20:47 -0500

    add the Start Here section
    Assign all of the elements with the class tab to the variable tabs and chain on a foreach creating a new instace of tablink class for each of them
    
    Blockers: Read code in its entirety before starting so when starting to code developer begins in proper place

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -49,14 +49,13 @@ class TabCard {
     this.cardElement.style.display = 'flex';
   }
 
-// }
+}
 
 // /* START HERE: 
 
 // - Select all classes named ".tab" and assign that value to the tabs variable
-
+let tabs = document.querySelectorAll('tab').forEach(tab => new TabLink(tab));
 // - With your selection in place, now chain a .forEach() method onto the tabs variable to iterate over the DOM NodeList
-
 // - In your .forEach() method's callback function, return a new instance of TabLink and pass in each tab as a parameter
 
 // */
