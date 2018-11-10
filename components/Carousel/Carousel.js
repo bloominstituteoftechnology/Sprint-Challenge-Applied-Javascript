f2f6075f404a089a703784579064cf444e65f50d
mustafa inaya <moose.inaya@icloud.com>
2018-11-10 11:51:31 -0800

    ADD, Added Two Articles(TABS).
    I added two other tabs with content inside of them . I named one "MUSTAFA, LAMBDA".

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,11 +1,29 @@
 class Carousel {
-
+    constructor(mustafa) {
+        this.mustafa = mustafa;
+        this.leftButton = this.mustafa.querySelector('.left-button')
+        this.rightButton = this.mustafa.querySelector('.right-button')
+        this.img = this.mustafa.querySelectorAll(".carousel img")
+        this.leftButton.addEventListener('click', () => {
+            this.toggleMustafa()
+        })
+    }
+    toggleMustafa() {
+        this.leftButton.classList.toggole("left-button")
+    }
+    toggleInaya(){
+        this.rightButton.classList.toggole("right-button")
+    }
+}
+let skims = document.querySelectorAll(".carousel").forEach( skim =>{
+    new Carousel (skim)
 }
 
-let carousel = document.querySelector();
+)
+
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
+    1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
     2. You will need to grab a reference to all of the images
     3. Create a current index
     4. Those buttons are gonna need some click handlers.
