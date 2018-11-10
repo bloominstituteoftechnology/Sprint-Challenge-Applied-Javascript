475eb2f57c3cb249af0c8334be0b66964cf08174
brettmadrid <brettmadrid9@gmail.com>
2018-11-10 10:17:17 -0800

    added click event listener to select tab
    to select for articles.  Allows to select for specific category of
    articles or for all articles

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -16,7 +16,6 @@ class TabLink {
       // else if `all` is false, only select the cards with matching this.tabData values
        this.cards = document.querySelectorAll(`.card[data-tab="${this.tabData}"]`);
     }
-    console.log(this.cards);
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
     this.cards = Array.from(this.cards).map( card => new TabCard(card) );
 
