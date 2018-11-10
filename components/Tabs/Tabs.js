5c328b39f2bcd09cee0190e4fb860a1acf4b9218
iradsanchez <ira.d.sanchez@gmail.com>
2018-11-10 11:17:06 -0700

    fixed selectCard

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -59,7 +59,7 @@ class TabCard {
   }
   selectCard(){
     // Update the style of this.cardElement to display = "flex"
-    this.cardElement = this.cardElement.style.display = 'flex' ;
+    this.cardElement.style.display = 'flex' ;
   }
 
 }
