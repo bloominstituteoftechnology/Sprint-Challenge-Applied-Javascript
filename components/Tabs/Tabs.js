de58631b9ca8f545b14c14f6ac57ca2450f4de9a
brettmadrid <brettmadrid9@gmail.com>
2018-11-10 10:37:05 -0800

    completed selectTab function

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -28,29 +28,34 @@ class TabLink {
   selectTab(){
 
     // Select all elements with the .tab class on them
-    // const tabs = document.querySelectorAll();
+    const tabs = document.querySelectorAll('.tab');
     
     // Iterate through the NodeList removing the .active-tab class from each element
-    // tabs.forEach()
+    tabs.forEach( tab => {
+      tab.classList.remove('active-tab');
+    });
 
     // Select all of the elements with the .card class on them
-    // const cards = ;
+     const cards = document.querySelectorAll('.card');
 
     // Iterate through the NodeList setting the display style each one to 'none'
-    // cards.forEach()
+    cards.forEach( card => {
+      //card.style.display = 'none';
+      card.setAttribute('display','none');
+    })
     
     // Add a class of ".active-tab" to this.tabElement
-    // this.tabElement;
+    this.tabElement.classList.add('active-tab');
   
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class. Just un-comment the code and study what is happening here.
-    // this.cards.forEach(card => card.selectCard());
+    this.cards.forEach(card => card.selectCard());
   }
 }
 
 class TabCard {
   constructor(cardElement){
     // Assign this.cardElement to the cardElement DOM reference
-    // this.cardElement;
+     this.cardElement = cardElement;
   }
   selectCard(){
     // Update the style of this.cardElement to display = "flex"
