b3d3b2a6d76898dbea233ce0823cfcdc421efd2d
Ashley Owens <ash4741@gmail.com>
2018-11-10 14:59:37 -0500

    First Shot at Stetch Question

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,11 +1,42 @@
 class Carousel {
+    constructor(carouselElement){
+        this.carouselElement = carouselElement
+        this.navLeft = this.carouselElement.querySelector('.left-button');
+        this.navRight = this.carouselElement.querySelector('.right-button');
+        this.images = this.carouselElement.querySelectorAll('img');
+
+        this.navLeft.addEventListener('click', (e) => this.navigate(-1));
+        this.navRight.addEventListener('click', (e) => this.navigate(1));
+        this.navigate(0);
+      
+    }
+    navigate(direction){
+        let counter = 0;
+        let carouselLength = this.carouselElement.length;
+        let currentImg = this.images[0];
+
+        currentImg.classList.remove('current');
+               
+        counter += direction;
+        
+        if(direction === -1 && counter < 0){
+            counter = carouselLength - 1;
+        }
+        if(direction === 1 && !this.images[counter]){
+            counter = 0;
+        }
+        currentImg = this.images[counter];
+        currentImg.classList.add('current');
+    }
 
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector('.carousel');
+carousel = new Carousel(carousel);
+
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
+    1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
     2. You will need to grab a reference to all of the images
     3. Create a current index
     4. Those buttons are gonna need some click handlers.
