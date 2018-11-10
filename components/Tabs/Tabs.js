049f674c3e6b4446c0c569efd69f1757936605ef
Alex Bezruchko <alex.bezruchko@gmail.com>
2018-11-10 12:36:04 -0500

    ADD SELECTTAB METHOD

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -32,22 +32,26 @@ class TabLink {
   selectTab(){
 
     // Select all elements with the .tab class on them
-    // const tabs = document.querySelectorAll();
+    const tabs = document.querySelectorAll('.topics .tab');
     
     // Iterate through the NodeList removing the .active-tab class from each element
-    // tabs.forEach()
+    tabs.forEach(tab => {
+      tab.classList.remove('active-tab');
+    })
 
     // Select all of the elements with the .card class on them
-    // const cards = ;
+    const cards = document.querySelectorAll('.cards-container .card');
 
     // Iterate through the NodeList setting the display style each one to 'none'
-    // cards.forEach()
+    cards.forEach(card => {
+      card.style.display = 'none';
+    });
     
     // Add a class of ".active-tab" to this.tabElement
-    // this.tabElement;
+    this.tabElement.classList.toggle('active-tab');
   
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class. Just un-comment the code and study what is happening here.
-    // this.cards.forEach(card => card.selectCard());
+    this.cards.forEach(card => card.selectCard());
   }
 }
 
