4d2dd60385dd109f06d1f64cd8d0b027515c081b
ThomasUtsey <tomutsey@yahoo.com>
2018-11-10 13:08:09 -0500

    Add selectCard method
    Display flex to the cardElement

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -43,10 +43,11 @@ class TabCard {
 //     // Assign this.cardElement to the cardElement DOM reference
     this.cardElement = cardElement;
   }
-//   selectCard(){
-//     // Update the style of this.cardElement to display = "flex"
-//     // this.cardElement;
-//   }
+  selectCard(){
+    // Update the style of this.cardElement to display = "flex"
+    // this.cardElement;
+    this.cardElement.style.display = 'flex';
+  }
 
 // }
 
