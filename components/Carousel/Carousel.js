ef0a967dbcd4291425ddcedc8a3ca854488765c8
Leila <nb.leila10@gmail.com>
2018-11-13 18:12:32 -0600

    Add modifications based on PM feedback
    Modified carousel to slide out before slide in using GSAP

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -11,19 +11,59 @@ class Carousel {
   }
 
   previousPage() {
-    this.images[this.index].classList.remove('box-left'); 
-    this.images[this.index].classList.remove('box-right'); 
-    if(this.index === 0) this.index = this.images.length-1;
-    else this.index --;
-    this.images[this.index].classList.add('box-right');
+      // Keep track of index of image sliding out
+      const  current = this.index;
+  
+      // Get current image out to the left
+      TweenMax.to(this.images[this.index], 0.7, {xPercent:-100, opacity: 0, ease: Power4.easeOut});
+      TweenMax.to(this.images[this.index], 0.7, {xPercent:0, delay:1});
+      setTimeout(() => {
+        // Make current image display: none, after image is out
+        this.images[current].classList.remove('box-show');
+      }, 200);  
+  
+      //Go to previous image
+      if(this.index === 0) this.index = this.images.length-1;
+      else this.index --;
+  
+      //Make previous image show, and slide into view from the right
+      setTimeout(() => {
+        this.images[this.index].classList.add('box-show');  
+        this.images[this.index].style.opacity = 1;
+        this.images[this.index].style.zIndex = -1;
+        TweenMax.from(this.images[this.index], 0.7, {xPercent:100, opacity:0, ease: Power2.easeOut});
+      }, 200);  
   }
  
   nextPage() {
-    this.images[this.index].classList.remove('box-left'); 
-    this.images[this.index].classList.remove('box-right'); 
-    if(this.index === this.images.length-1) this.index = 0;
-    else this.index ++;
-    this.images[this.index].classList.add('box-left');
+          // Keep track of index of image sliding out
+          const  current = this.index;
+  
+          // Get current image out to the left
+          TweenMax.to(this.images[this.index], 0.7, {xPercent:100, opacity: 0, ease: Power4.easeOut});
+          TweenMax.to(this.images[this.index], 0.7, {xPercent:0, delay:0.5});
+          setTimeout(() => {
+            // Make current image display: none, after image is out
+            this.images[current].classList.remove('box-show');
+          }, 200);  
+      
+          //Go to previous image
+          if(this.index === this.images.length-1) this.index = 0;
+          else this.index ++;
+      
+          //Make previous image show, and slide into view from the right
+          setTimeout(() => {
+            this.images[this.index].classList.add('box-show');  
+            this.images[this.index].style.opacity = 1;
+            this.images[this.index].style.zIndex = -1;
+            TweenMax.from(this.images[this.index], 0.7, {xPercent:-100, opacity:0, ease: Power2.easeOut});
+          }, 200);  
+    
+    // this.images[this.index].classList.remove('box-left'); 
+    // this.images[this.index].classList.remove('box-right'); 
+    // if(this.index === this.images.length-1) this.index = 0;
+    // else this.index ++;
+    // this.images[this.index].classList.add('box-left');
   }
 }
 
