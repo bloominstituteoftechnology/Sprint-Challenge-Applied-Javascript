322865370dacb04fd78c11b9386d8b94eac412fc
Tevvin <Tevvin7395@icloud.com>
2018-11-17 13:44:52 -0500

    Finished Tabs.js

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -47,17 +47,17 @@ class TabLink {
   }
 }
 
-//class TabCard {
-//constructor(cardElement) {
-//Assign this.cardElement to the cardElement DOM reference
-//this.cardElement;
-//}
-// selectCard() {
-//Update the style of this.cardElement to display = "flex"
-//this.cardElement;
-//}
+class TabCard {
+  constructor(cardElement) {
+    //Assign this.cardElement to the cardElement DOM reference
+    this.cardElement = cardElement;
+  }
+  selectCard() {
+    //Update the style of this.cardElement to display = "flex"
+    this.cardElement.style.display = 'flex';
+  }
 
-//}
+}
 
 
 
