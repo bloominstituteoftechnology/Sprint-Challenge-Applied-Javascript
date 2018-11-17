d2237320586584103dbb0599227159ddf2d9c5bb
ltann <leeroyleeroy18@gmail.com>
2018-11-17 11:21:18 -0800

    removed console.log to clean up code

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -12,11 +12,9 @@ class TabLink {
     if(this.tabData === 'all'){
       // If `all` is true, select all cards regardless of their data attribute values
       this.cards = document.querySelectorAll('.card[data-tab]');
-      console.log(this.cards)
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
-      this.cards = document.querySelectorAll(`.card[data-tab='${this.tabData}']`);
-      console.log(this.cards)
+      this.cards = document.querySelectorAll(`.card[data-tab='${this.tabData}']`); 
     }
     
      // Map over the newly converted NodeList we just created in our if above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
