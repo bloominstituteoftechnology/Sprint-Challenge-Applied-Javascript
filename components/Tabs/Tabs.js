bfae3c4b508f1ca13e7c725bf24656c64297a449
kjohna <43828763+kjohna@users.noreply.github.com>
2018-12-07 10:06:58 -0700

    TabLink.selectTab(), TabCard constructor, TabCard.selectCard() complete

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -22,40 +22,41 @@ class TabLink {
     this.cards = Array.from(this.cards).map(card => new TabCard(card));
 
     // Add a click event that invokes this.selectTab
-    this.tabElement.addEventListener('click', this.selectTab());
-    console.log(this, this.cards);
+    this.tabElement.addEventListener('click', () => this.selectTab());
+    // console.log(this, this.tabElement);
   }
 
   selectTab(){
-    
+    // console.log("selected: ", this.tabElement)
     // Select all elements with the .tab class on them
-    // const tabs = document.querySelectorAll();
+    const tabs = document.querySelectorAll('.tab');
     
     // Iterate through the NodeList removing the .active-tab class from each element
-    // tabs.forEach()
+    tabs.forEach(tab => tab.classList.remove('active-tab'));
 
     // Select all of the elements with the .card class on them
-    // const cards = ;
+    const cards = document.querySelectorAll('.card');
 
     // Iterate through the NodeList setting the display style each one to 'none'
-    // cards.forEach()
+    cards.forEach(card => card.style.display = 'none');
     
     // Add a class of ".active-tab" to this.tabElement
-    // this.tabElement;
+    this.tabElement.classList.add('active-tab');
   
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class. Just un-comment the code and study what is happening here.
-    // this.cards.forEach(card => card.selectCard());
+    this.cards.forEach(card => card.selectCard());
   }
 }
 
 class TabCard {
   constructor(cardElement){
     // Assign this.cardElement to the cardElement DOM reference
-    // this.cardElement;
+    this.cardElement = cardElement;
   }
   selectCard(){
     // Update the style of this.cardElement to display = "flex"
-    // this.cardElement;
+    console.log("card selected:", this.cardElement)
+    this.cardElement.style.display = 'flex';
   }
 
 }
