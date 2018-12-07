e90962ca70c0e0f26a48019f549ab1e1b5e88557
Henry Neal <henry.neal0@gmail.com>
2018-12-07 13:04:57 -0500

    still hung up on a console error not sure how to resolve at the moment

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -9,17 +9,17 @@ class TabLink {
     // We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task:    
     
     // Check to see if this.tabData is equal to 'all'
-    if(this.tabData === 'ALL' ){
+    if(this.tabData === 'All' ){
       // If `all` is true, select all cards regardless of their data attribute values
-      this.cards = this.tabData;
+      this.cards = this.cardElement.dataset.tab;
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
-      this.cards = document.querySelector(`cards[data-tab="${this.tabData}"]`);
+      this.cards = document.querySelector(`.cards[data-tab="${this.tabData}"]`);
     }
     
 
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
-    this.cards = Array.from(cards).map(new TabItem(this.cards));
+    this.cards = Array.from(this.cards).map(card => new TabCard(card));
 
     // Add a click event that invokes this.selectTab
     this.tabElement.addEventListener('click',() => {this.selectTab();});
@@ -34,7 +34,7 @@ class TabLink {
     tabs.forEach(tab => { tab.classList.remove('active-tab')});
 
     // Select all of the elements with the .card class on them
-    const cards = querySelectorAll('.card');
+    const cards = document.querySelectorAll('.card');
 
     // Iterate through the NodeList setting the display style each one to 'none'
     cards.forEach().style.display = 'none'
@@ -68,4 +68,5 @@ class TabCard {
 - In your .forEach() method's callback function, return a new instance of TabLink and pass in each tab as a parameter
 
 */
-let tabs = document.querySelectorAll('.tab').forEach(tab => new TabLink(tab));
+let tabs = document.querySelectorAll('.tab');
+tabs.forEach(tabElement => new TabLink(tabElement));
