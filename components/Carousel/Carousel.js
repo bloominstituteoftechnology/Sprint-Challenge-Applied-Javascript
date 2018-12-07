510f0e981e53d588da6a9eb57e6d3e211dd85cc1
mcferrenm <mcferrenmax@gmail.com>
2018-12-07 12:34:23 -0500

    Infinite loop on Carousel component

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,7 +1,47 @@
 class Carousel {
     constructor(domElement) {
         this.element = domElement;
-        console.log(this.element)
+
+        this.leftBtn = document.querySelector('.left-button');
+        this.rightBtn = document.querySelector('.right-button');
+
+        this.images = document.querySelectorAll('.carousel img');
+
+        this.currentIndex = 0;
+
+        this.images[0].style.display = 'block';
+
+        this.leftBtn.addEventListener('click', () => {
+            this.images.forEach(image => image.style.display = 'none');
+
+            this.currentIndex -= 1;
+
+            if(this.currentIndex < 0) {
+                this.currentIndex = 3;
+            }
+
+            this.images.forEach((image, index) => {
+                if(this.currentIndex === index) {
+                    image.style.display = 'block';
+                }
+            })
+        })
+        
+        this.rightBtn.addEventListener('click', () => {
+            this.images.forEach(image => image.style.display = 'none');
+
+            this.currentIndex += 1;
+
+            if(this.currentIndex > 3) {
+                this.currentIndex = 0;
+            }
+
+            this.images.forEach((image, index) => {
+                if(this.currentIndex === index) {
+                    image.style.display = 'block';
+                }
+            })
+        })
     }
 }
 
