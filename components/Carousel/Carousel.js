e9fc09a9b4b5df37856b529fd431948b5bf460d1
Matt Basile <matthewbasile2@gmail.com>
2018-12-07 13:12:27 -0500

    Able to store the carousel in a Class so that we can now create multipleseemlessly. LINES: carousel.js 2-28

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,42 +1,46 @@
 class Carousel {
+    constructor(carousel){
+        this.rightBtn = document.querySelector(".right-button");
+        this.leftBtn = document.querySelector(".left-button");
+        this.slides = document.querySelectorAll('.caro-img');
 
+        this.rightBtn.addEventListener('click', ()=>{this.plusSlides(1)});
+        this.leftBtn.addEventListener('click', ()=>{this.plusSlides(-1)});
+        this.slideIndex = 1;
+        this.showSlides(this.slideIndex);
+    }
+    plusSlides(n){
+        this.showSlides(this.slideIndex += n);
+    }
+    currentSlide(n){
+        showSlides(slideIndex = n);
+    }
+    showSlides(n){
+        let i;
+        let slides = document.querySelectorAll(".caro-img");
+        if (n > slides.length) {this.slideIndex = 1} 
+        if (n < 1) {this.slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none"; 
+        }
+        slides[this.slideIndex-1].style.display = "block";  
+    }
  }
  
 
-let slides = document.querySelectorAll('.caro-img');
-new Carousel(slides);
+
+
 
 
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
+    1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
     2. You will need to grab a reference to all of the images
     3. Create a current index
     4. Those buttons are gonna need some click handlers.
     5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
     6. Have fun!
 */
+const carousel = document.querySelector(".carousel");
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.querySelectorAll(".caro-img");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
-  }
-  slides[slideIndex-1].style.display = "block"; 
-}
+new Carousel(carousel);
