3e97201957a5f4246746160cbb655cd2261ca430
Lenna <lenna@lennamau.com>
2018-12-07 11:20:58 -0700

    Sprint task 3 complete

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,14 +1,20 @@
 class Carousel {
+    constructor(element);
+    
 
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector('.carousel .left-button .right-button');
+let images = document.querySelectorAll('.image');
 
-/* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
+/* If You've gotten this far, you're on your own! Although we will give you
+ some hints:
+    1. You will need to grab a reference to the carousel, and in it grab
+     the laft and right buttons
     2. You will need to grab a reference to all of the images
     3. Create a current index
     4. Those buttons are gonna need some click handlers.
-    5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
+    5. Think of how you would animate this compoennt. Make the cards
+     slide in and out, or fade. It's up to you!
     6. Have fun!
 */
