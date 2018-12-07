17630e94775a9708c4a867ca76581a01baf0b921
RachelKolk <rach.m.obrien@gmail.com>
2018-12-07 12:12:49 -0500

    added a few new articles to the html and started on stretch

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -2,7 +2,7 @@ class Carousel {
 
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector('.carousel');
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
