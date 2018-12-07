48388ee2ee2f49b3edf12476165485954dfb7a0d
Jacob Bryan <jtb36688@gmail.com>
2018-12-07 11:46:23 -0500

    Tabs70%

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -26,22 +26,22 @@ class TabLink {
   selectTab(){
 
     // Select all elements with the .tab class on them
-    // const tabs = document.querySelectorAll();
+    const tabs = document.querySelectorAll('.tab');
     
     // Iterate through the NodeList removing the .active-tab class from each element
-    // tabs.forEach()
+    tabs.forEach(item => item.classList.remove('active-tab'))
 
     // Select all of the elements with the .card class on them
-    // const cards = ;
+    const cards = querySelectorAll(document.querySelectorAll('.card'));
 
     // Iterate through the NodeList setting the display style each one to 'none'
-    // cards.forEach()
+    cards.forEach(item => item.style.display = 'none')
     
     // Add a class of ".active-tab" to this.tabElement
-    // this.tabElement;
+    this.tabElement.classList.add('.active-tab');
   
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class. Just un-comment the code and study what is happening here.
-    // this.cards.forEach(card => card.selectCard());
+    this.cards.forEach(card => card.selectCard());
   }
 }
 
