7f8383e743a83b567247c34d08fda37cdebbd0a7
Kingaa6 <Kingaa6@gmail.com>
2018-12-07 11:57:04 -0800

    Carousel refactor

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -2,22 +2,20 @@ class Carousel {
     constructor(carousel) {
         this.carousel = carousel;
         this.carouselImage = document.querySelectorAll('.carousel-image');
+        this.leftButton = document.querySelector('.left-button');
+        this.rightButton = document.querySelector('.right-button');
+
         console.log(this.carouselImage);
 
-        // this.data = this.carouselImage.dataset.tab;
+        // this.data = this.carouselImage.dataset.img;
         // this.imageDataPoint = document.querySelectorAll(`.carousel-image[data-tab='${this.data}']`)
         // this.carouselImageDataPoint = new CarouselImage(this.imageDataPoint);
         // console.log(this.carouselImageDataPoint);
 
         this.carouselImage[0].style.display = 'block';
 
-
-        this.leftButton = document.querySelector('.left-button');
-        this.rightButton = document.querySelector('.right-button');
-
         this.leftButton.addEventListener('click', () => this.scrollLeft());
         this.rightButton.addEventListener('click', () => this.scrollRight());
-
 
     }
 
@@ -35,7 +33,7 @@ class CarouselImage {
         this.image = image;
     }
     selectImage() {
-        this.image.style.display = 'flex';
+        this.image.style.display = 'block';
     }
 }
 
