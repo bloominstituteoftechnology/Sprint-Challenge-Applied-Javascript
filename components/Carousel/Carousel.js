87701fd35918f51dc547ff52229ea5ee15ca3315
stellarluminant <stellarluminant@gmail.com>
2018-12-07 10:10:41 -0800

    Add carousel animation
    Used GSAP TweenLite for the first time, and it's way easier than I thought it'd be~.

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -3,41 +3,26 @@ class Carousel {
         this.element = element;
         this.leftButton = this.element.querySelector('.left-button');
         this.rightButton = this.element.querySelector('.right-button');
-        this.images = this.element.querySelectorAll('img');
-        this.index = 0;
-        this.lastIndex = 0;
+        this.imgContainer = this.element.querySelector('.carousel-imgs');
+        this.imgCount = this.imgContainer.querySelectorAll('img').length;
 
-        this.images.forEach(elem => elem.classList.remove('current-img'));
-        this.images[0].classList.add('current-img');
+        this.imgContainer.style.width = `${this.imgCount} * 100}%`;
+        this.index = 0;
 
         this.leftButton.addEventListener('click', _ => this.scroll(-1));
         this.rightButton.addEventListener('click', _ => this.scroll(+1));
     }
 
     scroll(increment) {
-        this.lastIndex = this.index;
         this.index += increment;
-        this.index = this.index % this.images.length;
+        this.index = this.index % this.imgCount;
         if (this.index < 0) {
-            this.index += this.images.length;
+            this.index += this.imgCount;
         }
         
-        this.instantTransition();
-    }
-
-    instantTransition() {
-        this.images.forEach(elem => elem.classList.remove('current-img'));
-        this.images[this.index].classList.add('current-img');
+        let position = (this.index === 0) ? '0%' : `-${this.index * 100}%`;
+        TweenLite.to(this.imgContainer, 0.4, { left: position });
     }
 }
 
 let carousel = new Carousel(document.querySelector('.carousel'));
-
-/* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
-    2. You will need to grab a reference to all of the images
-    3. Create a current index
-    4. Those buttons are gonna need some click handlers.
-    5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
-    6. Have fun!
-*/
