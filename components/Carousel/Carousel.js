ab320b988d50cdc4a6b79864e36a206a0d0a65e9
bhumip214 <bhumip214@gmail.com>
2018-12-07 13:50:26 -0500

    created slideBack method

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -23,16 +23,24 @@ class Carousel {
     console.log("slide next clicked");
     this.images[this.currentImageIndex].style.display = "none";
     this.currentImageIndex++;
-    // 4 === 4, reset to 0
+
+    // at the end of all images, reset to 0
     if (this.currentImageIndex === this.images.length) {
       this.currentImageIndex = 0;
     }
+
     this.images[this.currentImageIndex].style.display = "block";
   }
+
   slideBack() {
     console.log("slide back clicked");
     this.images[this.currentImageIndex].style.display = "none";
     this.currentImageIndex--;
+
+    // if at the first image go to the last image
+    if (this.currentImageIndex === -1) {
+      this.currentImageIndex = this.images.length - 1;
+    }
     this.images[this.currentImageIndex].style.display = "block";
   }
 }
