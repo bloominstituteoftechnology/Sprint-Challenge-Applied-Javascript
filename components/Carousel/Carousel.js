03cd7354b412d9a4bbb6038d50b2a768f4d04135
matt0418 <mfeldma7@gmail.com>
2018-12-07 12:36:59 -0500

    Failed attempt at carrousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,38 @@
-class Carousel {
+// class Carousel {
+//     constructor(carouselElement){
+//         this.carouselElement = carouselElement;
 
+//     }
+// }
+
+// let carousel = document.querySelector('.carousel');
+// let carouselImg = document.querySelectorAll('.car-img')
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
 }
 
-let carousel = document.querySelector();
+function currentSlide(n) {
+    showSlides(slideIndex = n)
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.querySelectorAll('.car-img')
+    if (n > slides.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slides[slideIndex-1].style.display = "block"; 
+  }
+
+
+
+
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
