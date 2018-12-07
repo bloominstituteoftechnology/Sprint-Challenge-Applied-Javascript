be7a82037cde17786e706679de941675681e77f1
Matt Basile <matthewbasile2@gmail.com>
2018-12-07 13:28:37 -0500

    Added a nice Fade animation to my slides using GSAP. LINES: carousel.js 27-37.

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,9 +1,12 @@
 class Carousel {
     constructor(carousel){
+        // Right Button
         this.rightBtn = document.querySelector(".right-button");
+        // Left Button
         this.leftBtn = document.querySelector(".left-button");
+        //Slides
         this.slides = document.querySelectorAll('.caro-img');
-
+        //Button Events
         this.rightBtn.addEventListener('click', ()=>{this.plusSlides(1)});
         this.leftBtn.addEventListener('click', ()=>{this.plusSlides(-1)});
         this.slideIndex = 1;
@@ -21,24 +24,26 @@ class Carousel {
         if (n > slides.length) {this.slideIndex = 1} 
         if (n < 1) {this.slideIndex = slides.length}
         for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none"; 
+            TweenMax.to(slides[i], 0, {
+                display:'none',
+                opacity: '.4'
+            })
+            // slides[i].style.display = "none"; 
         }
-        slides[this.slideIndex-1].style.display = "block";  
+        TweenMax.to(slides[this.slideIndex-1], 1, {
+            display: "block",
+            opacity: '1'
+        })
+        // slides[this.slideIndex-1].style.display = "block";  
     }
  }
- 
-
-
-
-
-
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
     2. You will need to grab a reference to all of the images
     3. Create a current index
     4. Those buttons are gonna need some click handlers.
-    5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
+    5. Think of how you would animate this compoent. Make the cards slide in and out, or fade. It's up to you!
     6. Have fun!
 */
 const carousel = document.querySelector(".carousel");
