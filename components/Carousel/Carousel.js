5120c3b1f889fdb4d3515b63e9878b474fc95ef0
Brandon Lent <blent1050@yahoo.com>
2018-12-07 13:20:27 -0500

    Carousel Functionality completed

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,32 +1,41 @@
-
-
-
 class Carousel {
-    constructor(carousel){
-        this.carousel = carousel;
-        this.btnData = this.carousel.dataset.btn;
-        this.imgData = this.carousel.dataset.pos;
-        console.log(this.btnData)
-        console.log(this.imgData);
-        //grabs the img attached to carousel
-        this.img = document.querySelectorAll('.carousel img');
-        //grabs the buttons attached to carousel
-        this.btn = document.querySelectorAll
-        
+    constructor(carouselElement) {
+        this.carouselElement = carouselElement;
+        //Store the current index in a variable
+        this.currentIndex = 0;
+        //Get the left button
+        this.leftButton = document.querySelector(".left-button");
+        //Get the right button
+        this.rightButton = document.querySelector(".right-button");
+        //Gets all images in the carousel
+        this.images = document.querySelectorAll(".carousel img");
+        this.leftButton.addEventListener("click", event => this.clickedLeft(event));
+        this.rightButton.addEventListener("click", event => this.clickedRight(event));
     }
-    //Methods
-
+    clickedRight(event) {
+        this.images[this.currentIndex].style.display = "none";
+        this.currentIndex = this.currentIndex + 1 === this.images.length ?
+            0 : this.currentIndex + 1;
+        this.images[this.currentIndex].style.display = "block";
+        TweenMax.from(this.images, 0.5, {
+            x: 400,
+            opacity: 0.1
+          });
+    }
+    clickedLeft(event) {
+        this.images[this.currentIndex].style.display = "none";
+        
+        this.currentIndex = this.currentIndex ?
+            this.currentIndex - 1 : this.images.length - 1;
+        this.images[this.currentIndex].style.display = "block";
+        TweenMax.from(this.images, 0.5, {
+            x: -400,
+            opacity: 0.1
+          });
+    }
 }
 
-let carousel = document.querySelector('.carousel')
-carousel = new Carousel(carousel);
 
+let carousel = document.querySelectorAll('.carousel');
+new Carousel(carousel);
 
-/* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
-    2. You will need to grab a reference to all of the images
-    3. Create a current index
-    4. Those buttons are gonna need some click handlers.
-    5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
-    6. Have fun!
-*/
