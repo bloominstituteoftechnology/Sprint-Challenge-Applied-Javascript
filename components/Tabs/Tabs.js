b847efb5251e684f64aea056c6862eebd864c748
Raine Wallace <rainebeaudubs@gmail.com>
2018-12-07 12:32:16 -0500

    Website is functioning properly.

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -26,45 +26,46 @@ class TabLink {
     
 
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
-    // this.cards = Array.from(this.cards).map();
+    this.cards = Array.from(this.cards).map(card => 
+      new TabCard(card));
 
     // Add a click event that invokes this.selectTab
-    // this.tabElement.addEventListener();
+    this.tabElement.addEventListener('click', () => this.selectTab());
   }
 
-  // selectTab(){
+  selectTab(){
 
     // Select all elements with the .tab class on them
-    // const tabs = document.querySelectorAll();
+    const tabs = document.querySelectorAll('.tab');
     
     // Iterate through the NodeList removing the .active-tab class from each element
-    // tabs.forEach()
+    tabs.forEach(card => card.classList.remove('active.tab'));
 
     // Select all of the elements with the .card class on them
-    // const cards = ;
+    const cards = document.querySelectorAll('.card');
 
     // Iterate through the NodeList setting the display style each one to 'none'
-    // cards.forEach()
+    cards.forEach(card => card.style.display = 'none');
     
     // Add a class of ".active-tab" to this.tabElement
-    // this.tabElement;
+    this.tabElement.classList.add('active-tab');
   
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class. Just un-comment the code and study what is happening here.
-    // this.cards.forEach(card => card.selectCard());
-  // }
+    this.cards.forEach(card => card.selectCard());
+  }
 }
 
-// class TabCard {
-//   constructor(cardElement){
-//     // Assign this.cardElement to the cardElement DOM reference
-//     // this.cardElement;
-//   }
-//   selectCard(){
-//     // Update the style of this.cardElement to display = "flex"
-//     // this.cardElement;
-//   }
+class TabCard {
+  constructor(cardElement){
+    // Assign this.cardElement to the cardElement DOM reference
+    this.cardElement = cardElement;
+  }
+  selectCard(){
+    // Update the style of this.cardElement to display = "flex"
+    this.cardElement.style.display = 'flex';
+  }
 
-// }
+}
 
 /* START HERE: 
 
