9bf423343a5484a610aa93255bdd1df51f62eb90
EJaona <ejaona@gmail.com>
2018-12-07 13:12:07 -0600

    assigned the passed in tab value at TabLink to `tabElement`

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,7 +1,7 @@
 class TabLink {
   constructor(tabElement) {
     // assign this.tabElement to the tabElement DOM reference
-    //this.tabElement;
+    this.tabElement = tabElement;
     // Get the `data-tab` value from this.tabElement and store it here
     //this.tabData;
     // We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task:
