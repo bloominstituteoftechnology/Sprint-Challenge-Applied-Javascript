c2cdbb1415550ddc6dceb9a332e5d05d346472b9
EJaona <ejaona@gmail.com>
2018-12-07 13:17:36 -0600

    set up event listener to trigger `SelectTab` method on tabElement click

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -21,8 +21,7 @@ class TabLink {
     this.cards = Array.from(this.cards).map(card => new TabCard(card));
 
     // Add a click event that invokes this.selectTab
-    //this.tabElement.addEventListener();
-  }
+    this.tabElement.addEventListener("click", () => this.selectTab());
 
   selectTab() {
     // Select all elements with the .tab class on them
