c15e19960639642d85aab468550e425c06a4430f
cori-peck <cori.peck@ymail.com>
2018-12-07 13:02:35 -0600

    Basically thought about the Stretch carousel and not much more

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -2,7 +2,7 @@ class Carousel {
 
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector('.carousel');
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
@@ -12,3 +12,4 @@ let carousel = document.querySelector();
     5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
     6. Have fun!
 */
+
