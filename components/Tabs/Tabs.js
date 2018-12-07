2bca13d43473290a8f59680998924c21face786c
EJaona <ejaona@gmail.com>
2018-12-07 13:25:39 -0600

    completed objectives

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -4,6 +4,7 @@ class TabLink {
     this.tabElement = tabElement;
     // Get the `data-tab` value from this.tabElement and store it here
     this.tabData = this.tabElement.dataset.tab;
+
     // We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task:
 
     // Check to see if this.tabData is equal to 'all'
@@ -22,32 +23,36 @@ class TabLink {
 
     // Add a click event that invokes this.selectTab
     this.tabElement.addEventListener("click", () => this.selectTab());
+  }
 
   selectTab() {
     // Select all elements with the .tab class on them
-      const tabs = document.querySelectorAll(".tab");
+    const tabs = document.querySelectorAll(".tab");
+
     // Iterate through the NodeList removing the .active-tab class from each element
-      tabs.forEach(tab => tab.classList.remove("active-tab"));
+    tabs.forEach(tab => tab.classList.remove("active-tab"));
     // Select all of the elements with the .card class on them
-      const cards = document.querySelectorAll(".card");
+    const cards = document.querySelectorAll(".card");
 
     // Iterate through the NodeList setting the display style each one to 'none'
-      cards.forEach(card => (card.style.display = "none"));
+    cards.forEach(card => (card.style.display = "none"));
+
     // Add a class of ".active-tab" to this.tabElement
-      this.tabElement.classList.add("active-tab");
+    this.tabElement.classList.add("active-tab");
+
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class. Just un-comment the code and study what is happening here.
-      this.cards.forEach(card => card.selectCard());
+    this.cards.forEach(card => card.selectCard());
   }
 }
-/*
+
 class TabCard {
   constructor(cardElement) {
     // Assign this.cardElement to the cardElement DOM reference
-    //this.cardElement;
+    this.cardElement = cardElement;
   }
   selectCard() {
     // Update the style of this.cardElement to display = "flex"
-    //this.cardElement;
+    this.cardElement.style.display = "flex";
   }
 }
 
