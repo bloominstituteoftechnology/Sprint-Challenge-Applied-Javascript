ef8c95d6c2f57d29745ddccf1e7ca727a147829d
matt0418 <mfeldma7@gmail.com>
2018-12-07 12:37:45 -0500

    Minor change

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -8,29 +8,6 @@
 // let carousel = document.querySelector('.carousel');
 // let carouselImg = document.querySelectorAll('.car-img')
 
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n)
-}
-
-function showSlides(n) {
-    let i;
-    let slides = document.querySelectorAll('.car-img')
-    if (n > slides.length) {slideIndex = 1} 
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; 
-    }
-    slides[slideIndex-1].style.display = "block"; 
-  }
-
-
 
 
 
