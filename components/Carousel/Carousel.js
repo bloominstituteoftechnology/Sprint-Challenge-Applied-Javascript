6c95f8e5dff1da004981b25ee1b175b842651827
stellarluminant <stellarluminant@gmail.com>
2018-12-07 09:40:03 -0800

    Complete carousel functionality
    - Carousel basic functionality, no animation.
    - Infinitely scrolls (loops back to starting image).
    - Slight style fixes on the buttons.
    - Added a "noSelect" mixin which prevents the carousel from being highlighted.
    - Added the compiled CSS so that it will run out of the box.

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,37 @@
 class Carousel {
+    constructor (element) {
+        this.element = element;
+        this.leftButton = this.element.querySelector('.left-button');
+        this.rightButton = this.element.querySelector('.right-button');
+        this.images = this.element.querySelectorAll('img');
+        this.index = 0;
+        this.lastIndex = 0;
 
+        this.images.forEach(elem => elem.classList.remove('current-img'));
+        this.images[0].classList.add('current-img');
+
+        this.leftButton.addEventListener('click', _ => this.scroll(-1));
+        this.rightButton.addEventListener('click', _ => this.scroll(+1));
+    }
+
+    scroll(increment) {
+        this.lastIndex = this.index;
+        this.index += increment;
+        this.index = this.index % this.images.length;
+        if (this.index < 0) {
+            this.index += this.images.length;
+        }
+        
+        this.instantTransition();
+    }
+
+    instantTransition() {
+        this.images.forEach(elem => elem.classList.remove('current-img'));
+        this.images[this.index].classList.add('current-img');
+    }
 }
 
-let carousel = document.querySelector();
+let carousel = new Carousel(document.querySelector('.carousel'));
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
