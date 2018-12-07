c8713c658ac5ae261cf58863223ad13c4a1957eb
yannafaith <yannafaith97@gmail.com>
2018-12-07 10:05:58 -0700

    filled in TabCard class

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -53,25 +53,26 @@ class TabLink {
     }
   }
 
-class TabCard {
-  constructor(cardElement){
-    // Assign this.cardElement to the cardElement DOM reference
-    // this.cardElement;
+
+  class TabCard {
+    constructor(cardElement){
+      // Assign this.cardElement to the cardElement DOM reference
+      this.cardElement = cardElement;
+    }
+    selectCard(){
+      // Update the style of this.cardElement to display = "flex"
+      this.cardElement.style.display = 'flex';
+    }
+  
   }
-  selectCard(){
-    // Update the style of this.cardElement to display = "flex"
-    // this.cardElement;
-  }
-
-}
-
-/* START HERE: 
-
-- Select all classes named ".tab" and assign that value to the tabs variable
-
-- With your selection in place, now chain a .forEach() method onto the tabs variable to iterate over the DOM NodeList
-
-- In your .forEach() method's callback function, return a new instance of TabLink and pass in each tab as a parameter
-
-*/
-let tabs = document.querySelectorAll();
+  
+  /* START HERE: 
+  
+  - Select all classes named ".tab" and assign that value to the tabs variable
+  
+  - With your selection in place, now chain a .forEach() method onto the tabs variable to iterate over the DOM NodeList
+  
+  - In your .forEach() method's callback function, return a new instance of TabLink and pass in each tab as a parameter
+  
+  */
+  let tabs = document.querySelectorAll('.tab').forEach(tab => new TabLink(tab));
