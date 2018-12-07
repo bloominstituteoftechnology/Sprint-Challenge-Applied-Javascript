49479b970cf46af226c1820aa86e28379a454af4
spinon187 <spinon187@gmail.com>
2018-12-07 12:59:30 -0600

    SCIV incomplete stretch

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,47 @@
 class Carousel {
-
+    constructor(element){
+        this.element = element;
+        this.left = document.querySelector(`.left-button`);
+        this.right = document.querySelector(`.right-button`);
+        this.image = document.querySelectorAll(`img`);
+        this.image[1].style.display = 'block';
+        this.image[2].style.display = 'block';
+        this.disp = [1,2];
+        this.left.addEventListener('click', () => this.cycleLeft());
+        this.right.addEventListener('click', () => this.cycleRight());
+    }
+    cycleLeft(){
+        this.image.forEach(x => x.style.display = 'none');
+        const count = Array.from(this.image).length;
+        if(this.disp[0] > 0){
+            this.disp[0] = this.disp[0] - 1; 
+        }
+        else{
+            this.disp[0] = count;
+        }
+        this.disp[1] = this.disp[1] - 1;
+        this.image[this.disp[0]].style.display = 'block';
+        this.image[this.disp[1]].style.display = 'block';
+        console.log('left');
+    }
+    cycleRight(){
+        this.image.forEach(x => x.style.display = 'none');
+        const count = Array.from(this.image).length;
+        if(this.disp[1] < count){
+            this.disp[1] += 1; 
+        }
+        else{
+            this.disp[0] +=1;
+        }
+        this.disp[1] += 1;
+        this.image[this.disp[0]].style.display = 'block';
+        this.image[this.disp[1]].style.display = 'block';
+        console.log('right');
+    }
 }
 
-let carousel = document.querySelector();
+
+let carousel = document.querySelectorAll('.carousel').forEach(x => new Carousel(x));
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
