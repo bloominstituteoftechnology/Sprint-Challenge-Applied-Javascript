6277bcb72b4484345f54b4a506f55c24d73a94c2
Jacob Bryan <jtb36688@gmail.com>
2018-12-07 13:58:05 -0500

    BuggyCarouselButWorks

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -2,35 +2,40 @@ class Carousel {
     constructor(carousel) {
         this.carousel = carousel;
         this.images = document.querySelectorAll('.carousel img')
-        this.imagesmap = Array.from(this.images).map(item => new CarouselImg(item));
+        this.imagesmap = Array.from(this.images)
         this.current = this.images[0];
         this.current.style.display = 'inline-block';
-        // if (this.carousel.classList.contains('left-button')) {
-        
-        // };
+        this.carousel.addEventListener('click', () => {this.selectArrow()}
+        );
+    }
+    selectArrow() {
+        if (this.carousel.classList.contains('left-button')) {
+        console.log('leftbutton');
+        this.current.style.display = 'none';
+        this.imagesmap.unshift(this.imagesmap.pop());
+        this.current = this.imagesmap[0];
+        this.current.style.display = 'inline-block';
+        }
 
-        // else if (this.carousel.classList.contains('right-button') {
-
-        // };
-        
-        // this.carousel.addEventListener('click', function() {
-
-        // };
-
-        // )
-        
+        else if (this.carousel.classList.contains('right-button')) {
+        console.log('rightbutton')
+        this.current.style.display = 'none';
+        this.imagesmap.push(this.imagesmap.shift());
+        this.current = this.imagesmap[0];
+        this.current.style.display = 'inline-block';
+        };
     }
 }
 
-class CarouselImg {
-    constructor(imgElement) {
-    this.imgElement = imgElement;
-}
-    selectImg() {
-    console.log('testing')
-    this.imgElement.style.display = 'inline-block';
-    }
-}
+// class CarouselImg {
+//     constructor(imgElement) {
+//     this.imgElement = imgElement;
+// }
+//     selectImg() {
+//     console.log('testing')
+//     this.imgElement.style.display = 'inline-block';
+//     }
+// }
 
 let carousel = document.querySelectorAll('.carousel, .left-button, .right-button');
 carousel.forEach(item => new Carousel(item));
