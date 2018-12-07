12e424c0bc7dcc556b57191fa8ad446fdc317d45
EJaona <ejaona@gmail.com>
2018-12-07 13:21:25 -0600

    assigning cards class to cards var and looping over to set display prop to  none for each card

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -29,9 +29,10 @@ class TabLink {
     // Iterate through the NodeList removing the .active-tab class from each element
       tabs.forEach(tab => tab.classList.remove("active-tab"));
     // Select all of the elements with the .card class on them
-    //const cards;
+      const cards = document.querySelectorAll(".card");
+
     // Iterate through the NodeList setting the display style each one to 'none'
-    //cards.forEach(card => );
+      cards.forEach(card => (card.style.display = "none"));
     // Add a class of ".active-tab" to this.tabElement
     //this.tabElement;
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class. Just un-comment the code and study what is happening here.
