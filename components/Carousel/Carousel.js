ef9874890855e667f1846a069e3b2e2dffaed3ba
Imon Ovbude <imonovbude@gmail.com>
2018-12-07 10:27:00 -0800

    uncommented carousel element, uncommented carousel script tag in html, referenced the carousel element to carousel variable in carousel.js page

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -2,13 +2,19 @@ class Carousel {
 
 }
 
-let carousel = document.querySelector();
-
+let carousel = document.querySelector(".carousel");
+console.log(carousel)
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
+    1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
+
     2. You will need to grab a reference to all of the images
+
     3. Create a current index
+
     4. Those buttons are gonna need some click handlers.
+
     5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
+
     6. Have fun!
 */
+
