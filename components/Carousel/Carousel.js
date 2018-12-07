68e652bccfa0848bff0575b4efad4c703440746d
samjeffcoat <samjeffcoat@gmail.com>
2018-12-07 12:37:17 -0600

    couldnt figure out carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,14 @@
 class Carousel {
+    constructor (element){
+        this.carousel=element;
+        this.leftbutton=element.leftbutton;
+        this.rightbutton=element.rightbutton;
+        
 
+    }
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector('.carousel');
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
