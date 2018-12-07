0b9f0de8dcd105232fdad324b40fe2178553b543
Henry Neal <henry.neal0@gmail.com>
2018-12-07 11:31:58 -0500

    working on if statement in tabElement

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,10 +1,10 @@
 class TabLink {
   constructor(tabElement){
     // assign this.tabElement to the tabElement DOM reference
-    // this.tabElement;
+    this.tabElement = tabElement;
     
     // Get the `data-tab` value from this.tabElement and store it here
-    // this.tabData = ; 
+    this.tabData = this.tabElement.dataset.tab; 
     
     // We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task:    
     /*
@@ -16,7 +16,7 @@ class TabLink {
       // else if `all` is false, only select the cards with matching this.tabData values
       // this.cards = ;
     }
-    /*
+    /*]]
 
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
     // this.cards = Array.from(this.cards).map();
@@ -68,4 +68,5 @@ class TabCard {
 - In your .forEach() method's callback function, return a new instance of TabLink and pass in each tab as a parameter
 
 */
-let tabs = document.querySelectorAll();
+
+let tabs = document.querySelectorAll('.tab').forEach(tab => new TabLink(link));
