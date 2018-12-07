6ba8326c4eaf419b228260dc050d9ae27ae18418
KieranVieira <kieranvieira@live.com>
2018-12-07 12:05:10 -0500

    completed Carousel Stretch question

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,34 @@
 class Carousel {
-
+    constructor(carousel){
+        this.element = carousel;
+        this.currentIndex = 1;
+        this.data = this.element.dataset.position;
+        this.leftBTN = this.element.querySelector('.left-button');
+        this.rightBTN = this.element.querySelector('.right-button');
+        this.leftBTN.addEventListener('click', () => this.left())
+        this.rightBTN.addEventListener('click', () => this.right())
+    }
+    right(){
+        this.currentIndex +=1;
+        if(this.currentIndex >= 5){
+            this.currentIndex = 1
+        }
+        this.element.querySelectorAll('.slider').forEach((cv)=>{cv.classList.toggle('active-slider',false)})
+        this.element.querySelector(`.slider[data-position='${this.currentIndex}']`).classList.toggle('active-slider',true)
+    }
+    left(){
+        this.currentIndex -=1;
+        if(this.currentIndex <= 0){
+            this.currentIndex = 4
+        }
+        this.element.querySelectorAll('.slider').forEach((cv)=>{cv.classList.toggle('active-slider',false)})
+        this.element.querySelector(`.slider[data-position='${this.currentIndex}']`).classList.toggle('active-slider',true)
+    }
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector('.carousel');
+
+new Carousel(carousel)
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
@@ -12,3 +38,5 @@ let carousel = document.querySelector();
     5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
     6. Have fun!
 */
+
+//These hints were very helpful^^
