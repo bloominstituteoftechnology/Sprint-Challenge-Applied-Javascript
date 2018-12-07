393af86599d5fa75d14b25bfce3ce3e0a9bd082e
BrannanC <brannanconrad@gmail.com>
2018-12-07 11:03:24 -0600

    Added carousel. I feel really accomplished

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,9 +1,38 @@
 class Carousel {
+    constructor(carElement){
+        this.element = carElement;
+        this.left = this.element.querySelector('.left-button');
+        this.right = this.element.querySelector('.right-button');
+        this.imgs = this.element.querySelectorAll('img');
+        this.numImgs = this.imgs.length;
+        this.currentIndex = 0;
+        this.imgs[0].style.display = "block";
+        this.left.addEventListener('click', () => this.leftClick());
+        this.right.addEventListener('click', () => this.rightClick());
+    }
 
+    leftClick(){
+        if(this.currentIndex === 0){
+            this.currentIndex = this.numImgs - 1;
+        } else {
+            this.currentIndex--;
+        }
+        this.imgs.forEach(el => el.style.display = 'none');
+        this.imgs[this.currentIndex].style.display = 'block';
+    }
+
+    rightClick(){
+        if(this.currentIndex === this.numImgs - 1){
+            this.currentIndex = 0;
+        } else {
+            this.currentIndex++;
+        }
+        this.imgs.forEach(el => el.style.display = 'none');
+        this.imgs[this.currentIndex].style.display = 'block';
+    }
 }
 
-let carousel = document.querySelector();
-
+let carousel = new Carousel(document.querySelector('.carousel'));
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
     2. You will need to grab a reference to all of the images
