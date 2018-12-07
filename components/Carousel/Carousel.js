9d4cb3c9ad9e9f0dd0aba0caa05f89858081a91c
joshua-disney <joshua.a.disney@gmail.com>
2018-12-07 12:02:58 -0700

    Began work on carousel component.

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,10 @@
 class Carousel {
-
+    constructor(carouselElement) {
+        this.carouselElement = carouselElement;
+        this.pictures = document.querySelectorAll('img');
+    }
 }
 
-let carousel = document.querySelector();
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
@@ -12,3 +14,6 @@ let carousel = document.querySelector();
     5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
     6. Have fun!
 */
+const carousel = document.querySelector('.carousel').forEach( carousel => new Carousel(carousel));
+const leftButton = document.querySelector('.left-button');
+const rightButton = document.querySelector('.right-button');
