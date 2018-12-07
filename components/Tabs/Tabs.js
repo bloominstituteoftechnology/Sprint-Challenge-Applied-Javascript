498f61214efeb17784e906efc21f3363d3223a9c
mcferrenm <mcferrenmax@gmail.com>
2018-12-07 11:44:25 -0500

    Bug fix in TabLink component constructor on this.tabElement, selectTab method

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,7 +1,7 @@
 class TabLink {
   constructor(tabElement){
     // assign this.tabElement to the tabElement DOM reference
-    this.tabElement = this.tabElement;
+    this.tabElement = tabElement;
     
     // Get the `data-tab` value from this.tabElement and store it here
     this.tabData = this.tabElement.dataset.tab;
@@ -27,22 +27,23 @@ class TabLink {
   selectTab(){
 
     // Select all elements with the .tab class on them
-    // const tabs = document.querySelectorAll();
+    const tabs = document.querySelectorAll('.tab');
     
     // Iterate through the NodeList removing the .active-tab class from each element
-    // tabs.forEach()
+    tabs.forEach(tab => tab.classList.remove('active-tab'));
 
     // Select all of the elements with the .card class on them
-    // const cards = ;
+    const cards = document.querySelectorAll('.card');
+    
 
     // Iterate through the NodeList setting the display style each one to 'none'
-    // cards.forEach()
+    cards.forEach(card => card.style.display = 'none');
     
     // Add a class of ".active-tab" to this.tabElement
-    // this.tabElement;
+    this.tabElement.classList.add('active-tab');
   
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class. Just un-comment the code and study what is happening here.
-    // this.cards.forEach(card => card.selectCard());
+    this.cards.forEach(card => card.selectCard());
   }
 }
 
@@ -69,4 +70,7 @@ class TabCard {
 */
 let tabs = document.querySelectorAll('.tab');
 
-tabs.forEach(tab => new TabLink(tab));
+// tabs.forEach(tab => new TabLink(tab));
+tabs.forEach(tab => {
+  return new TabLink(tab);
+});
