cb12858eb0caa5f2a07143bf3a5811e655ac88fa
Daniel Weinman <namniewd@gmail.com>
2018-12-07 13:27:28 -0500

    add animations when carousel picture changes

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -12,27 +12,32 @@ let images = document.querySelectorAll('.carousel img');
 let currentIndex = 0;
 
 images[0].style.display = 'flex';
+images[0].style.opacity = '1';
 
 // images.forEach(image => new Carousel(image));
 
 right.addEventListener('click', () => {
     images[currentIndex].style.display = 'none';
+    TweenMax.set(images[currentIndex], {opacity: 0});
     if (currentIndex === (images.length - 1)) {
         currentIndex = 0;
     } else {
         currentIndex++;
     }
+    TweenMax.to(images[currentIndex], 1, {opacity: 1});
     images[currentIndex].style.display = 'flex';
 })
 
 left.addEventListener('click', () => {
     images[currentIndex].style.display = 'none';
+    TweenMax.set(images[currentIndex], {opacity: 0});
     if (currentIndex === 0) {
         currentIndex = images.length - 1;
     } else {
         currentIndex--
     }
-    images[currentIndex].style.display = 'flex'
+    TweenMax.to(images[currentIndex], 1, {opacity: 1});
+    images[currentIndex].style.display = 'flex';
 })
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
