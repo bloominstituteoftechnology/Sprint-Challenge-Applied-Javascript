0f233b6cff403c9207bde630a5aea5a41558d98a
EJaona <ejaona@gmail.com>
2018-12-07 13:16:34 -0600

    maping over cards array and passing the single card to TabCard class constructor

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -18,7 +18,8 @@ class TabLink {
     }
 
     // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class.
-    //this.cards = Array.from(this.cards).map(card => );
+    this.cards = Array.from(this.cards).map(card => new TabCard(card));
+
     // Add a click event that invokes this.selectTab
     //this.tabElement.addEventListener();
   }
