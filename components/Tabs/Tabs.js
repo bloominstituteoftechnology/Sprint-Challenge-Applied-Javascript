eb98d17cef23929580347298dc153d7998e18a7d
Nathan Thomas <nwthomas@me.com>
2018-12-07 08:58:13 -0800

    Fix selection bug and finish core assignment

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -39,7 +39,7 @@ class TabLink {
     cards.forEach(card => (card.style.display = "none"));
 
     // Add a class of ".active-tab" to this.tabElement
-    this.tabElement = this.tabElement.classList.toggle("active-tab");
+    this.tabElement.classList.toggle("active-tab");
 
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class. Just un-comment the code and study what is happening here.
     this.cards.forEach(card => card.selectCard());
