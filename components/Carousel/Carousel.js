9f4909b820eb14f392da7b4f8592cca6121b476b
RachelKolk <rach.m.obrien@gmail.com>
2018-12-07 19:35:15 -0500

    still wokring on carousel stretch

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,48 +1,52 @@
-class Carousel {
-    constructor(photoCarousel) {
-        this.photoCarousel = photoCarousel;
+// class Carousel {
+//     constructor(photoCarousel) {
+//         this.photoCarousel = photoCarousel;
+//         this.index = 0;
 
-        this.leftButton = document.querySelector('.left-button');
-        this.rightButton = document.querySelector('right-button');
-        this.photoData = this.photoCarousel.dataset.tab;
+//         this.leftButton = document.querySelector('.left-button');
+//         this.rightButton = document.querySelector('right-button');
+//        // this.photoData = this.photoCarousel.dataset.tab;
 
-        this.photo = document.querySelectorAll(`.image[data-tab='${this.photoData}']`);
+//        // this.imageContent = document.querySelectorAll(`.image[data-tab='${this.photoData}']`);
+//         this.imageContent = document.querySelectorAll('.image');
+//         this.imageContent = new Image(this.imageContent);
 
-        this.photo = new Image(this.imageContent);
+//         this.leftButton.addEventListener('click', () => {this.leftClick()});
+//         this.rightButton.addEventListener('click', () => {this.rightClick()});
 
-        this.leftButton.addEventListener('click', () => {this.leftClick()});
-        this.rightButton.addEventListener('click', () => {this.rightClick()});
+//     };
 
-    };
+//     leftClick() {
+//         let pictures = document.querySelectorAll('.image');
+//          pictures.forEach(picture => picture.classList.remove('active-img'));
+//         // this.photoCarousel.classList.add('active-img');
+//         // this.pictures.forEach(pictures => (pictures.selectImage));
+//         for (let i = 0; i < pictures.length; i++) {
+//             this.pictures[i].add('active-image');
+//         }
+//     }
 
-    leftClick() {
-        let pictures = document.querySelectorAll('.image');
-        pictures.forEach(picture => picture.classList.remove('img'));
-        this.photoCarousel.classList.add('active-img');
-        this.pictures.forEach(pictures => (pictures.selectImage));
-    }
-
-    rightClick() {
-        let pictures = document.querySelectorAll('.image');
-        Array.from(pictures).forEach(picture => picture.classList.remove('img'));
-        this.photoCarousel.classList.add('active-img');
-        this.pictures.forEach(pictures => (pictures.selectImage));
-    }
-}
+//     rightClick() {
+//         let pictures = document.querySelectorAll('.image');
+//         Array.from(pictures).forEach(picture => picture.classList.remove('active-img'));
+//         pictures[3].classList.add('active-img');
+//         this.pictures.forEach(pictures => (pictures.selectImage));
+//     }
+// }
 
 
-class Image {
-    constructor(imageContent) {
-        this.imageContent = imageContent;
-    }
+// class Image {
+//     constructor(imageContent) {
+//         this.imageContent = imageContent;
+//     }
 
-    selectImage() {
-        this.imageContent.style.display = 'flex';
-    }
-}
+//     selectImage() {
+//         this.imageContent.style.display = 'flex';
+//     }
+// }
 
-let carousel = document.querySelector('.image');
-carousel.forEach(image => new Carousel(image));
+// let carousel = document.querySelector('.image');
+// carousel.forEach(image => new Carousel(image));
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
@@ -52,3 +56,28 @@ carousel.forEach(image => new Carousel(image));
     5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
     6. Have fun!
 */
+
+class Carousel {
+    constructor(photoCarousel) {
+        this.photoCarousel = photoCarousel;
+        this.imageContent = document.querySelectorAll('.image');
+        this.index = 0;
+
+        this.leftButton = document.querySelector('.left-button');
+        this.rightButton = document.querySelector('right-button');
+
+        this.leftButton.addEventListener('click', () => {this.leftClick()});
+        this.rightButton.addEventListener('click', () => {this.rightClick()});
+
+    }
+
+    leftClick(){
+        this.index = this.index++;
+
+    }
+
+    rightClick(){
+        this.index = this.index--;
+    }
+
+}
