a506833657e1bd0b1500e073de809d4644955513
Estevan <estevan.rodriguez10@yahoo.com>
2018-12-09 01:42:21 -0800

    small chnage that i needed to make the code work

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -18,7 +18,7 @@ class TabLink {
        //console.log(this.cards);
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
-       this.cards = document.querySelectorAll(this.tabData);
+       this.cards = document.querySelectorAll(`.card[data-tab = "${this.tabData}"]`);
       // console.log(this.cards);
     }
     
