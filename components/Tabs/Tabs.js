566bcba5cf115a05aeaca3c04aabf1f0c4220086
drkfogle <rodriguez.kevin.094@gmail.com>
2018-12-14 13:11:36 -0500

    new error on line 25

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -19,7 +19,7 @@ class TabLink {
 
 
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
-    this.cards = Array.from(this.cards).map(new TabCard(this.cards));
+    this.cards = Array.from(this.cards).map(card => new TabCard(card));
 
     // Add a click event that invokes this.selectTab
     this.tabElement.addEventListener('click', selectTab());
@@ -43,7 +43,7 @@ class TabLink {
     this.tabElement.classList.add('.active-tab');
   
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class. Just un-comment the code and study what is happening here.
-    // this.cards.forEach(card => card.selectCard());
+    this.cards.forEach(card => card.selectCard());
   }
 }
 
