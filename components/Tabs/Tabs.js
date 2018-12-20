e4e60f33f74279f173ff0e1b4173c325bbbecfd9
unknown <jjamal95@gmail.com>
2018-12-20 11:11:49 -0600

    finished the slectTab method

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -5,21 +5,22 @@ class TabLink {
 
 
 
+  
+selectTab() {
+  const tabs = document.querySelectorAll(".tab");
+  for (let i = 0; i < tabs.length; i++) {
+    tabs[i].classList.remove("active-tab");
+  }
+}
+  const cards = document.querySelectorAll(".card");
+   for(let i = 0; i < cards.length; i++) {
+     cards[i].style.display = "none";
+   }
+   this.tabElement.classList.add("active-tab");
+   this.cards.forEach(card => card.selectCard());
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
     class TabCard {
   constructor(cardElemtnt) {
     this.cardElemtnt = cardElemtnt;
