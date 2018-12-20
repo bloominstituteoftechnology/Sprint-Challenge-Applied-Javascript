f55d04b2f2e33e3d59c789532de4eeeab17a9344
unknown <jjamal95@gmail.com>
2018-12-20 11:42:04 -0600

    working on last parent class and adding event listener

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,26 +1,36 @@
 class TabLink {
   constructor(tabElement){
-    
-
-
-
-
+    this.tabElement = tabElement;
+    this.tabData = this.tabElement.dataset.tab
   
+  if (this.tabData = "all") {
+      
+       this.cards = document.querySelectorAll("card");
+  } else {
+       this.cards = document.querySelector(".tabData");
+  }
+
+
+
+
+  }
+}
 selectTab() {
+  
   const tabs = document.querySelectorAll(".tab");
   for (let i = 0; i < tabs.length; i++) {
     tabs[i].classList.remove("active-tab");
   }
-}
+
   const cards = document.querySelectorAll(".card");
    for(let i = 0; i < cards.length; i++) {
      cards[i].style.display = "none";
    }
    this.tabElement.classList.add("active-tab");
    this.cards.forEach(card => card.selectCard());
-}
+  }
 
-}
+
     class TabCard {
   constructor(cardElemtnt) {
     this.cardElemtnt = cardElemtnt;
