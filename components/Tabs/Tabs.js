714d3d0fe3367c9c445c4d400f0cd53d592877ae
Ba-Zen <bazenatlas@gmail.com>
2019-04-12 15:26:51 -0400

    typo fix

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -41,7 +41,7 @@ class TabLink {
      cards.forEach(item => item.style.display = 'none');
     
     // Add a class of ".active-tab" to this.tabElement
-     this.tabElement.classList.add('.active-tab');
+     this.tabElement.classList.add('active-tab');
   
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class. Just un-comment the code and study what is happening here.
      this.cards.forEach(card => card.selectCard());
