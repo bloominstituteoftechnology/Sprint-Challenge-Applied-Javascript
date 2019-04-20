d8a8a3e3d6a692232817c66aeebf5ee2cf93caf9
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-04-20 09:37:04 -0700

    hmmm

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -36,11 +36,13 @@ class TabLink {
      const cards = document.querySelectorAll('.card');
 
     // Iterate through the NodeList setting the display style each one to 'none'
-    // cards.forEach()
-    
-    // Add a class of ".active-tab" to this.tabElement
-    // this.tabElement;
+     cards.forEach(var i = 0; i <myNodeList.length; i++){
+       var tab = 
+     }
   
+    // Add a class of ".active-tab" to this.tabElement
+    this.tabElement = new Active ('.active-tab');
+     
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class. Just un-comment the code and study what is happening here.
     // this.cards.forEach(card => card.selectCard());
   }
