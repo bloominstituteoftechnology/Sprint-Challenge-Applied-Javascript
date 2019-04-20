73ef30393f65305c922a3198bdfa96c088e4be2e
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-04-20 09:44:12 -0700

    its still not workng per usual

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -4,7 +4,7 @@ class TabLink {
     this.tabElement = document.tabElement;
     
     // Get the `data-tab` value from this.tabElement and store it here
-    this.tabData = this. 
+    this.tabData = this.element.querySelector('.data-tab')
     
     // We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task:    
     
