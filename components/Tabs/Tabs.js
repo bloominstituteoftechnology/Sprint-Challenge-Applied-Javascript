1ac218ebe28b24f7b89a532be38d024712880a2d
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-04-20 09:41:09 -0700

    i missed the important word

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -30,7 +30,7 @@ class TabLink {
      const tabs = document.querySelectorAll('.tab');
     
     // Iterate through the NodeList removing the .active-tab class from each element
-    tabs.forEach(this.tabElement.classList.toggle('active-tab'))
+    tabs.forEach(this.tabElement.classList.toggle.remove('active-tab'))
 
     // Select all of the elements with the .card class on them
      const cards = document.querySelectorAll('.card');
