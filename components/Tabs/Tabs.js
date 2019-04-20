5c1a0d162c2b361a7f517e553887d6fe292e2b47
jacobdurst <jdd2007@gmail.com>
2019-04-20 14:45:33 -0400

    almost completed selectTab

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -12,36 +12,35 @@ class TabLink {
     // We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task:    
     
      // Check to see if this.tabData is equal to 'all'
-  /* 
-     if(){
+ 
+     if (this.tabData === "all") {
       // If `all` is true, select all cards regardless of their data attribute values
-      // this.cards = ;
+      this.cards = document.querySelectorAll(".card");
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
-      // this.cards = ;
-    }
-  */
+      this.cards = document.querySelectorAll(`.card[data-tab="${this.cards}"`);
+    };
 
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
-    // this.cards = Array.from(this.cards).map();
+    this.cards = Array.from(this.cards).map( x => new TabCard(x));
 
     // Add a click event that invokes this.selectTab
-    // this.tabElement.addEventListener();
+    this.tabElement.addEventListener("click", () => this.selectTab());
   }
 
   selectTab(){
 
     // Select all elements with the .tab class on them
-    // const tabs = document.querySelectorAll();
+    const tabs = document.querySelectorAll(".tab");
     
     // Iterate through the NodeList removing the .active-tab class from each element
-    // tabs.forEach()
+    tabs.forEach( tab  = tab.classlist.remove('.active-tab'));
 
     // Select all of the elements with the .card class on them
-    // const cards = ;
+    const cards = document.querySelectorAll(".card");
 
     // Iterate through the NodeList setting the display style each one to 'none'
-    // cards.forEach()
+    cards.forEach( card => card.getElementsByClassName.display = "none");
     
     // Add a class of ".active-tab" to this.tabElement
     // this.tabElement;
