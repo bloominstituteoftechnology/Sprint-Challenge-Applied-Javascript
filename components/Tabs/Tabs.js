07e8d07c405dbb275ca11374b4f47098ef5ea1a6
Alberto Rodriguez <juniorrodriguez@Albertos-iMac.local>
2019-04-21 20:23:20 -0500

    code for if statement

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -4,18 +4,23 @@ class TabLink {
     this.tabElement = tabElement;
     
     // Get the `data-tab` value from this.tabElement and store it here
-    // this.tabData = ; 
+    //  this.tabData =;
+    this.tabData = this.tabElement.dataset.tab; 
     
     // We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task:    
     
-    /* <- Delete this comment block when you work on the if statement
+     <- Delete this comment block when you work on the if statement
     // Check to see if this.tabData is equal to 'all'
-    if(){
+
+    if(this.tabData === 'all'){
       // If `all` is true, select all cards regardless of their data attribute values
       // this.cards = ;
+      this.cards = document.querySelectorAll('.card');
+
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
       // this.cards = ;
+      this.cards = document.querySelectorAll(`.card[data-tab='${this.tabData}']`);
     }
     /* <- Delete this comment block when you work on the if statement
 
