1d5c2b200a0a068fbddc9ba91b8d3dc0ffffe50d
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-05-22 18:20:46 +0200

    typo

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -9,6 +9,6 @@ let carousel = document.querySelector();
     2. You will need to grab a reference to all of the images
     3. Create a current index
     4. Those buttons are gonna need some click handlers.
-    5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
+    5. Think of how you would animate this component. Make the cards slide in and out, or fade. It's up to you!
     6. Have fun!
 */
