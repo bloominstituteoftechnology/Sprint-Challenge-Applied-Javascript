abeb496896a5aecdfef7507b8be34b77747d903d
KimberlySwinton <kimberlyswinton@gmail.com>
2019-05-24 13:05:22 -0400

    Constructor Function and  If statement is Done

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -7,26 +7,30 @@ class TabLink {
     // Get the `data-tab` value from this.tabElement and store it here
     this.tabData = this.tabElement.dataset.tab; 
     
-    // We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task:    
+    //////////// We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task:    
     
-    /* <- Delete this comment block when you work on the if statement
+    
     // Check to see if this.tabData is equal to 'all'
-    if(){
+    if(this.tabData === 'all'){ //////KIM'S NOTES///// === equal value and equal type	
       // If `all` is true, select all cards regardless of their data attribute values
-      // this.cards = ;
+       this.cards = document.querySelectorAll('.card');
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
-      // this.cards = ;
+      this.cards = document.querySelectorAll(
+        `.card[data-tab = '${this.tabData}']`,
+        );
+      };
     }
-    /* <- Delete this comment block when you work on the if statement
+    
 
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
-    // this.cards = Array.from(this.cards).map();
+    this.cards = Array.from(this.cards).map(card => new TabCard(card));
 
     // Add a click event that invokes this.selectTab
-    // this.tabElement.addEventListener();
+     this.tabElement.addEventListener('click', () => this.selectTab());
   }
 
+ 
   selectTab(){
 
     // Select all elements with the .tab class on them
