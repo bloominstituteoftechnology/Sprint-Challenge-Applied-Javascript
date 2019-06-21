c344eba4d505ce48e570cd04c65b5c0b55bd741c
johnschneider1 <johnlawschneider@gmail.com>
2019-06-21 12:03:59 -0500

    begin work on stretccccccccch

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,7 @@
-class Carousel {
+class Carousel {}
 
-}
-
-let carousel = document.querySelector();
+let carousel = document.querySelector(".carousel");
+carousel = new Carousel(carousel);
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
