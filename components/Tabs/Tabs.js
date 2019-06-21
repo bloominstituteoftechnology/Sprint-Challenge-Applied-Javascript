a1ddc87a3b0d81b6195eb3c57f57bd1a1b56aa28
Trang Nguyen <ttn2127@columbia.edu>
2019-06-21 11:06:20 -0400

    First commit, 1/4 done with tabs

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,21 +1,21 @@
 class TabLink {
   constructor(tabElement){
     // assign this.tabElement to the tabElement DOM reference
-    // this.tabElement;
+    this.tabElement = tabElement;
     
     // Get the `data-tab` value from this.tabElement and store it here
-    // this.tabData = ; 
+    this.tabData = this.tabElement.dataset.tab; 
     
     // We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task:    
     
-    /* <- Delete this comment block when you work on the if statement
+  
     // Check to see if this.tabData is equal to 'all'
-    if(){
+    if(this.tabData === "all"){
       // If `all` is true, select all cards regardless of their data attribute values
-      // this.cards = ;
+      this.cards = document.querySelectorAll('.card');
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
-      // this.cards = ;
+      this.cards = document.querySelectorAll(`.card[data-tab= '${this.tabData}']`);
     }
     /* <- Delete this comment block when you work on the if statement
 
@@ -69,4 +69,4 @@ class TabCard {
 - In your .forEach() method's callback function, return a new instance of TabLink and pass in each tab as a parameter
 
 */
-let tabs = document.querySelectorAll();
+let tabs = document.querySelectorAll(".tab").forEach(tab => new TabLink(tab));
