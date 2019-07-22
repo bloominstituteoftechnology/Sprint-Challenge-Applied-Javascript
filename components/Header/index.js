367655578a16a8e879b9121c9cb0f75f82e69569
YasirHasn9 <hassanyasir223@gmail.com>
2019-07-21 18:18:40 -0700

    define the new Elements

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,14 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+function Header() {
+    // define the new Elemnts
+    const header = document.createElement('div');
+    const spanDate = document.createElement('span');
+    const h1 = document.createElement('h1');
+    const spanTemp = document.createElement('span');
+
+
+    
+
+}
