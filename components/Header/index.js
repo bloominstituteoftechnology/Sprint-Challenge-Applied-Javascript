2645adfd0326bcc599b30ef25e936ae7a490cea4
milano12345 <dmilano857@gmail.com>
2019-08-09 11:00:16 -0500

    completed header component

--- components/Header/index.js
+++ components/Header/index.js
@@ -8,5 +8,26 @@
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .headerContainer component
+const headerContainer = document.querySelector('.header-container')
+headerContainer.appendChild(Header())
+function Header() {
+    const header = document.createElement('div')
+    const date = document.createElement('span')
+    const title = document.createElement('h1')
+    const temp = document.createElement('span')
 
-function Header() {}
+    header.classList.add('header')
+    date.classList.add('date')
+    temp.classList.add('temp')
+
+    date.textContent = 'SMARCH 28, 2019'
+    title.textContent = 'Lambda Times'
+    temp.textContent = '98°'
+
+    header.appendChild(date)
+    header.appendChild(title)
+    header.appendChild(temp)
+    
+    return header;
+    
+}
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,15 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios
+    .get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(res => res.for)
+    .catch(err => console.log(err))
+
+function TabComp(item) {
+    const tab = document.createElement('div')
+    tab.classList.add('tab')
+    tab.textContent(item)
+
+}
