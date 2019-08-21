605ddc87a3af8a4ab64674f8eee19f71f2751619
Nick Durbin <nick.durbin@gmail.com>
2019-08-21 16:44:04 -0400

    Fixed Two Minor Spelling Errors in components/Header/index.js
    The first was the date in the instructions from 'SMARCH' to March.
    
    Second, I fixed the instruction to add the component to the DOM. The class name was incorrect. It should now be the same as the html document.

--- components/Header/index.js
+++ components/Header/index.js
@@ -3,10 +3,10 @@
 // Using a function create the component you see below:
 //
 //  <div class="header">
-//    <span class="date">SMARCH 28, 2019</span>
+//    <span class="date">MARCH 28, 2019</span>
 //    <h1>Lambda Times</h1>
 //    <span class="temp">98°</span>
 //  </div >
-// And add it to the DOM in the .headerContainer component
+// And add it to the DOM in the .header-container component
 
 function Header() {}
