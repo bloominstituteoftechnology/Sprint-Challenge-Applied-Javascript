850d456a3f2b34b2457e64e734c95c261b0e274f
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2019-09-06 08:39:23 -0700

    90% on title tabs. Not returning right info

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -1,9 +1,32 @@
 // Step 2: Create Tabs
 // -----------------------
-// Using axios send a GET request to the address: https://lambda-times-backend.herokuapp.com/topics
-// Once the data is returned console.log it and review the structure.
-// Iterate over the topics creating a new Tab component and add it to the DOM
+// Using axios send a GET request to: https://lambda-times-backend.herokuapp.com/topics
+// Console.log the data and review the structure.
+// Iterate over the topics creating a new Tab component to add the DOM
 // under the .topics element.
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(response =>{
+        console.log(response);
+        response.data.topics.forEach(item=> {
+            let newTab = createTab(item);
+            allTopicsTab.appendChild(newTab)
+        });
+    })
+
+const allTopicsTab = document.querySelector('.topics')
+
+function createTab(obj){
+    let topicTab = document.createElement('div');
+    topicTab.classList.add('tab');
+
+    for (let i = 0; i < obj.length; i++) {
+        topicTab.textContent = obj[i];
+    };
+    
+    return topicTab;
+}
