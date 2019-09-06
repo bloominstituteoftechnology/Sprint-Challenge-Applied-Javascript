a286213f7aede7a78b0aaf8a60a5daab7ca66d9c
Afia-Lydia <you@example.com>
2019-09-06 18:59:55 -0400

    a final attempt

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,52 +17,80 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+const containMe = document.querySelector('.cards-container')
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+
+    .then(response => {
+        const {
+            javascript,
+            bootstrap,
+            technology,
+            jquery,
+            node
+        } = response.data.articles;
+        console.log(response.data.articles)
+        // {
+        //     javascript,
+        //     bootstrap,
+        //     technology,
+        //     jquery,
+        //     node
+        // }
+        const dog = response.data.articles;
+            dog.forEach(element => {
+                // const tryMe = getArticles(element);
+                // console.log(tryMe);
+                const ralph = createCard(element);
+                console.log(ralph)
+            });
 
 
-// axios.get('https://lambda-times-backend.herokuapp.com/articles')
-//     .then(response => {
-//         const {
-//             javascript,
-//             bootstrap
-//         } = response.data.articles;
-//         console.log(response);
-//         getArticles(javascript);
 
+    })
+    .catch(error => {
+        console.log("The data was not returned", error);
+    });
+
+
+
+// function getArticles(array) {
+//     // createCard()
+
+//     data.forEach(data => {
+//         createCard(data)
 //     })
-
-
-
-// function getArticles(array){
-// // createCard()
-// array.forEach(element => {
-//     createCard(article)
-// });
-// }
-
-// function createCard(article) {
-//     const
-//         cardContainer = document.createElement('div'),
-//         headline = document.createElement('div'),
-//         author = document.createElement('div'),
-//         imgContainer = document.createElement('div'),
-//         image = document.createElement('img'),
-//         name = document.createElement('span');
-
-//     cardContainer.appendChild(headline);
-//     cardContainer.appendChild(author);
-//     author.appendChild(name);
-//     author.appendChild(imgContainer);
-//     imgContainer.appendChild(image);
-
-//     cardContainer.classList.add('card');
-//     headline.classList.add('headline');
-//     author.classList.add('author');
-//     imgContainer.classList.add('img-container');
-
-//     headline.textContent = `${data.headline}`;
-//     image.src = `${data.authorPhoto}`
-
-
-
+//     containMe.appendChild(ralph);
 
 // }
+
+// const indeX = response.data.articles;
+
+function createCard(article) {
+    const
+        cardContainer = document.createElement('div'),
+        headline = document.createElement('div'),
+        author = document.createElement('div'),
+        imgContainer = document.createElement('div'),
+        image = document.createElement('img'),
+        name = document.createElement('span');
+
+    cardContainer.appendChild(headline);
+    cardContainer.appendChild(author);
+    author.appendChild(name);
+    author.appendChild(imgContainer);
+    imgContainer.appendChild(image);
+
+    cardContainer.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author');
+    imgContainer.classList.add('img-container');
+
+    headline.textContent = `${this.headline}`;
+    image.src = `${this.authorPhoto}`;
+    name.textContent = `${this.authorName}`
+
+
+    console.log(`im here ${article}`)
+    return cardContainer;
+}
