03e70fc379ac03ec7c9a916c9b3815a4f12dd33d
Christina <chmagorton@gmail.com>
2019-09-06 07:19:11 -0600

    Fixing header-container to match class

--- components/Header/index.js
+++ components/Header/index.js
@@ -7,6 +7,6 @@
 //    <h1>Lambda Times</h1>
 //    <span class="temp">98°</span>
 //  </div >
-// And add it to the DOM in the .headerContainer component
+// And add it to the DOM in the .header-container component
 
 function Header() {}
