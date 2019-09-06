2e2bcb879e56bf64d7cfec7c47764ac77d316d1f
Martins-O-U <martinsonyedikachi@gmail.com>
2019-09-06 09:00:40 +0100

    Worked on the header task, created its component and appended it to the DOM

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,27 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+function Header( text, text2, text3) {
+    let div = document.createElement('div');
+    let span = document.createElement('span');
+    let h1 = document.createElement('h1');
+    let span2 = document.createElement('span');
+
+    div.classList.add('header');
+    span.classList.add('date');
+    span2.classList.add('temp');
+
+    span.textContent = text;
+    h1.textContent = text2;
+    span2.textContent = text3;
+
+    div.appendChild(span);
+    div.appendChild(h1);
+    div.appendChild(span2)
+
+    return div;
+
+}
+
+let header = document.querySelector(".header-container");
+header.append(Header('MARCH 28, 2019', "Lambda Times", "98°"));
