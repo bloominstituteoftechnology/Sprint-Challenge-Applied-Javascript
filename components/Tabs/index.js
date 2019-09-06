288334254cc58209e6d990918b1b2b4a88d2fafa
Manju KC <kcmanjuthapa@gmail.com>
2019-09-06 11:34:05 -0400

    crated createTabs function in Tabs index.js

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,14 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+const createTabs = (tab) => {
+    const tabs = document.createElement("div");
+    tabs.classList.add("tab");
+    tabs.textContent = tab;
+    //"tab" whatever the argument I pass in is
+  
+    return tabs;
+  };
+  
