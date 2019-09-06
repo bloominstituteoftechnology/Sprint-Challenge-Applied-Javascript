68f724c5791abcfa977a38ed5f91c926e93251b9
Manju KC <kcmanjuthapa@gmail.com>
2019-09-06 13:45:45 -0400

    attempted stretch

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,39 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+
+
+class Carousel {
+  constructor(element) {
+      this.element = element;
+      console.log(this.element)
+
+      // Get references to the classes we need
+      this.photoBox = this.element.querySelector('.carousel');
+      this.next = this.element.querySelector('.right-button');;
+      this.prev = this.element.querySelector('.left-button');
+      this.photos = this.element.querySelectorAll('.carousel img');
+
+      // Initialize variables
+      this.counter = 0; // Keeps track of which picture is showing
+      this.totalPhotos = this.photos.length;
+      this.current = this.photos[0];
+      console.log(this.current)
+
+      // Event listeners for next and previous buttons
+      this.next.addEventListener('click', () => {
+          console.log("Clicked!")
+          this.navigate(1);
+        });
+
+      this.prev.addEventListener('click', () => {
+          console.log("Clicked!")
+          this.navigate(-1);
+        });
+  }
+
+}
+
+const carasol = new Carousel()
+carousel;
