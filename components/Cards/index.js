9b0ab221f894f854c3274b8cff43abb51001b5d6
adkhiker <poirier.greg@gmail.com>
2019-09-06 12:51:04 -0400

    Card components

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,53 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then(data => {
+    const articles = Object.values(data.data.articles);
+    const artData = Object.keys(data.data.articles);
+
+    articles.forEach(articleType => {
+      articleType.forEach(article => {
+        const index = articles.indexOf(articleType);
+        const cards = document.querySelector(".cards-container");
+        const card = Article(article);
+        card.setAttribute("data-tab", artData[index]);
+        cards.appendChild(card);
+      });
+    });
+    console.log("Success", artData);
+  })
+  .catch(error => {
+    console.log("ERROR", error);
+  });
+
+function Article(obj) {
+  const card = document.createElement("div");
+  const headline = document.createElement("div");
+  const author = document.createElement("div");
+  const imgContainer = document.createElement("div");
+  const img = document.createElement("img");
+  const authorName = document.createElement("span");
+
+  //Add Classes
+  card.classList.add("card");
+  headline.classList.add("headline");
+  author.classList.add("author");
+  imgContainer.classList.add("img-container");
+
+  //Add Content
+  headline.textContent = obj.headline;
+  img.src = obj.authorPhoto;
+  authorName.textContent = obj.authorName;
+
+  //Append Content
+  imgContainer.appendChild(img);
+  author.appendChild(imgContainer);
+  author.appendChild(authorName);
+  card.appendChild(headline);
+  card.appendChild(author);
+
+  return card;
+}
