19852ff76c50935e9593f2bcb8b79a92c645deed
Sean <Sean@Witch-Doctor.home>
2019-09-06 11:42:45 -0400

    Header completed

--- README.md
+++ README.md
@@ -52,10 +52,10 @@ A component is reusable code that can be modified and used over and over again t
 
 ### Git Set up
 
-* [ ] Fork the project into your GitHub user account
-* [ ] Clone the forked project into a directory on your machine
-* [ ] Create a pull request before you start working on the project requirements.  You will continuously push your updates throughout the project.
-* [ ] You are now ready to build this project with your preferred IDE
+* [X] Fork the project into your GitHub user account
+* [X] Clone the forked project into a directory on your machine
+* [X] Create a pull request before you start working on the project requirements.  You will continuously push your updates throughout the project.
+* [X] You are now ready to build this project with your preferred IDE
 
 ## Minimum Viable Product
 
--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,36 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+
+
+function Header(item) {
+    //1. create elements - document.createElement()
+    const header = document.createElement('div');
+    const date = document.createElement('span');
+    const h1 = document.createElement('h1');
+    const temp = document.createElement('span');
+          
+    //2. .textContent
+    date.textContent = `MARCH 28, 2019`;
+    h1.textContent = `Lambda Times`;
+    temp.textContent = `98°`;
+
+    //3. .classList.add()
+    header.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+
+    //4. .appendChildren()
+    header.appendChild(date);
+    header.appendChild(h1);
+    header.appendChild(temp);
+
+    const headerContainer = document.querySelector('.header-container');
+    headerContainer.appendChild(header);
+    //notes for the future, this appending NEEDS to happen in the function and then called outside. Different from the API exercises because we were defining outside the API and the function, but then calling inside the .then(). The appending needs to happen inside a method. 
+
+    return header
+
+}
+
+console.log(Header());
