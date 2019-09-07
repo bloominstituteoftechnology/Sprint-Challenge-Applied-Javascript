de5fba47719a8bc69a25d0d61af0bca8842502f7
Daryl Magera <darylmagera@gmail.com>
2019-09-06 20:41:48 -0400

    working on a carosel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,11 +1,69 @@
-class Carousel {
-    
+
+
+let carouselImg = document.querySelectorAll('.carousel img');
+let images = Array.from(carouselImg);
+let parent = document.querySelector('.carousel');
+
+
+let createBtn = (type, imgSlide) => `
+<div data-goto=${type === 'left' ? imgSlide - 1 : imgSlide + 1} class="${type}-button btn">${type === 'right' ? '>' : '<'}</div>
+
+`
+let renderBtn = (currImg, numOfImgs, imgPerSlide) => {
+    const slides = numOfImgs / imgPerSlide;
+
+    let btn;
+    if(currImg === 1 && slides > 1){
+        // only show the next btn
+        btn = createBtn('right', currImg);
+    } else if (currImg < slides){
+        // show both buttons
+        btn = `
+        ${createBtn('left', currImg)}
+        ${createBtn('right', currImg)}
+        
+        `
+    } else if (currImg === slides && slides > 1){
+        btn = createBtn('left', currImg);
+    }
+
+    parent.insertAdjacentHTML('afterbegin', btn);
 }
 
 
+const renderImg = (img, currImg = 1, totalImgPerShow = 1) => {
+    let start = (currImg - 1) * totalImgPerShow;
+    let end = currImg * totalImgPerShow;
+    img.slice(start, end).forEach((image) => {
+        image.style.display = 'block';
+        
+    });
+    renderBtn(currImg, img.length, totalImgPerShow);
+}
+renderImg(images)
+
+parent.addEventListener('click', (e) =>{
+    const btn = e.target.closest('div')
+    if(btn){
+        const goToImg = parseInt(btn.dataset.goto);
+        renderImg(images, goToImg);
+    }
+ 
+});
 
 
-let carousel = document.querySelector('.carousel')
+
+
+
+
+
+
+
+
+
+
+
+
 
 // const carouselInstance = new Carousel(carousel);
 
