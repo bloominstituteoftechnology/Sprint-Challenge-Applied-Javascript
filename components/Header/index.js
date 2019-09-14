6c36f047d084eddb547a9349103e7b3c81a045f9
anatulea <anatulea14@.gmali.com>
2019-09-14 11:21:37 -0500

    Created the Header

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,32 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+      
+    // create all elements
+    const header = document.createElement('div');
+    const headerDate =document.createElement('span');
+    const headerH1 =document.createElement('h1');
+    const headerTem =document.createElement('span');
+
+    // create structure
+    header.appendChild(headerDate);
+    header.appendChild(headerH1);
+    header.appendChild(headerTem);
+
+      // apply styles
+    header.classList.add('header');
+    headerDate.classList.add('date');
+    headerTem.classList.add('temp');
+
+    // set the content
+    headerDate.textContent='MARCH 28, 2019';
+    headerH1.textContent='Lambda Times';
+    headerTem.textContent='98°';
+
+
+    return header;
+}
+ const selectHeader = document.querySelector('.header-container');
+ selectHeader.appendChild(Header());
+
