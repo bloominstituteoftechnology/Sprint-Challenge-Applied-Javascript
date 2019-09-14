dc2894910ffeb6e7459e21ca2b16172721c6afc1
Lambda_School_Loaner_181 <lambda_school_loaner_181@Lambda-School-Loaner-181s-MacBook-Pro.local>
2019-09-13 22:43:40 -0500

    finished cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,68 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+let cardContainer = document.querySelector('.cards-container');
+
+function createCard(arr) {
+    let card = document.createElement('div');
+    let headline = document.createElement('div');
+    let author = document.createElement('div');
+    let imgContainer = document.createElement('div');
+    let img = document.createElement('img');
+    let byline = document.createElement('span');
+
+
+
+    imgContainer.appendChild(img);
+    author.appendChild(imgContainer);
+    author.appendChild(byline);
+    card.appendChild(headline);
+    card.appendChild(author);
+    cardContainer.appendChild(card);
+
+    card.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author');
+    imgContainer.classList.add('img-container')
+
+    headline.textContent = arr.headline;
+    img.src = arr.authorPhoto
+    byline.textContent = `By ${arr.authorName}`
+
+    return card;
+}
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then((result) => {
+        console.log(result.data.articles);
+        let bootstrap = result.data.articles.bootstrap;
+        let javascript = result.data.articles.javascript;
+        let jquery = result.data.articles.jquery;
+        let node = result.data.articles.node;
+        let technology = result.data.articles.technology;
+
+        bootstrap.forEach(item => {
+            const newCard = createCard(item);
+            cardContainer.appendChild(newCard);
+        })
+        javascript.forEach(item => {
+            const newCard = createCard(item);
+            cardContainer.appendChild(newCard);
+        })
+        jquery.forEach(item => {
+            const newCard = createCard(item);
+            cardContainer.appendChild(newCard);
+        })
+        node.forEach(item => {
+            const newCard = createCard(item);
+            cardContainer.appendChild(newCard);
+        })
+        technology.forEach(item => {
+            const newCard = createCard(item);
+            cardContainer.appendChild(newCard);
+        })
+    })
+    .catch((err) => {
+        console.log(err);
+    });
