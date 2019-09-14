14e0091075f9f6c4ab5848e0dd14f2a4cfb5257d
Rodney-Race <rodneyrace@gmail.com>
2019-09-13 21:40:39 -0400

    update

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,23 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+class tablink {
+    constructor(element) {
+    //assigns this.element to element reference
+    this.element.element;
+    //gets the tab data attr and saves the value here.
+    this.tabData = this.element.dataset.tab;
+    // this finds all elements with the .card class in index.html that corrispond to the tab data attr. 
+    // if the data is 'all' then select all cards regaurdless of there data attr.
+    if (this.this.tabData === 'all') {
+    // if all tabs are selected then this grab all cards with .card class.
+    this.cards = document.querySelectorAll('.card');
+    } else {
+    // if any other card is selected then this grabs individual dataSet.
+    this.cards = document.querySelectorAll('.card[data-tab = "${this.tabData}"]');
+    }
+    
+
+    }
+}
