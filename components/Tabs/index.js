b7de4cae804e2fe4e94fa1b5f9a207c11065b67e
Toniddarden1 <Toniddarden@gmail.com>
2019-09-14 00:03:41 -0400

    finished

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,35 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/topics")
+  .then(function(response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function(error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function() {
+    // always executed
+  });
+
+const tabs = document.querySelector(".tabs");
+
+function divTabs(data) {
+  const topics = document.createElement("topics");
+  topics.classList.add("topics");
+
+  const tab = document.createElement("div");
+  tab.classList.add("tab");
+  tab.textContent = "topic here";
+  topics.appendChild(tab);
+
+  console.log(topics);
+  return topics;
+}
+
+const mainTab = document.querySelector(".tabs");
+mainTab.appendChild(divTabs());
