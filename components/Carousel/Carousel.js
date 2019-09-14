ce3898ac2f36912b826a6fae8ce824d94f396169
anatulea <anatulea14@.gmali.com>
2019-09-14 14:01:11 -0500

    Streatch goal almost completed

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,57 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+function carouselMaker(){
+
+  // create all elements
+const carouselContainer = document.createElement('div');
+const left =document.createElement('div');
+const img1 = document.createElement('img');
+const img2 = document.createElement('img');
+const img3 = document.createElement('img');
+const img4 = document.createElement('img')
+const right =document.createElement('div');
+
+// create structure
+carouselContainer.appendChild(left);
+carouselContainer.appendChild(img1);
+carouselContainer.appendChild(img2);
+carouselContainer.appendChild(img3);
+carouselContainer.appendChild(img4);
+carouselContainer.appendChild(right);
+
+// set the content
+img1.src = "./assets/carousel/mountains.jpeg";
+img2.src = "./assets/carousel/computer.jpeg";
+img3.src = "./assets/carousel/trees.jpeg";
+img4.src = "./assets/carousel/turntable.jpeg";
+
+// apply styles
+carouselContainer.classList.add('carousel');
+left.classList.add('left-button');
+right.classList.add('right-button');
+
+const carouselImages = [img1, img2, img3, img4];
+
+ img1.style.display = 'inline';
+ carouselImages.forEach(() => {
+    right.addEventListener('click', () => {
+      if(img1.style.display = 'inline'){
+        img1.style.display = 'none';
+        img2.style.display = 'inline'
+      }else if ( img2.style.display = 'inline'){
+        img2.style.display = 'none';
+        img3.style.display = 'inline'
+      }else if(img3.style.display = 'inline'){
+        img3.style.display = 'none';
+        img4.style.display = 'inline'
+      }
+    });
+ });
+  
+ 
+return carouselContainer;
+}
+const carouselIma =document.querySelector('.carousel-container');
+carouselIma.appendChild(carouselMaker())
