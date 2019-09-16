a655f8189cbb7d6fff5decd7ea016886a7abd61b
Cory Murray <corymurray@Corys-MacBook-Pro.local>
2019-09-15 20:10:22 -0700

    (0_0)

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,111 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+function cardGen(headline, author, url){
+    card = document.createElement('div');
+    card.classList.add('card');
+
+    cardHeadline = document.createElement('div');
+    cardHeadline.classList.add('headline');
+    cardHeadline.textContent = headline;
+
+    cardAuthor = document.createElement('div');
+    cardAuthor.classList.add('author');
+
+    cardImg = document.createElement('div');
+    cardImg.classList.add('img-container');
+
+    cardUrl = document.createElement('img');
+    cardUrl.setAttribute("src", url);
+
+    cardAuthorName = document.createElement('span')
+    cardAuthorName.textContent = author;
+
+    card.appendChild(cardHeadline);
+    card.appendChild(cardAuthor);
+
+    cardAuthor.appendChild(cardImg);
+    cardImg.appendChild(cardUrl);
+    cardAuthor.appendChild(cardAuthorName);
+    
+    return card;
+};
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then (response =>{
+    response.data.articles.bootstrap.forEach(item => {
+        document.querySelector('.cards-container').appendChild(
+            cardGen(
+                item.headline,
+                item.authorName,
+                item.authorPhoto
+            )
+        )
+        
+    });
+    response.data.articles.javascript.forEach(item => {
+        document.querySelector('.cards-container').appendChild(
+            cardGen(
+                item.headline,
+                item.authorName,
+                item.authorPhoto
+            )
+        )
+        
+    });
+
+    response.data.articles.technology.forEach(item => {
+        document.querySelector('.cards-container').appendChild(
+            cardGen(
+                item.headline,
+                item.authorName,
+                item.authorPhoto
+            )
+        )
+        
+    });
+
+    response.data.articles.jquery.forEach(item => {
+        document.querySelector('.cards-container').appendChild(
+            cardGen(
+                item.headline,
+                item.authorName,
+                item.authorPhoto
+            )
+        )
+        
+    });
+
+    let node = "node.js";
+
+    response.data.articles.node.forEach(item => {
+        document.querySelector('.cards-container').appendChild(
+            cardGen(
+                item.headline,
+                item.authorName,
+                item.authorPhoto
+            )
+        )
+        
+    });
+
+    console.log(response.data);
+})
+
+.catch (err => {
+    console.log("Error:", err);
+});
+
+/**
+ *     response.data.articles.forEach(item => {
+        document.body.appendChild(
+            cardGen(
+                item.headline,
+                item.authorName,
+                item.authorPhoto
+            )
+        )
+        
+    });
+ */
--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,28 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+    let header = document.createElement('div');
+    header.classList.add('header');
+
+    let date = document.createElement('span');
+    date.classList.add('date');
+    date.textContent = 'MARCH 28, 2019';
+
+    let title = document.createElement('h1');
+    title.textContent = 'Lambda Times';
+
+    let temp= document.createElement('span');
+    temp.classList.add('temp');
+    temp.textContent = '98°';
+
+    header.appendChild(date);
+    header.appendChild(title);
+    header.appendChild(temp);
+
+    return header;
+}
+
+ document.querySelector('.header-container').appendChild(Header());
+
+
