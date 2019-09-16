61447cda2ab3d2e9d603f894eb617a472e98b03f
Ricardo Barbosa <ric3639@gmail.com>
2019-09-16 05:36:34 -0700

    Attempted Stretch

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,38 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+class Carousel {
+  carousel = (function(){
+    var box = document.querySelector('.carousel');
+    var next = box.querySelector('.left-button');
+    var prev = box.querySelector('.right-button');
+    var items = box.querySelectorAll('.carousel img');
+    var counter = 0;
+    var amount = items.length;
+    var current = items[0];
+
+    function navigate(direction) {
+      current.classList.remove('current');
+      counter = counter + direction;
+      if (direction === -1 && 
+          counter < 0) { 
+        counter = amount - 1; 
+      }
+      if (direction === 1 && 
+          !items[counter]) { 
+        counter = 0;
+      }
+      current = items[counter];
+      current.classList.add('current');
+    }
+    next.addEventListener('click', function(ev) {
+      navigate(1);
+    });
+    prev.addEventListener('click', function(ev) {
+      navigate(-1);
+    });
+    navigate(0);
+  })(); 
+}
+
