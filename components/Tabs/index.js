1e57970fc704eb381f522356d3d41584e1c8f1cf
itsericfig <itsericfig@gmail.com>
2019-10-04 10:53:54 -0500

    Created tabs using JS and Axios

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,23 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios
+    .get("https://lambda-times-backend.herokuapp.com/topics")
+    .then(response => {
+        const topics = response.data.topics;
+        topics.forEach(topic => {
+            // CREATE ELEMENTS
+            const tab = document.createElement("div");
+            // ADD CLASSES
+            tab.classList.add("tab");
+            // ADD TEXT/VALUE
+            tab.textContent = topic;
+
+            const tabs_topics = document.querySelector(".topics");
+            tabs_topics.appendChild(tab);
+        });
+    })
+    .catch(error => {
+        console.log("🚨", error);
+    });
