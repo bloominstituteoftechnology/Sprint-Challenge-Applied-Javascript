babc0c454bb7c8500cb48d5b1adde269a586071c
Norma Bunton <te_nj25@hotmail.com>
2019-10-04 12:52:11 -0400

    tabs added

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,34 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios 
+    .get("https://lambda-times-backend.herokuapp.com/topics")
+    .then(response => {
+        console.log(response);
+    //     response.data.message.forEach('https://lambda-times-backend.herokuapp.com/topics' => {
+    //         const newTab = tabs(item);
+    //         entryPoint.appendChild(newTab);
+    //     });
+    // })
+    // .catch(error => {
+    //     console.log('The data was not returned', error);
+    });
+    
+function createTab(topics, title){
+
+    const tab = document.createElement('div');
+    const topics = document.createElement('div');
+    const title = document.createElement('span');
+
+tab.appendChild(topics)
+tab.appendChild(title)
+
+tab.classList.add('tabs')
+topics.classList.add('topics')
+title.classList.add('title')
+
+tab.textContent = topics
+tab.textContent = title
+}
+return tab;
