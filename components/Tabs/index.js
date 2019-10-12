033e27b60fbf0b9453a22218806f3cb9cad01943
Samuel Kraus <S_read@aol.com>
2019-10-12 15:10:22 -0500

    completed header

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,49 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+
+
+
+
+ const topiCont = document.querySelector('.topics');
+
+
+
+  function Tab(data){
+
+    
+        var topicItem = document.createElement('div')
+        topicItem.classList.add('tab')
+        topicItem.textContent = data
+
+        return Tab;
+  };
+
+  console.log(Tab());
+
+ 
+  
+  axios.get('https:lambda-times-backend.herokuapp.com/topics')
+  .then(function (response) {
+    return(response);
+   
+  })
+
+  .then(function(response) {
+      console.log((response.data));
+  })
+
+
+  .then(function(response){
+      response.data.forEach((data) => {
+        let tabby = Tab(response.data);
+        topiCont.appendChild(tabby);
+      })
+
+  })
+
+  .catch(function (error) {
+    console.log(error);
+  });
