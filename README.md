b2ce92e2d04c475da79723e3330aabe669bb4134
CLYPR Technologies <cbanks@clypr.co>
2019-10-12 12:36:36 -0400

    completed /header/index.js

--- README.md
+++ README.md
@@ -29,14 +29,19 @@ Demonstrate your understanding of this week's concepts by answering the followin
 Edit this document to include your answers after each question. Make sure to leave a blank line above and below your answer so it is clear and easy to read by your project manager
 
 1. What is the DOM?
+- sdfldsjflsdf
 
 2. What is an event?
+- sdljflsfjsd 
 
 3. What is an event listener?
+- sdfjsdlfjsdf
 
 4. Why would we convert a NodeList into an Array?
+- sdfjsdlfjsdf
 
 5. What is a component?
+- sdfjsdlfjsdf
 
 ### Git Set up
 
--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,33 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+const mainHeader = document.querySelector('.header-container');
+
+function createHeader () {
+
+    // Creating the Elements
+    const header = document.createElement('div');
+    const headerDate = document.createElement('span');
+    const headerTitle = document.createElement('h1');
+    const headerTemp = document.createElement('span');
+
+    // Adding Classlists to the Elements
+    headerDate.classList.add('date');
+    headerTemp.classList.add('temp');
+    header.classList.add('header');
+
+    // Adding text to the Elements
+    headerDate.textContent = `MARCH 28, 2019`;
+    headerTitle.textContent = `Lambda Times`;
+    headerTemp.textContent = `98°`;
+
+    // Appending children to parent
+    header.appendChild(headerDate);
+    header.appendChild(headerTitle);
+    header.appendChild(headerTemp);
+
+    return header;
+}
+
+mainHeader.appendChild(createHeader());
+
