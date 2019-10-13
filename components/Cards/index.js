31c43b924a71b5f4e289bafef18a07202d81d05d
ChiragThesia <harithesia@gmail.com>
2019-10-13 17:13:21 -0500

    initial commit with code

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -1,7 +1,7 @@
 // STEP 3: Create Article cards.
 // -----------------------
 // Send an HTTP GET request to the following address: https://lambda-times-backend.herokuapp.com/articles
-// Stduy the response data you get back, closely.
+// Study the response data you get back, closely.
 // You will be creating a component for each 'article' in the list.
 // This won't be as easy as just iterating over an array though.
 // Create a function that will programmatically create the following DOM component:
@@ -17,3 +17,37 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+function createCard(){
+    //element creation 
+    const cardDiv = document.createElement('div');
+
+        const headlineDiv = document.createElement('div');
+        const authorDiv = document.createElement('div');
+            const imgContainerDiv = document.createElement('div');
+            const cardImage = document.createElement('img');
+        const cardSpan = document.createElement('span');
+
+    //class assignment. 
+
+    cardDiv.classList.add('card');
+    headlineDiv.classList.add('headline');
+    authorDiv.classList.add('author');
+    imgContainerDiv.classList.add('img-container');
+
+    //appendment 
+    cardDiv.appendChild(headlineDiv);
+        headlineDiv.appendChild(authorDiv);
+            authorDiv.appendChild(imgContainerDiv);
+                imgContainerDiv.appendChild(cardImage);
+            authorDiv.appendChild(cardSpan);
+
+
+    //text content 
+
+    return cardDiv;
+}
+
+const newFunc = createCard();
+
+console.log(newFunc);
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -1,9 +1,27 @@
 // Step 2: Create Tabs
 // -----------------------
-// Using axios send a GET request to the address: https://lambda-times-backend.herokuapp.com/topics
+// Using axios send a GET request to the 
+//address: https://lambda-times-backend.herokuapp.com/topics
 // Once the data is returned console.log it and review the structure.
 // Iterate over the topics creating a new Tab component and add it to the DOM
 // under the .topics element.
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const topicTabs = document.querySelector('.tabs');
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics');
+.then(response =>{
+    console.log(response.data.topics);
+    topicTabs.appendChild(topicsDiv(response.data.topics));
+})
+
+
+
+function topicsDiv(){
+    const tabDiv = document.createElement('div');
+    tabDiv.classList.add('tab');
+    
+    return tabDiv;
+}
