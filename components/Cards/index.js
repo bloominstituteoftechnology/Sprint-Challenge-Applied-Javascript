6335fb75857e568c5f0b7e51c6cdcf2a86778c62
Nick Verdina <nv1028@aggienetwork.com>
2019-10-13 19:47:35 -0500

    Corrected Topics Count

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,100 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+  function articleCreator(name, photo, headline){
+    let divC = document.createElement('div');
+    let divH = document.createElement('div');
+    let divA = document.createElement('div');
+    let divImg = document.createElement('div');
+    let spanA = document.createElement('span');
+    let imgSource = document.createElement('img');
+
+    divC.classList.add('card');
+    divH.classList.add('headline');
+    divA.classList.add('author');
+    divImg.classList.add('img-container');
+
+    divH.textContent = headline;
+    spanA.textContent = `By ${name}`;
+    imgSource.src = photo;
+
+    divC.appendChild(divH);
+    divC.appendChild(divA);
+    divA.appendChild(divImg);
+    divA.appendChild(spanA);
+    divImg.appendChild(imgSource);
+
+    return divC;
+  }
+
+  let cardsContainer = document.querySelector('.cards-container');
+
+  //bootstrap
+for (let i = 0; i< 3; i++){
+
+
+    axios.get(`https://lambda-times-backend.herokuapp.com/articles`)
+    .then((response)=> cardsContainer.appendChild(articleCreator(response["data"]["articles"]["bootstrap"][i]["authorName"],response["data"]["articles"]["bootstrap"][i]["authorPhoto"],response["data"]["articles"]["bootstrap"][i]["headline"])))
+    .catch(function (error) {
+            console.log(error);
+          });
+    
+}
+
+//javascript
+for (let i = 0; i< 4; i++){
+
+
+    axios.get(`https://lambda-times-backend.herokuapp.com/articles`)
+    .then((response)=> cardsContainer.appendChild(articleCreator(response["data"]["articles"]["javascript"][i]["authorName"],response["data"]["articles"]["javascript"][i]["authorPhoto"],response["data"]["articles"]["javascript"][i]["headline"])))
+    .catch(function (error) {
+            console.log(error);
+          });
+    
+}
+
+//jquery
+for (let i = 0; i< 3; i++){
+
+
+    axios.get(`https://lambda-times-backend.herokuapp.com/articles`)
+    .then((response)=> cardsContainer.appendChild(articleCreator(response["data"]["articles"]["jquery"][i]["authorName"],response["data"]["articles"]["jquery"][i]["authorPhoto"],response["data"]["articles"]["jquery"][i]["headline"])))
+    .catch(function (error) {
+            console.log(error);
+          });
+    
+}
+
+//node
+for (let i = 0; i< 2; i++){
+
+
+    axios.get(`https://lambda-times-backend.herokuapp.com/articles`)
+    .then((response)=> cardsContainer.appendChild(articleCreator(response["data"]["articles"]["node"][i]["authorName"],response["data"]["articles"]["node"][i]["authorPhoto"],response["data"]["articles"]["node"][i]["headline"])))
+    .catch(function (error) {
+            console.log(error);
+          });
+    
+}
+
+//technology
+for (let i = 0; i< 3; i++){
+
+
+    axios.get(`https://lambda-times-backend.herokuapp.com/articles`)
+    .then((response)=> cardsContainer.appendChild(articleCreator(response["data"]["articles"]["technology"][i]["authorName"],response["data"]["articles"]["technology"][i]["authorPhoto"],response["data"]["articles"]["technology"][i]["headline"])))
+    .catch(function (error) {
+            console.log(error);
+          });
+    
+}
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,30 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+// axios.get('https://lambda-times-backend.herokuapp.com/topics')
+//   .then(function (response) {
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+
+  function tabCreator(topicParam){
+    let divT = document.createElement('div');
+    divT.classList.add('tab');
+    divT.textContent = topicParam;
+    return divT;
+  }
+
+let tabTopicsElement = document.querySelector('.topics');
+console.log(tabTopicsElement);
+
+for (let i = 0; i<4; i++){
+
+axios.get(`https://lambda-times-backend.herokuapp.com/topics`)
+.then((response)=> tabTopicsElement.appendChild(tabCreator(response["data"]["topics"][i])))
+.catch(function (error) {
+        console.log(error);
+      });
+}
