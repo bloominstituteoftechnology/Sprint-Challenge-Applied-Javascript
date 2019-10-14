f96f3ae97db7111dc1fcfea623664d609e968475
rlom96 <rlom96@gmail.com>
2019-10-13 19:37:15 -0500

    updated tab function and roEach method to create newTab for each topic

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -8,14 +8,24 @@
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
 
+function Tab(topic) {
+  const newTab = document.createElement("div");
+  newTab.classList.add("tab");
+  newTab.textContent = topic;
+
+  return newTab;
+}
+
+const topics = document.querySelector(".topics");
+
 axios
   .get("https://lambda-times-backend.herokuapp.com/topics")
   .then(response => {
     console.log(response);
     response.data.topics.forEach(topic => {
       console.log(response);
-      const newTabEntry = new Tab();
-      tab.appendChild(newTabEntry);
+      const newTab = new Tab(topic);
+      topics.appendChild(newTab);
     });
   })
 
