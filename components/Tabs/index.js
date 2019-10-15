dcc7333272b7c91064ba24a9cd2b0a197355cab7
Ana Araujo <actanaaraujo@gmail.com>
2019-10-15 14:40:33 -0700

    Fiannly updating these changes git add .

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,26 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+function tabMaker(topic){
+    const newTab = document.createElement('div');
+    newTab.classList.add('tab');
+
+    newTab.textContent = topic;
+
+    return newTab;
+}
+
+topics = document.querySelector('.topics');
+
+
+axios
+    .get('https://lambda-times-backend.herokuapp.com/topics')
+    .then( response => {
+        response.data.topics.forEach( topic => {
+            let tab = tabMaker(topic);
+            topics.appendChild(tab);
+        })
+    })
+    .catch( err => {
+
+    })
