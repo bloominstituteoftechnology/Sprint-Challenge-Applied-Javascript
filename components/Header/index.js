7d1f16be38bab31ab9486956518e34f9912a9da8
stevedole808 <stevedolelambda@gmail.com>
2019-11-01 05:36:50 -1000

    finished header

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,28 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+
+    const header = document.createElement('div');
+    const date = document.createElement('span');
+    const title = document.createElement('h1');
+    const temp = document.createElement('span');
+  
+    header.appendChild(date);
+    header.appendChild(title);
+    header.appendChild(temp);
+  
+    header.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+  
+  
+    date.textContent = `MARCH 28, 2019`;
+    title.textContent = `Lambda Times`;
+    temp.textContent = `98°`;
+  
+  return header;
+  
+  }
+  const headerContainer = document.querySelector('.header-container');
+  headerContainer.appendChild(Header());
