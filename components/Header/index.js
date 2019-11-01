2c212db0605cfd5fab60e9d97ce1f82aa13b7db9
Kaela M Gallo <kaelamgallo@Kaelas-MBP.home>
2019-11-01 12:38:23 -0400

    header.js: created components

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,26 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+const headerContainer = document.querySelector('.header-container');
+  
+headerContainer.append(header);
+
+function Header() {
+    const topH = document.createElement('div'),
+          span = document.createElement('span'),
+          h1 = document.createElement('h1'),
+          span2 = document.createElement('span'),
+
+          topH.appendchild(span, h1, span2);
+
+          topH.classList.add("header");
+          span.classList.add("date");
+          span2.classList.add("temp");
+
+          span.textContent = "SMARCH 28, 2019";
+          h1.textContent = "Lambda Times";
+          span2.textContent = "98";
+ 
+          return topH;
+}
+
