229b48618e91b93cbda8e3566061ba89a81d127f
wchamber01 <52251104+wchamber01@users.noreply.github.com>
2019-11-01 16:30:32 -0400

    got tabs working

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -9,28 +9,32 @@
 //    <div class="tab">topic here</div>
 
 axios.get('https://lambda-times-backend.herokuapp.com/topics')
-.then(response => {
-    console.log(response);
-    newTab(response);
-})
+    .then(response => {
+        // console.log(response);
+        newTab(response);
+    })
 
-.catch(error => {
-    console.log('The data request was not returned!', error);
-});
+    .catch(error => {
+        console.log('The data request was not returned!', error);
+    });
 
 const tabs = document.querySelector('.topics');
+// console.log(tabs)
+
 
 function newTab(tabData) {
 
-const tab = document.createElement('div');
+    let arr = tabData.data.topics;
+    console.log(arr, 'arr');
 
-tab.classList.add('tab');
-
-
-
-const entryPoint = document.querySelector('.topics');
-entryPoint.appendChild(tab);
-
-return tab;
+    arr.forEach(item => {
+        console.log(item, 'item')
+        const tab = document.createElement('div');
+        tab.classList.add('tab');
+        tab.textContent = item;
+        const entryPoint = document.querySelector('.topics');
+        entryPoint.appendChild(tab);
+    });
+    return tabs;
 }
 
