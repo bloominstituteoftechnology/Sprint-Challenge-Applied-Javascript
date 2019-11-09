213c8f6355c6e74016344a332ac61754f6ae9818
Adrian Guadalupe <safisweetkeyz@gmail.com>
2019-11-09 14:41:23 -0500

    stretch attempt, then I gave up...

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,46 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+//Create Carousel Component
+const createCarousel = () => {
+
+  // Create Elements
+  const carousel = document.createElement('div');
+  const leftButton = document.createElement('div');
+  const mountains = document.createElement('img');
+  const computer = document.createElement('img');
+  const trees = document.createElement('img');
+  const turntable = document.createElement('img');
+  const rightButton = document.createElement('img');
+
+  // Element Content
+  mountains.src = './assets/carousel/mountains.jpeg';
+  computer.src = './assets/carousel/computer.jpeg';
+  trees.src = './assets/carousel/trees.jpeg';
+  turntable.src = './assets/carousel/turntable.jpeg';
+
+  //Nesting Elements
+  carousel.appendChild(leftButton);
+  carousel.appendChild(mountains);
+  carousel.appendChild(computer);
+  carousel.appendChild(trees);
+  carousel.appendChild(turntable);
+  carousel.appendChild(rightButton);
+
+  //Set Class Names
+  carousel.classList.add('carousel');
+  leftButton.classList.add('left-button');
+  rightButton.classList.add('right-button');
+
+  // Return Carousel
+  return carousel;
+};
+
+
+const carContainer = document.querySelector('carousel-container');
+const newCarousel = createCarousel();
+carContainer.appendChild(newCarousel);
+
+
+// I give up...
