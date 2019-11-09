a9df1b12e179620883b15eac17fd04950322a412
Adrian Guadalupe <safisweetkeyz@gmail.com>
2019-11-09 13:45:43 -0500

    stuck: images not displaying on cards. all other data is displaying fine

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,85 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+
+
+
+// Create Card Component
+const cardCreator = (article) => {
+
+   // Create Elements
+   const card = document.createElement('div');
+   const headline = document.createElement('div');
+   const author = document.createElement('div');
+   const imgContainer = document.createElement('div');
+   const img = document.createElement('img');
+   const name = document.createElement('span');
+
+
+   // Element Content
+   headline.textContent = article.headline;
+   img.scr = article.authorPhoto;
+   name.textContent = article.authorName;
+
+
+   // Nesting Elements
+   card.appendChild(headline);
+   card.appendChild(author);
+
+   author.appendChild(imgContainer);
+   author.appendChild(name);
+
+   imgContainer.appendChild(img);
+
+
+   //Set Class Names
+   card.classList.add('card');
+   headline.classList.add('headline');
+   author.classList.add('author');
+   imgContainer.classList.add('img-container');
+
+   // Return Card
+   return card;
+};
+
+
+
+
+// Select Cards Container
+const cardContainer = document.querySelector('.cards-container');
+
+// Get Article Data
+axios
+   .get('https://lambda-times-backend.herokuapp.com/articles')
+   .then(response => {
+      console.log(response.data.articles);
+      return response.data.articles;
+   })
+
+   // Return an Array of Articles, Sorted Into Arrays By Topic
+   .then(topics => {
+      const artByTopic = Object.values(topics)
+      console.log(artByTopic);
+      return artByTopic;
+   })
+
+   // Merge Sorted Arrays Into One Array of Articles
+   .then(artByTopic => {
+      const allArticles = artByTopic.reduce((a, b) => a.concat(b), []);
+      console.log(allArticles);
+      return allArticles;
+   })
+
+   // Create and Append a Card for Every Article in the Merged Array
+   .then(allArticles => {
+      allArticles.forEach(piece => {
+         console.log(piece.authorPhoto);
+         newArticle = cardCreator(piece);
+         cardContainer.appendChild(newArticle);
+      })
+   })
+
+   .catch(err => {
+      console.log('something went wrong', err);
+   });
