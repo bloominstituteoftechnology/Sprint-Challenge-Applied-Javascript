c534ba0b54512a71e84b4a4b91209bf735b83ff9
Ana Tulea <anatulea@MacBook-Pro-de-Ana.local>
2019-11-09 09:26:39 -0800

    Created the header, tabs, and cards.

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -16,4 +16,53 @@
 //   </div>
 // </div>
 //
+// Create a card for each of the articles and add the card to the DOM
+
+function cardMaker(arr){
+    // create all elements
+    const newCard = document.createElement('div');
+    const cardHeadline = document.createElement('div');
+    const cardAuthor = document.createElement('div');
+    const cardImg = document.createElement('div');
+    const cardPic =document.createElement('img');
+    const authorName =document.createElement('span');
+
+    // create structure
+    newCard.appendChild(cardHeadline);
+    newCard.appendChild(cardAuthor);
+    cardAuthor.appendChild(cardImg);
+    cardImg.appendChild(cardPic);
+    cardImg.appendChild(authorName);
+
+    // set the content
+    cardHeadline.textContent= arr.headline
+    cardPic.setAttribute('src', arr.authorPhoto)
+    authorName.textContent = `By ${arr.authorName}`
+
+    // apply styles
+    newCard.classList.add('card');
+    cardHeadline.classList.add('headline');
+    cardAuthor.classList.add('author');
+    cardImg.classList.add('img-container');
+    
+    return newCard;
+}
+
+//
 // Create a card for each of the articles and add the card to the DOM.
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+  .then(arr =>{
+    console.log(arr.data.articles)
+   const cardsContainer = document.querySelector('.cards-container')
+    
+    articleKeys = Object.keys(arr.data.articles)
+    
+    articleKeys.forEach(element => {
+      console.log(arr.data.articles[element])
+      arr.data.articles[element].forEach(x => {
+        cardsContainer.appendChild(cardMaker(x))
+      })
+    });
+})
+.catch(err => 
+  console.log(err))
