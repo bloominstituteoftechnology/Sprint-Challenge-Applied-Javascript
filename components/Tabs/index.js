9c40e9a13ce8d2dc4c07e14797e64ab3bf1fa6cc
Adrian Guadalupe <safisweetkeyz@gmail.com>
2019-11-09 12:50:13 -0500

    tabs done

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,40 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+
+// Create Tap Component
+const Tab = (topic) => {
+   // Create Element
+   const tab = document.createElement('div');
+
+   // Element Content
+   tab.textContent = topic;
+
+   // Set Class Name
+   tab.classList.add('tab');
+
+   // Return Tab
+   return tab;
+};
+
+
+
+// Get data
+axios
+   .get('https://lambda-times-backend.herokuapp.com/topics')
+   .then(response => {
+      // console.log(response);
+      return response.data.topics;
+   })
+   
+   // Create Topics Tabs
+   .then(topicsArray => {
+      // console.log(topicsArray);
+      const topics = document.querySelector('.topics');
+      topicsArray.forEach(topic => {
+         newTab = Tab(topic);
+         topics.appendChild(newTab);
+      })
+   })
