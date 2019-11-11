2df4247600a834576a59c5fc14d1031530415e98
tyler broadway <tbroadway96@gmail.com>
2019-11-10 20:42:04 -0600

    Cards complete. Project finished

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,66 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response => {
+        console.log(response)
+        let articles = response.data.articles
+        let js = articles.javascript
+        let boot = articles.bootstrap
+        let jquery = articles.jquery
+        let node = articles.node
+        let tech = articles.technology
+
+        js.forEach(element => {
+            document.querySelector('.cards-container').appendChild(CardCreator(element))
+        });
+
+        boot.forEach(element => {
+            document.querySelector('.cards-container').appendChild(CardCreator(element))
+        });
+
+        jquery.forEach(element => {
+            document.querySelector('.cards-container').appendChild(CardCreator(element))
+        });
+
+        node.forEach(element => {
+            document.querySelector('.cards-container').appendChild(CardCreator(element))
+        });
+
+        tech.forEach(element => {
+            document.querySelector('.cards-container').appendChild(CardCreator(element))
+        });
+    })
+
+    .catch(error => {
+        console.log(error)
+    })
+
+function CardCreator(data) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const headline = document.createElement('div');
+    headline.classList.add('headline');
+    headline.textContent = data.headline;
+    card.appendChild(headline);
+
+    const author = document.createElement('div');
+    author.classList.add('author');
+    card.appendChild(author);
+
+    const imgContainer = document.createElement('div');
+    imgContainer.classList.add('img-container');
+    author.appendChild(imgContainer);
+
+    const img = document.createElement('img');
+    img.src = data.authorPhoto;
+    imgContainer.appendChild(img);
+
+    const name = document.createElement('span');
+    name.textContent = `By ${data.authorName}`;
+    author.appendChild(name);
+
+    return card;
+}
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -10,9 +10,9 @@
 
 axios.get('https://lambda-times-backend.herokuapp.com/topics')
     .then(response => {
-        console.log(response)
+        //console.log(response)
         let topics = response.data.topics
-        console.log(topics)
+        //console.log(topics)
         topics.forEach(topic => {
             document.querySelector('.topics').appendChild(TabCreator(topic))
         })
