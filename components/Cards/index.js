54ca944f1c5ecefb9fda6196d5e54a1ff48071f2
Ana Tulea <anatulea@MacBook-Pro-de-Ana.local>
2019-11-11 17:27:34 -0800

    made the topic tab functional

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -16,16 +16,68 @@
 //   </div>
 // </div>
 //
-// Create a card for each of the articles and add the card to the DOM
+// Create a card for each of the articles and add the card to the DOM.
+axios
+    .get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response => {
+        console.log('here are the arrays of articles by toping', response.data.articles)
+        const cardsContainer = document.querySelector('.cards-container')
+        articleKeys = Object.keys(response.data.articles)
 
-function cardMaker(arr){
+        const tabTitle = document.querySelectorAll(".tab");
+        console.log("tabTitle", tabTitle[1]); //select individual tabs
+
+
+        //This renders all the articles form all the categories======>>>
+        // articleKeys.forEach(element => {
+        //   console.log("Array of a specific toping", response.data.articles[element])
+        //   response.data.articles[element].forEach(x => {
+        //     cardsContainer.appendChild(cardMaker(x))
+        //   })
+        // });
+        //============^^^^^^^^^
+
+        //This rensers  the articles from a specific category only when clicked on the tab=====>>>>
+        tabTitle[0].addEventListener('click', (e) => {
+            response.data.articles['javascript'].forEach(x => {
+                cardsContainer.appendChild(cardMaker(x))
+            })
+        })
+        tabTitle[1].addEventListener('click', (e) => {
+            response.data.articles['bootstrap'].forEach(x => {
+                cardsContainer.appendChild(cardMaker(x))
+            })
+        })
+        tabTitle[2].addEventListener('click', (e) => {
+            response.data.articles['technology'].forEach(x => {
+                cardsContainer.appendChild(cardMaker(x))
+            })
+        })
+        tabTitle[3].addEventListener('click', (e) => {
+            response.data.articles['jquery'].forEach(x => {
+                cardsContainer.appendChild(cardMaker(x))
+            })
+        })
+        tabTitle[4].addEventListener('click', (e) => {
+            response.data.articles['node'].forEach(x => {
+                cardsContainer.appendChild(cardMaker(x))
+            })
+        })
+        //==============^^^^
+
+    })
+    .catch(err =>
+        console.log(err))
+
+
+function cardMaker(arr) {
     // create all elements
     const newCard = document.createElement('div');
     const cardHeadline = document.createElement('div');
     const cardAuthor = document.createElement('div');
     const cardImg = document.createElement('div');
-    const cardPic =document.createElement('img');
-    const authorName =document.createElement('span');
+    const cardPic = document.createElement('img');
+    const authorName = document.createElement('span');
 
     // create structure
     newCard.appendChild(cardHeadline);
@@ -35,7 +87,7 @@ function cardMaker(arr){
     cardImg.appendChild(authorName);
 
     // set the content
-    cardHeadline.textContent= arr.headline
+    cardHeadline.textContent = arr.headline
     cardPic.setAttribute('src', arr.authorPhoto)
     authorName.textContent = `By ${arr.authorName}`
 
@@ -44,25 +96,6 @@ function cardMaker(arr){
     cardHeadline.classList.add('headline');
     cardAuthor.classList.add('author');
     cardImg.classList.add('img-container');
-    
+
     return newCard;
 }
-
-//
-// Create a card for each of the articles and add the card to the DOM.
-axios
-    .get('https://lambda-times-backend.herokuapp.com/articles')
-    .then(response =>{
-    console.log('here are the arrays of articles by toping', response.data.articles)
-    const cardsContainer = document.querySelector('.cards-container')
-    articleKeys = Object.keys(response.data.articles)
-    articleKeys.forEach(element => {
-      console.log("Array of a specific toping", response.data.articles[element])
-      response.data.articles[element].forEach(x => {
-        cardsContainer.appendChild(cardMaker(x))
-      })
-    });
-    
-})
-.catch(err => 
-  console.log(err))
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -24,4 +24,3 @@ axios.get(`https://lambda-times-backend.herokuapp.com/topics`)
     })
     .catch(err =>
         console.log(err));
-
