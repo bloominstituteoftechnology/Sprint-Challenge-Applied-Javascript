71a6b819c5d3e789a3da766377e584b78a845eca
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2019-12-06 13:29:10 -0500

    got the the tabs component working

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,27 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const topicTabs = document.querySelector('.topics');
+/*  I think this needs to be inside the axios 
+function tabComp(tabTopics) {
+    //create element
+    const topicHere = document.createElement('div');
+    //add class
+    topicHere.classList.add('tab');
+    //textContent
+    topicHere.textContent = 'topic here';
+};
+*/ 
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+.then (response => {
+    response.data.topics.forEach(element => {
+        let topicValue = document.createElement('div');//created the element
+        topicValue.classList.add('tab');//added the class
+        topicValue.textContent = element; // dont know how this works but work on the last assigment so I'm assuming it works
+        topicTabs.appendChild(topicValue);//this adds the topicsValue to the .topic div on the html
+    });
+})
+.catch(error => {
+    console.log('Something is not right.', error);//if something goes wrong it will print error
+})
