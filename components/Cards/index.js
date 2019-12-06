d3890b8d8a256d816cb0928213bf6c7fdebd5318
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2019-12-06 14:16:39 -0500

    completed the cards component and got it to work using axios

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,50 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const selectCard = document.querySelector('.cards-container');
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then(response => {
+    console.log(response);
+    for (const [key, value] of Object.entries(response.data.articles)) {
+        (key, value).forEach(elementItem => {
+            selectCard.appendChild(articlesCard(elementItem));
+        })
+    }
+})
+.catch(error => {
+    console.log('something went wrong', error);
+})
+
+function articlesCard(objData) {
+    //create elements
+    const cardBody = document.createElement('div');
+    const headLine = document.createElement('div');
+    const authorBody = document.createElement('div');
+    const imgContainer = document.createElement('div');
+    const img = document.createElement('img');
+    const authorName = document.createElement('span');
+
+    //structure
+    cardBody.appendChild(headLine);
+    cardBody.appendChild(authorBody);
+
+    authorBody.appendChild(imgContainer);
+    authorBody.appendChild(authorName);
+
+    imgContainer.appendChild(img);
+
+    //add classes
+    cardBody.classList.add('card');
+    headLine.classList.add('headline');
+    authorBody.classList.add('author');
+    imgContainer.classList.add('img-container');
+
+    //text content
+    headLine.textContent = objData.headline;
+    authorBody.textContent = objData.authorName;
+    img.src = objData.authorPhoto;
+
+    return cardBody;
+}
