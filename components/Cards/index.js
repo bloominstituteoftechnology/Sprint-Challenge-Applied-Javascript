0aeec701b0122d091ba3f31662336ee3f0cc4ee1
Anthony Streett <arjstreett@gmail.com>
2019-12-06 13:07:12 -0500

    Finshed last part of MVP

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,41 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+let container = document.querySelector(".cards-container");
+axios.get("https://lambda-times-backend.herokuapp.com/articles").then(response => {
+    for (var article in response.data.articles) {
+        for (var item in response.data.articles[article]) {
+            container.appendChild(CardComponent(response.data.articles[article][item]));
+        }
+    }
+})
+
+function CardComponent(data) {
+
+    let card = document.createElement("div");
+    card.classList.add("card");
+
+    let headline = document.createElement("div");
+    headline.textContent = data.headline;
+    headline.classList.add("headline");
+    card.appendChild(headline);
+
+    let author = document.createElement("div");
+    author.classList.add("author");
+    card.appendChild(author);
+
+    let imgCont = document.createElement("div");
+    imgCont.classList.add("img-container");
+    author.appendChild(imgCont);
+
+    let img = document.createElement("img");
+    img.src = data.authorPhoto
+    imgCont.appendChild(img);
+
+    let name = document.createElement("span");
+    name.textContent = `By ${data.authorName}`;
+    author.appendChild(name);
+
+    return card;
+}
