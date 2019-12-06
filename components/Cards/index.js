ae1d6f5ebf6fa55fcc1202d1df13830f1d13b418
Jonathan <56617776+qb1968@users.noreply.github.com>
2019-12-06 11:53:04 -0500

    Tabs and Cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,59 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+function newCard(data){
+    //main div
+    const cardDiv = document.createElement('div');
+    cardDiv.classList.add('card');
+
+    //headline div
+    const headlineDiv = document.createElement('div');
+    headlineDiv.classList.add('headline');
+    headlineDiv.textContent = data.headline;
+
+    //author div
+    const authorDiv = document.createElement('div');
+    authorDiv.classList.add('author');
+
+    //img Container 
+    const authorImgCont = document.createElement('div');
+    authorImgCont.classList.add('img-container');
+
+    //author image
+    const authorImg = document.createElement('img');
+    authorImg.src = data.authorPhoto;
+
+    //Author name 
+    const authorName = document.createElement('span');
+    authorName.textContent = data.authorName;
+
+    //append
+    cardDiv.appendChild(headlineDiv);
+    cardDiv.appendChild(authorDiv);
+
+    authorDiv.appendChild(authorImgCont);
+    authorDiv.appendChild(authorName);
+
+    authorImgCont.appendChild(authorImg);
+
+    const mainCardCont = document.querySelector('.cards-container');
+    mainCardCont.appendChild(cardDiv);
+
+    return cardDiv;
+
+
+}
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles') 
+    .then((response) => { 
+        console.log(response);
+        response.data.articles.javascript.forEach(article => newCard(article));
+        response.data.articles.bootstrap.forEach(article => newCard(article));
+        response.data.articles.jquery.forEach(article => newCard(article));
+        response.data.articles.node.forEach(article => newCard(article));
+        response.data.articles.technology.forEach(article => newCard(article));
+    })
+
+    .catch((err) => { 
+        console.log(err) 
+    })
