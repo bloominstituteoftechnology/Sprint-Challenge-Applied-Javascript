c9c9dd0466afcba26c042e95379cf101a280aefa
unknown <jabaritf@gmail.com>
2019-12-16 14:08:00 -0500

    finished Step 1 header

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,33 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+
+function Header() {
+};
+
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('header');
+
+    const newSpan = document.createElement('span');
+    newSpan.classList.add('date');
+    newSpan.textContent = 'SMARCH 28, 2019';
+
+    const newH1 = document.createElement('h1');
+    newH1.textContent='Lambda Times';
+
+    const newSpan2 = document.createElement('span');
+    newSpan2.classList.add('temp');
+    newSpan2.textContent = "98°";
+
+    const headerStuff = document.querySelector('.header-container');
+    headerStuff.appendChild(newDiv);
+    newDiv.appendChild(newSpan);
+    newDiv.appendChild(newH1);
+    newDiv.appendChild(newSpan2);
+
+
+
+
+
+
+
