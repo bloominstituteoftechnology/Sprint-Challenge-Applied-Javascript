c6537dfc25e89c2ee972d4be667e8f83e971dc75
Drew Tora <drewtora@live.com>
2019-12-15 19:27:11 -0500

    MVP complete

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,48 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+  .then((response) => {
+    console.log(response);
+    let cardsCont = document.querySelector(".cards-container");
+    let arrayValues = Object.values(response.data.articles);
+    arrayValues.forEach(element => {
+      element.forEach(article => {
+        cardsCont.appendChild(createCard(article));
+        console.log("CreatedCard");
+      })
+    })
+  })
+  .catch((error) => {
+
+  })
+
+function createCard(object){
+
+  let div = document.createElement("div");
+  div.classList.add("card");
+
+  let div2 = document.createElement("div");
+  div2.classList.add("headline");
+  div2.innerHTML = object.headline;
+  div.appendChild(div2);
+
+  let div3 = document.createElement("div");
+  div3.classList.add("author");
+  div.appendChild(div3);
+
+  let div4 = document.createElement("div");
+  div4.classList.add("img-container");
+  div3.appendChild(div4);
+
+  let img = document.createElement("img");
+  img.src = object.authorPhoto;
+  div4.appendChild(img);
+
+  let span = document.createElement("span");
+  span.innerHTML = "By " + object.authorName;
+  div3.appendChild(span);
+
+  return div;
+}
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,24 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+  .then((response) => {
+    let dom = document.querySelector(".topics");
+    response.data.topics.forEach(element => {
+      dom.appendChild(createTab(element));
+    })
+  })
+  .catch((error) => {
+    console.log("Network Error");
+    console.log(error);
+  })
+
+function createTab(object) {
+
+    let div = document.createElement("div");
+    div.classList.add("tab");
+    div.innerHTML = object;
+
+    return div;
+}
