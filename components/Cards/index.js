c1690e976b62240e2502c29e49ac8c837d8346aa
Mirage Kamran <mirage.kamran.pkt@gmail.com>
2019-12-15 17:53:19 -0800

    sprint updated

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,42 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios
+    .get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(res => {
+        console.log(res);
+        const cardArr = res.data.articles;
+        cardArr.forEach((authorCard) => {
+            const newCard = updateArticles(authorCard);
+            headerContainer.appendChild(newCard);
+        });
+        console.log(cardArr);
+
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
+function updateArticles() {
+    // creating elements
+    const card = document.createElement('div');
+    const cardHeadline = document.createElement('div');
+    const author = document.createElement('div');
+    const imageContainer = document.createElement('div');
+    const image = document.createElement('img');
+    const authorName = document.createElement('span');
+
+    // appending to the header div
+    card.appendChild(cardHeadline);
+    card.appendChild(author);
+    author.appendChild(imageContainer);
+    imageContainer.appendChild(image);
+    author.appendChild(authorName);
+
+    // adding classes back in
+    card.classList.add('card');
+    cardHeadline.classList.add('headline');
+    author.classList.add('author');
+    imageContainer.classList.add('img-container');
+}
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -8,12 +8,23 @@
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
 
+const topicsArr = document.querySelector('.topics');
+
 axios
     .get('https://lambda-times-backend.herokuapp.com/topics')
     .then(res => {
         console.log(res)
+        const newTab = res.data.topics;
+        newTab.push('aNewTab');
     })
+        
 
     .catch(err => {
         console.log(err);
     })
+
+function addTab() {
+    const tab = document.createElement('div');
+    tab.classList.add('tab');
+
+}
