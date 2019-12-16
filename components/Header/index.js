b85ad1a582a1ae3cc2ae034258bf108486ed39aa
Michelle Tinajero <elletinajero11@gmail.com>
2019-12-15 19:57:45 -0600

    mpv completed

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,26 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+Const headerCont = document.querySelector('.header-container');
+console.log(headerCont); 
+
+const component = Header();
+headerCont.appendChild(component);
+
+function Header() {  //function that will hold the component//
+    
+    const header = document.createElement('div'); //created variable and set that equal to the created of the div element//
+    header.classList.add('header'); //now you can use the variable you created and add a class to what the variable equals which is a created div//
+
+    const date = document.createElement('span');
+    date.classList.add('date');
+    date.textContent = "SMARCH 28, 2019";
+    header.appendChild(date);
+
+    const temp = document.createElement('span');
+    temp.classList.add('temp');
+    temp.textContent = "98°";
+    header.appendChild(temp);
+
+    return header;
+}
