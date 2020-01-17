7ccf0c2622ffb8cf304783cdb4bb2af7367e8d82
richknicks <rlynch@gmail.com>
2020-01-17 12:15:13 -0500

    create cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,37 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+axios.get("https://lambda-times-backend.herokuapp.com/articles")
+.then(response => {
+    console.log(response.data.articles);
+    for(const puller =  )
+
+
+})
+
+function createCards (){
+    const containerDiv = document.createElement('div'),
+          headlineDiv = document.createElement('div'),
+          authorDiv = document.createElement('div'),
+          imgC = document.createElement('div'),
+          imgE = document.createElement('img'),
+          spanE = document.createElement('span');
+
+          containerDiv.append(headlineDiv);
+          containerDiv.append(authorDiv);
+          authorDiv.append(imgC);
+          imgC.append(imgE);
+          authorDiv.append(spanE);
+
+          containerDiv.classList.add('card');
+          headlineDiv.classList.add('headline');
+          authorDiv.classList.add('author');
+          imgC.classList.add('img-container');
+
+
+
+          
+
+          return createCards;
+        }
+        const getCards = document.querySelector('.topics');
