11ff018844f86c904e30676f0a2bd1ba2b474289
Zach Stack <stackz.personal@gmail.com>
2020-01-17 11:52:14 -0800

    MVP

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -6,6 +6,59 @@
 // This won't be as easy as just iterating over an array though.
 // Create a function that will programmatically create the following DOM component:
 //
+
+//
+// Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+
+.then(response => {
+    const cardsContainer = document.querySelector('.cards-container')
+    const values = Object.values(response.data.articles);
+    console.log("Card response", response)
+    values.forEach(item => {
+    item.forEach(article => {
+    cardsContainer.appendChild(createCard(article))
+    console.log("Card")
+    })
+})
+})
+
+.catch(error => {
+console.log("Surprise, nothing works.", error)
+})
+
+
+
+    
+
+
+
+function createCard (object) {
+    const card = document.createElement('div'),
+          headline = document.createElement('div'),
+          author = document.createElement('div'),
+          imgContainer = document.createElement('div'),
+          imgSrc = document.createElement('img'),
+          span = document.createElement('span')
+
+    card.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author');
+    imgContainer.classList.add('img-container');
+
+    headline.textContent = object.headline;
+    imgSrc.src = object.authorPhoto;
+    span.textContent = `By ${object.authorName}`;
+
+    card.append(headline);
+    card.append(author);
+    author.append(imgContainer);
+    imgContainer.append(imgSrc);
+    author.append(span);
+
+    return card
+};
 // <div class="card">
 //   <div class="headline">{Headline of article}</div>
 //   <div class="author">
@@ -15,13 +68,3 @@
 //     <span>By {authors name}</span>
 //   </div>
 // </div>
-//
-// Create a card for each of the articles and add the card to the DOM.
-
-axios.get('https://lambda-times-backend.herokuapp.com/articles')
-    .then(response => {
-
-    })
-    .catch(error => {
-        console.log("Surprise, nothing works.", error)
-    })
