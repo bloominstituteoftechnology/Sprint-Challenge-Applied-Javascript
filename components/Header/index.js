815b9ea39210b2f13a230853643a99b06797b988
Your Name <you@example.com>
2020-01-17 12:42:51 -0500

    added step one of MVP

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,29 @@
 
 //ready to start project, first commit to start PR according to Sprint Instructions.
 
-function Header() {}
+function Header() {
+    //creating elements
+    let header = document.createElement('div'),
+        date = document.createElement('span'),
+        time = document.createElement('h1'),
+        temp = document.createElement('span');
+
+    //adding class(s) where needed
+    header.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+
+    //Structuring of elements
+    header.append(date);
+    header.append(time);
+    header.append(temp);
+
+    //adding content to files
+    date.textContent = "March 28, 2019";
+    time.textContent = "Lambda Times";
+    temp.textContent = "98°"
+
+return header;
+}
+let headerContainer = document.querySelector('.header-container');
+headerContainer.append(Header())
