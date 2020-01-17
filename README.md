93da63e4798acd5c01b8c55d9fc3bb38d62f2559
Zach Stack <stackz.personal@gmail.com>
2020-01-17 08:50:01 -0800

    Complete header

--- README.md
+++ README.md
@@ -53,8 +53,8 @@ An element built with HTML, CSS and Javascript so that it can be reused througho
 
 * [x] Fork the project into your GitHub user account
 * [x] Clone the forked project into a directory on your machine
-* [ ] Create a pull request before you start working on the project requirements.  You will continuously push your updates throughout the project.
-* [ ] You are now ready to build this project with your preferred IDE
+* [x] Create a pull request before you start working on the project requirements.  You will continuously push your updates throughout the project.
+* [x] You are now ready to build this project with your preferred IDE
 
 ## Minimum Viable Product
 
--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,26 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+    const header = document.createElement('div'),
+          date = document.createElement('span'),
+          headline = document.createElement('h1'),
+          temp = document.createElement('span')
+
+    header.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+
+    header.append(date);
+    header.append(headline);
+    header.append(temp);
+
+    date.textContent = "SMARCH 28, 2019";
+    headline.textContent = "Lambda Times";
+    temp.textContent = "98°";
+
+    return header;
+
+}
+const headerContainer = document.querySelector('.header-container');
+headerContainer.append(Header());
