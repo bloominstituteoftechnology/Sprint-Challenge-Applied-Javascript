8b09e22afa907661641f0b82c159635c1cde5bc5
NaJthaGod <35042135+NaJthaGod@users.noreply.github.com>
2020-01-17 13:51:31 -0500

    incomplete

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,43 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get( 'https://lambda-times-backend.herokuapp.com/articles')
+.then(response => {
+    response.data.articles.forEach(element => {
+        cardsContainer.appendChild(createArticle1(element))
+    });
+  console.log(response);
+})
+.catch( error => {
+  // console.log("the data was not returned", error)
+})
+
+const cardsContainer = document.querySelector('cards-container')
+
+function createArticle1 (JavaSciptdata){
+
+    const card = document.createElement('div')
+    const headLine = document.createElement('div')
+    const headLineTitle = document.createElement('h1')
+    const author = document.createElement('div')
+    const imgContainer = document.createElement('div')
+    const authorImg = document.createElement('img')
+    const authorsName = document.createElement('span')
+
+    card.appendChild(headLine)
+    card.appendChild(author)
+    headLine.appendChild(headLineTitle)
+    author.appendChild(imgContainer)
+    author.appendChild(authorsName)
+    imgContainer.appendChild(authorImg)
+
+    card.classList.add('card')
+    headLine.classList('headLine')
+    author.classList.add('author')
+    imgContainer.classList.add('img-container')
+
+    headLineTitle.textContent = JavaScriptdata.headLine
+
+
+}
