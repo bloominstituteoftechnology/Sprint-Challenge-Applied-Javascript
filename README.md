62b38657741360a19edc7a88f044475ed26b05f3
Alicia Petgrave <alicia.petgrave@gmail.com>
2020-01-17 11:20:03 -0500

    adding component, appends, classes, and text content to Header index.js

--- README.md
+++ README.md
@@ -30,6 +30,8 @@ Edit this document to include your answers after each question. Make sure to lea
 
 1. What is the DOM?
 
+The DOM (Document Object Model) is an object representation of a webpage.
+
 2. What is an event?
 
 3. What is an event listener?
--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,30 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+    const headerDiv = document.createElement('div');
+    const date = document.createElement('span');
+    const h1 = document.createElement('h1');
+    const temp = document.createElement('span');
+
+
+    //append
+    headerDiv.append(date);
+    headerDiv.append(h1);
+    headerDiv.append(temp);
+
+    //classes
+    headerDiv.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+
+    //text content
+    date.textContent = 'SMARCH 28, 2019';
+    h1.textContent = 'Lambda Times';
+    temp.textContent = '98°';
+
+    return headerDiv;
+}
+
+const newspaper = document.querySelector('.header-container');
+newspaper.append(Header());
