c29f7e21cfebac336193004ca19a8fd16b191eb1
Pedro Revilla <peterevilla@gmail.com>
2020-01-24 22:23:44 -0600

    made mvp

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,32 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+
+let header = document.createElement('div')
+let date = document.createElement('span')
+let title = document.createElement('h1')
+let temp = document.createElement('span')
+
+header.appendChild(date);
+header.appendChild(title);
+header.appendChild(temp);
+
+header.classList.add('header')
+date.classList.add('date')
+temp.classList.add('temp')
+
+
+date.textContent = 'MARCH 28, 2019';
+title.textContent = 'Lambda Times';
+temp.textContent = '98°';
+
+return header;
+
+}
+
+let head = document.querySelector('.header-container');
+
+
+head.appendChild(Header());
+
