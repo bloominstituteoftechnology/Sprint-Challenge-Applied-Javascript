064b9e6d45963895852162e054d782d56195f867
Steven Waldron <stevenwaldron@Stevens-MBP.home>
2020-01-25 15:29:26 -0500

    not MVP

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,27 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+    let header = document.createElement('div');
+    let date = document.createElement('span');
+    let h1 = document.createElement('h1');
+    let temp = document.createElement('span');
+
+    header.classList.add('header');
+    date.classList.add('date');
+    date.textContent = 'MARCH 28, 2019';
+    h1.textContent = 'Lambda Times';
+    temp.classList.add('temp');
+    temp.textContent = '98°';
+
+    header.appendChild(date);
+    header.appendChild(h1);
+    header.appendChild(temp);
+
+    return header;
+
+}
+
+const headerContainer = document.querySelector('.header-container');
+
+headerContainer.appendChild(Header());
