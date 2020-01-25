d000b9f6ee029b9c2dec00d398fba60545719986
Donna Ballew <ballewdd@gmail.com>
2020-01-24 20:43:00 -0800

    finished step 2

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,21 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const topics = document.querySelector(".topics");
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/topics")
+  .then(response => {
+    console.log(response);
+    response.data.topics.forEach(element => {
+      const newTab = document.createElement("div");
+      topics.appendChild(newTab);
+      newTab.classList.add("tab");
+      newTab.textContent = element;
+    });
+  })
+
+  .catch(error => {
+    console.log("Oops! We can't find the data");
+  });
