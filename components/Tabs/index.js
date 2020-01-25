b1031832a310b2e42841acf204c99f76747c04f6
Jeff Clark <jeffclarktopeka@yahoo.com>
2020-01-24 22:54:16 -0600

    began creating the component for tabs index.js

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,10 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const tabsEntryPoint = document.querySelector('tabs');
+const axiosPromise = axios.get('https://lambda-times-backend.herokuapp.com/topics');
+
+console.log(axiosPromise);
+
+
