3835550748cb174b02743275fdf37adac7d9ad57
Anthony <chitowncode@gmail.com>
2020-01-26 22:37:53 -0600

    I have finished calling the cards, images and text is showing as they should,  I have completed the Sprint, I believe I have hit MVP.

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,45 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+const cardContainer = document.querySelector('.cards-container')
+
+function Card(arr) {
+    const card = document.createElement('div');
+    const cardHeadline = document.createElement('div');
+    const cardAuthorDiv = document.createElement('div');
+    const cardImgDiv = document.createElement('div');
+    const cardImg = document.createElement('img');
+    const cardAuthor = document.createElement('span')
+
+    cardHeadline.textContent = arr.headline;
+    cardImg.src = arr.authorPhoto;
+    cardAuthor.textContent = arr.authorName;
+
+    card.classList.add('card');
+    cardHeadline.classList.add('headline');
+    cardImgDiv.classList.add('img-container');
+    cardAuthorDiv.classList.add('author')
+
+    card.appendChild(cardHeadline);
+    card.appendChild(cardAuthorDiv);
+    cardAuthorDiv.appendChild(cardImgDiv);
+    cardImgDiv.appendChild(cardImg)
+    cardAuthorDiv.appendChild(cardAuthor);
+
+    return card;
+}
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response => {
+        console.log(response.data.articles);
+        const articles = response.data.articles;
+        const articlesKeys = Object.keys(articles);
+        articlesKeys.forEach(key => {
+            // console.log(articles[key]);
+            articles[key].forEach(article => {
+                cardContainer.appendChild(Card(article))
+            })
+        });
+        console.log(Object.keys(articles));
+    })
+    .catch(error => console.log('error :', error))
