7c165d71b80be543f0ac3837e140d40446f73676
unknown <william.z.wilson95@gmail.com>
2020-02-14 11:23:44 -0600

    did it all again

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -1,6 +1,46 @@
 // STEP 3: Create Article cards.
+function createCard (data){
+    const cardcontainer = document.createElement('div')
+    const headlineDiv = document.createElement('div')
+    const author = document.createElement('div')
+    const imgCont = document.createElement('div')
+    const img = document.createElement('img')
+    const span = document.createElement('span')
+  
+    cardcontainer.append(headlineDiv)
+    cardcontainer.append(author)
+    author.append(imgCont)
+    imgCont.append(img)
+    author.append(span)
+  
+    cardcontainer.classList.add('card')
+    headlineDiv.classList.add('headline')
+    author.classList.add('author')
+    imgCont.classList.add('img-container')
+  
+    headlineDiv.textContent = `${data.headline}`
+    img.src = data.authorPhoto
+    span.textContent = `by: ${data.authorName}`
+  
+    return cardcontainer
+    }
+
+    let entry = document.querySelector('.cards-container')
 // -----------------------
 // Send an HTTP GET request to the following address: https://lambda-times-backend.herokuapp.com/articles
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then(response => {
+    
+    Object.values(response.data.articles).forEach(item => {
+        item.forEach(items => {
+            entry.append(createCard(items))
+        })
+    });
+        
+})
+.catch( err => {
+    console.log('the error is', err)
+  })
 // Stduy the response data you get back, closely.
 // You will be creating a component for each 'article' in the list.
 // This won't be as easy as just iterating over an array though.
