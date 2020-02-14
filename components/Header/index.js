2bd8b1d697910482feebc8274f570c12cd168914
Your Name <you@example.com>
2020-02-14 11:53:12 -0500

    added comments for clarity, returned parent element inside function, appending it to header container

--- components/Header/index.js
+++ components/Header/index.js
@@ -10,18 +10,27 @@
 // And add it to the DOM in the .header-container component
 
 function Header() {
+    //Creating Elements
     let header = document.createElement('div'),
         date = document.createElement('span'),
         name = document.createElement('h1'),
         temp = document.createElement('span');
     
+    //Adding Classes
     header.classList.add('header');
     date.classList.add('date');
     temp.classList.add('temp');
 
+    //Appending Elements
     header.append(date, name, temp)
 
+    //textContent
     date.textContent = 'March 28, 2019';
     name.textContent = 'Lambda Times';
     temp.textContent = '98°';
+
+    return header; //returning parent element of function
 }
+
+let headerContainer = document.querySelector('.header-container'); //Getting parent div from html using its className
+headerContainer.append(Header())
