465d96ac2895753a659864cdcb30e3eab17ff325
Your Name <you@example.com>
2020-02-14 12:15:48 -0500

    added axios request

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,12 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then(res => {
+    console.log(res)
+    console.log(res.data)
+})
+.catch(err => {
+    console.log('no data found', err);
+})
