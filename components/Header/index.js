1c359c3fb158d366a3031d8cde73109aa01f5ff4
azrael3069 <cmrivera0004@student.stcc.edu>
2020-02-21 19:49:17 -0500

    header added

--- components/Header/index.js
+++ components/Header/index.js
@@ -8,3 +8,33 @@
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .header-container component
+
+function Header() {
+  const header = document.createElement("div");
+  const date = document.createElement("span");
+  const heading = document.createElement("h1");
+  const temperature = document.createElement("span");
+
+  var today = new Date();
+  var timeFormat = { month: "long", day: "numeric", year: "numeric" };
+  date.textContent = today
+    .toLocaleDateString("en-US", timeFormat)
+    .toUpperCase();
+  heading.textContent = "Lambda Times";
+  temperature.textContent = "98°";
+
+  //creating content structure
+
+  header.appendChild(date);
+  header.appendChild(heading);
+  header.appendChild(temperature);
+
+  header.classList.add("header");
+  date.classList.add("date");
+  temperature.classList.add("temp");
+
+  return header;
+}
+
+const headerContainer = document.querySelector(".header-container");
+headerContainer.appendChild(Header());
