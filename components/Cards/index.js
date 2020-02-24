1258d690955949a7efcb04d255378af28a5224cd
Lindsey Henager <lindseyhenager@lindseys-air.attlocal.net>
2020-02-23 18:53:59 -0800

    made an article function

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,53 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+function articleCards(object){
+  const newCard = document.createElement("div");
+  newCard.classList.add("card");
+  newCard.textContent = object;
+  const headline = document.createElement("div");
+  headline.textContent = object;
+  const author = document.createElement("div");
+  author.classList.add("author")
+  const imageContainer = document.createElement("div");
+  imageContainer.classList.add("img-container");
+  const image = document.createElement("img");
+  image.src = object;
+  const authName = document.createElement("span");
+  authName.textContent = `By: ${object}`
+
+  newCard.appendChild(headline);
+  newCard.appendChild(author);
+  author.appendChild(imageContainer);
+  imageContainer.appendChild(image);
+  author.appendChild(authName);
+ return newCard;
+}
+
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then((response) => {
+  console.log(response);
+  //for(const key in response.data.articles){
+     //console.log(key);
+     // key.forEach((article)=>{
+         //let newArticle = newCard(article);
+        //for (const property in response.data.articles) {
+          ////  console.log(property);
+          //  response.data.article.property.forEach((article) => {
+              //  let newArticle = articleCards(article);
+               // const cardsContainer = document.querySelector('cards-container');
+               // cardsContainer.appendChild(newArticle);
+          //  })
+         
+           console.log(response.data.articles);
+
+    
+
+          })
+//})
+    
+
+.catch((err) => { 
+    console.log(err) 
+})
