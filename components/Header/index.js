733d39b8eac383b248bd34a92682c4629a8c5cde
ngriffith29 <ngriffith29@student.egcc.edu>
2020-02-23 20:27:54 -0500

    finished sprint

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,30 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+const tops = document.querySelector(".header-container");
+
+function Header() {
+  // element creation
+  let header = document.createElement("div");
+  let date = document.createElement("span");
+  let title = document.createElement("h1");
+  let temp = document.createElement("span");
+
+  // class additions
+
+  header.classList.add("header");
+  date.classList.add("date");
+  temp.classList.add("temp");
+  // text
+  date.textContent = "SMARCH 28, 2019";
+  title.textContent = "Lambda Times";
+  temp.textContent = "98°";
+
+  header.appendChild(date);
+  header.appendChild(title);
+  header.appendChild(temp);
+
+  return tops.appendChild(header);
+}
+
+Header();
