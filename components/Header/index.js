2b0c1cc59915b0505d994b754308a8083527324b
Ahmad Wali Sayadi <sayadi@Ahmads-MacBook-Pro.local>
2020-02-23 16:47:26 -0800

    created-header-component

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,40 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+
+
+
+
+function Header() {
+
+
+const header = document.createElement('div');
+const date = document.createElement('span');
+const title = document.createElement('h1');
+const temp = document.createElement('span');
+
+header.appendChild(date);
+header.appendChild(title);
+header.appendChild(temp);
+
+
+header.classList.add('header');
+date.classList.add('date');
+temp.classList.add('temp');
+
+date.textContent = 'MARCH 28,2019';
+title.textContent = 'Lamda Times';
+temp.textContent = '98';
+
+
+
+return header
+
+}
+
+const headerContent = document.querySelector('.header-container');
+
+headerContent.appendChild(Header);
+
+
+
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,6 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+axios.get
