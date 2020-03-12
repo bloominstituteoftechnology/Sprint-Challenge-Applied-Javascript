dc3f742c2c8a2a79f4132e94a74de101e0735ac1
matthew-heideman <heidemanmr@yahoo.com>
2020-03-12 17:37:54 -0500

    completed cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -20,5 +20,45 @@
 
 axios.get('https://lambda-times-backend.herokuapp.com/articles')
 .then(response => {
-    
+    console.log(response)
+    response.data.articles.javascript.forEach(item => {
+        cardParent.appendChild(createArticle(item))
+    });
+    response.data.articles.bootstrap.forEach(item => {
+        cardParent.appendChild(createArticle(item))
+    });
+    response.data.articles.technology.forEach(item => {
+        cardParent.appendChild(createArticle(item))
+    });
+    response.data.articles.jquery.forEach(item => {
+        cardParent.appendChild(createArticle(item))
+    });
+    response.data.articles.node.forEach(item => {
+        cardParent.appendChild(createArticle(item))
+    });
 })
+.catch(error => {
+    console.log(error ,'the data was not retrieved')
+})
+function createArticle(articleData) {
+    const card = document.createElement('div')
+    const headline = document.createElement('div')
+    const author = document.createElement('div')
+    const imgContainer = document.createElement('div')
+    const img = document.createElement('img')
+    const by = document.createElement('span')
+    card.classList.add('card')
+    headline.classList.add('headline')
+    author.classList.add('author')
+    imgContainer.classList.add('img-container')
+    headline.textContent = articleData.headline;
+    by.textContent = `By ${articleData.authorName}`
+    img.src = articleData.authorPhoto
+    card.appendChild(headline)
+    card.appendChild(author)
+    author.appendChild(imgContainer)
+    imgContainer.appendChild(img)
+    author.appendChild(by)
+    return card
+}
+const cardParent = document.querySelector('.cards-container')
