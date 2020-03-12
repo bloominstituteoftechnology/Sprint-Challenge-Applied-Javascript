e9638a4f258304bf58ae4a1c979029df6cb5b0f2
matthew-heideman <heidemanmr@yahoo.com>
2020-03-12 15:33:11 -0500

    working on header component

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,14 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+    const header = document.createElement('div')
+    const date = document.createElement('span')
+    const title = document.createElement('h1')
+    const temp = document.createElement('span')
+    header.classList.add('header')
+    date.classList.add('date')
+    temp.classList.add('temp')
+    title.textContent = 'Lambda Times'
+    return header;
+}
