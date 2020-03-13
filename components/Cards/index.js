29953ac7ffdd80db2618c086b6744c4bc0f2ac3b
Enclosed Letter <C.LuisMartinez.A@gmail.com>
2020-03-13 11:02:29 -0700

    working on cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,70 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+function articleCard(data){
+    // console.log(data);
+    const card = document.createElement("div");
+    const headline = document.createElement("div");
+    const author = document.createElement("div");
+    const imgContainer = document.createElement("div");
+    const authorImg = document.createElement("img");
+    const authorName = document.createElement("span")
+
+// append
+    card.append(headline, author);
+    author.append(imgContainer, authorName);
+    imgContainer.append(authorImg); //do we need quotation marks?
+
+//add classes
+    card.classList.add("card");
+    headline.classList.add("headline");
+    author.classList.add("author");
+    imgContainer.classList.add("img-container");
+// add attributes
+    authorImg.src = data.data.authorPhoto;
+// add content
+headline.textContent = data.data.headline;
+authorName.textContent = `By ${data.data.authorName}`;
+
+return card;
+}
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then(response => {
+    console.log(response);
+    });
+
+
+    // axios
+    // .get("https://dog.ceo/api/breed/mastiff/images/random/12")
+    // .then(response => {
+    //   console.log(response);
+    //   response.data.message.forEach(item => {
+    //     entryPoint.append(DogCard(item))
+    //   });
+    // })
+    // .catch(error => {
+    //   console.log("The data was not returned", error);
+    // });
+
+
+
+
+
+
+
+// const connectToCardsContainer = document.querySelector(".cards-container"); //you are creating the referance to the HTML
+
+
+
+// axios
+//     .get("https://lambda-times-backend.herokuapp.com/articles")
+//     .then(response=>{
+//         Object.values(response.data.articles).forEach(item => {
+//             item.forEach()
+//         })
+//     })
+
+// headContainer.append(articleCard());
