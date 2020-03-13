9e343923462f20ee540d59c2135279fbf67cbc15
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2020-03-13 09:49:22 -0700

    Header is built, working on tabs

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,16 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(response => {
+        console.log(response);
+        response.data.topics.forEach(topic => {
+            console.log(response);
+            const newTabEnt = new newTab();
+            tab.appendChild(newTabEnt);
+        })
+    })
+    .catch(error => {
+        console.log(error);
+    });
