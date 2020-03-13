101a7dd26e277b6181cd77ecdbddfeb48536a850
Vladyslav Horbachenko <vladyslavhorbachenko@Vladyslavs-MacBook-Pro.local>
2020-03-13 10:30:59 -0700

    done sprint

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,15 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+topics = [];
+axios.get("https://lambda-times-backend.herokuapp.com/topics")
+.then(response => {
+    response.data.topics.forEach(e => {
+        let divy = document.createElement("div");
+        divy.classList.add("tab");
+        divy.textContent = `${e}`;
+        document.querySelector(".topics").appendChild(divy);
+    })
+})
