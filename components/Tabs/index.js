a46558038c3e712fcf00d73b468c339f10010aef
Spencer_Vail <vail.spencer@gmail.com>
2020-03-20 20:07:57 -0700

    MVP Done

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,31 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+const createTab = (tabData) =>{
+
+    const topic = document.createElement("div");
+    
+    topic.classList.add("tab");
+    topic.textContent = tabData;
+
+    return topic;
+
+}
+
+const tabHook = document.querySelector('.topics');
+const axiosPromise = axios.get("https://lambda-times-backend.herokuapp.com/topics");
+
+
+axiosPromise.then((response) => {
+
+const topicArray = response.data.topics;
+
+
+    topicArray.forEach((item)=>{
+        const newTab = createTab(item);
+        tabHook.appendChild(newTab);  
+    })
+
+});
