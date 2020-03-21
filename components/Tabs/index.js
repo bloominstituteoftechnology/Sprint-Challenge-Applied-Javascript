3ac5bd7bfe9fdfb2a291546721c59c14c038124d
thayne-allen <59717714+thayne-allen@users.noreply.github.com>
2020-03-20 20:07:26 -0600

    added tabs

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,26 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+.then((response) => {
+    const topics = response.data.topics;
+    topics.forEach(element => {
+        tabs(element);
+    });
+})
+.catch((error) => {
+    console.log(error);
+})
+
+
+function tabs(textCon) {
+    const topics = document.querySelector('.topics');
+    let tabDiv = document.createElement('div');
+    tabDiv.classList.add('tab');
+    tabDiv.textContent = textCon;
+    topics.appendChild(tabDiv);
+
+    return tabDiv;
+
+}
