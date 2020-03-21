08ad6080441d2a8b8cff9d2488ab90235b9ea5c5
thayne-allen <59717714+thayne-allen@users.noreply.github.com>
2020-03-20 21:49:21 -0600

    attempted carousel, didn't finish

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,56 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+function carousel() {
+
+  const carouselContainer = document.querySelector('.carousel-container');
+  const carouselDiv = document.createElement('div');
+  carouselDiv.classList.add('carousel');
+  carouselContainer.appendChild(carouselDiv);
+
+  const leftButton = document.createElement('div');
+  leftButton.classList.add('left-button');
+  leftButton.addEventListener('click', () => {
+    let images = document.querySelectorAll('.images');
+    let imgArray = Array.from(images);
+    if(imgArray[1].style.display == 'block'){
+      document.images[1].style.display = 'none';
+      document.images[0].style.display = 'block';
+    }
+  })
+  carouselDiv.appendChild(leftButton);
+
+  const imgs = ['.assets/carousel/mountains.jpeg','./assets/carousel/computer.jpeg','./assets/carousel/trees.jpeg','./assets/carousel/turntable.jpeg'];
+
+  function img(imgarray) {
+    imgarray.forEach((el) => {
+      const img = document.createElement('img');
+      img.src = el;
+      img.classList.add('images');
+      carouselDiv.appendChild(img);
+      if(el === './assets/carousel/mountains.jpeg') {
+        img.style.display = 'block';
+      }
+    });
+  }
+  img(imgs);
+
+  const rightButton = document.createElement('div');
+  rightButton.classList.add('right-button');
+  rightButton.addEventListener('click', () => {
+    let images = document.querySelectorAll('images');
+    let imgArray = Array.from(images);
+    if(imgArray[0].style.display == 'block') {
+      document.images[0].style.display = 'none';
+      document.images[1].style.display = 'block';
+    }
+  });
+
+  carouselDiv.appendChild(rightButton)
+
+    return carouselDiv;
+
+  }
+
+  carousel();
