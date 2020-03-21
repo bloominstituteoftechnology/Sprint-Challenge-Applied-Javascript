85ac26459abb00411d95e8705dec70d6555c597e
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-03-20 19:23:25 -0700

    Articles Built/ attempting to dry it up

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,54 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+const cardSection = document.querySelector('.cards-container')
+
+axios.get("https://lambda-times-backend.herokuapp.com/articles")
+.then(response => {
+    console.log(response.data.articles)
+    response.data.articles.javascript.forEach(article => {
+        cardSection.appendChild(cardGenerator(article))
+    })
+    response.data.articles.bootstrap.forEach(article => {
+        cardSection.appendChild(cardGenerator(article))
+    })
+    response.data.articles.technology.forEach(article => {
+        cardSection.appendChild(cardGenerator(article))
+    })
+    response.data.articles.jquery.forEach(article => {
+        cardSection.appendChild(cardGenerator(article))
+    })
+    response.data.articles.node.forEach(article => {
+        cardSection.appendChild(cardGenerator(article))
+    })
+
+})
+.catch(err => {
+    console.log('Error', err)
+})
+
+function cardGenerator(data) {
+    const card = document.createElement('div')
+    const headline = document.createElement('div')
+    const author = document.createElement('div')
+    const imgCont = document.createElement('div')
+    const pic = document.createElement('img')
+    const feature = document.createElement('span')
+
+    card.appendChild(headline)
+    card.appendChild(author)
+    author.appendChild(imgCont)
+    author.appendChild(feature)
+    imgCont.appendChild(pic)
+
+    card.classList.add('card')
+    headline.classList.add('headline')
+    author.classList.add('author')
+    imgCont.classList.add('img-container')
+    
+    headline.textContent = data.headline
+    pic.src = data.authorPhoto
+    feature.textContent = `By ${data.authorName}`
+
+    return card
+}
