b2baf375bf08133843fbdefcd1f16905f83b6145
Brian Clayton <finalb@projectfinalboss.com>
2020-03-20 22:32:29 -0400

    Stuck on the card section

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,65 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const cardCont = document.querySelector('.cards-container')
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then(resp => {
+
+    console.log('It works, ty Jesus', resp.data.articles)
+
+resp.data.articles.forEach(element => {
+    
+    cardCont.appendChild(someCard(element))
+
+});
+
+
+
+}).catch(err => {
+    
+    console.log("Oh no, please don't do this!", err)
+
+}
+    )
+
+    function someCard(obj){
+
+
+        //Create Element
+
+        let card = document.createElement('div')
+        let headL = document.createElement('div')
+        let author = document.createElement('div')
+        let imgCont = document.createElement('div')
+        let img = document.createElement('img')
+        let authName = document.createElement('span')
+
+        //Append Children
+
+        card.appendChild(headL)
+        card.appendChild(author)
+        author.appendChild(imgCont)
+        imgCont.appendChild(img)
+        headL.appendChild(authName)
+
+        //Add Classes
+
+        card.classList.add('card')
+        headL.classList.add('headline')
+        author.classList.add('author')
+        imgCont.classList.add('img-container')
+        
+        
+        //Add Content
+
+        headL.textContent = obj.headline
+        img.src = obj.authorPhoto
+        authName.textContent = obj.authorName
+
+
+
+        return card
+
+    }
