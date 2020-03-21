4707450c338db8c95002bf46489692aead00c160
workfromhome <workfromhome@Estebans-MacBook-Pro.local>
2020-03-21 08:48:06 -0700

    Header complete

--- components/Header/index.js
+++ components/Header/index.js
@@ -8,5 +8,37 @@
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .header-container component
+    //Outer Container
+    let headerContainer = document.querySelector('.header-container');
 
-function Header() {}
+
+    //Inner Container
+let internalHeader = document.createElement('div');
+    internalHeader.classList.add('header')
+
+
+    //Date Span
+let dateSpan = document.createElement('span');
+    dateSpan.classList.add("date");
+    dateSpan.textContent = 'MARCH 28, 2019'
+
+    //Main Header
+let headerTitle = document.createElement('h1');
+    headerTitle.textContent = 'Lambda Times';
+
+
+    //Temperaure Span
+let tempSpan = document.createElement('span');
+    tempSpan.classList.add('temp')
+
+function Header(){
+    internalHeader.appendChild(tempSpan);
+    internalHeader.appendChild(dateSpan);
+    internalHeader.appendChild(headerTitle);
+
+    headerContainer.appendChild(internalHeader);
+
+    return Header
+}
+
+Header();
