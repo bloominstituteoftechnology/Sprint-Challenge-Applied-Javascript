b503bd797124f45a667ebeb47c9d0fdff6e69c2d
Anthony Moton <ysir09@gmail.com>
2020-03-20 22:22:05 -0500

    finished with what was asked of me for the project. may work on some sprint questions

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,50 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+function articles(article) {
+    const card = document.createElement('div')
+    const headline = document.createElement('div')
+    const author = document.createElement('div')
+    const imgContainer = document.createElement('div')
+    const authorImg = document.createElement('img')
+    const authorName = document.createElement('span')
+
+    card.appendChild(headline)
+    card.appendChild(author)
+    author.appendChild(imgContainer)
+    author.appendChild(authorName)
+    imgContainer.appendChild(authorImg)
+
+    card.classList.add('card')
+    headline.classList.add('headline')
+    author.classList.add('author')
+    imgContainer.classList.add('img-container')
+
+    headline.textContent = article.headline;
+    authorImg.textContent = article.authorImg;
+    authorName.textContent = article.authorName
+
+
+    return card
+}
+
+const cardsContainer = document.querySelector('.cards-container')
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response => {
+        console.log(response.data.articles)
+
+        const data = response.data.articles
+        const keys = Object.keys(data);
+
+        keys.forEach(a => {
+            data[a].forEach(b => {
+                const newCardOne = newArticle(b)
+                cardsContainer.append(newCardOne)
+            })
+        })
+    })
+    .catch(error => {
+        console.log(error)
+    })
--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,29 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+    //create my elements
+    const topHeader = document.createElement('div');
+    const date = document.createElement('span');
+    const h1 = document.createElement('h1');
+    const temp = document.createElement('span');
+    //append my elements
+    topHeader.append(date, h1, temp);
+
+    //add class to my elements
+    topHeader.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+    //give text content to my elements
+    date.textContent = 'MARCH 28, 2019';
+    h1.textContent = 'Lambda Times';
+    temp.textContent = '98';
+
+    return topHeader;
+}
+
+//grab component that we are appending this to and give name
+const headerContainer = document.querySelector('.header-container')
+const newHeader = Header()
+headerContainer.append(newHeader);
+//create forEach function to append to DOM
