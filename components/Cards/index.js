3f0a3363bde73ecc40abe4dbcecf58b502a33eb3
Tyler Nelson <tyler.d.nelson@outlook.com>
2020-03-23 00:01:22 -0400

    finished mvp

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,59 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+let createCard = (cardData) => {
+    const card = document.createElement("div");
+    const headline = document.createElement("div");
+    const author = document.createElement("div");
+    const imgContainer = document.createElement("div");
+    const authorImage = document.createElement("img");
+    const authorName = document.createElement("span");
+
+    card.classList.add("card");
+    headline.classList.add("headline");
+    author.classList.add("author");
+    imgContainer.classList.add("img-container");
+    authorImage.classList.add("img");
+    authorName.classList.add("img");
+
+    headline.textContent = cardData.headline;
+    authorImage.src = cardData.authorPhoto;
+    authorName.textContent = cardData.authorName;
+
+
+    card.appendChild(headline);
+    card.appendChild(author);
+
+    author.appendChild(imgContainer);
+
+    imgContainer.appendChild(authorImage);
+
+    author.appendChild(authorName);
+
+    return card;
+
+}
+
+let info = axios.get('https://lambda-times-backend.herokuapp.com/articles');
+
+// console.log(info)
+
+let cardNet = document.querySelector('.cards-container');
+
+
+info.then((response) => {
+    let articles = Object.values(response.data.articles);
+
+    articles.forEach((item) => {
+        let anItem = item;
+
+        anItem.forEach((lowItem) => {
+            let exitCard = createCard(lowItem);
+            cardNet.appendChild(exitCard);
+        })
+    })
+})
+
+
+
