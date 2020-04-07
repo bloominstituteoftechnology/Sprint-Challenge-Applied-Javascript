6a3d1ee0ad3216c736d8dc4120fc0781417c389e
e-licea <48616990+e-licea@users.noreply.github.com>
2020-04-06 18:39:38 -0700

    Update index.js

--- components/Header/index.js
+++ components/Header/index.js
@@ -8,5 +8,55 @@
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .header-container component
+let myObj2 = axios.get("https://lambda-times-backend.herokuapp.com/articles")
+  .then(response => {
+    console.log(response);
+    
+    response.data.articles.javascript.forEach(item => {
+        console.log(item);
+        cards.appendChild(articleCreator(item));
+    })
+    response.data.articles.bootstrap.forEach(item => {
+        cards.appendChild(articleCreator(item));
+    })
+    response.data.articles.technology.forEach(item => {
+        cards.appendChild(articleCreator(item));
+    })
+    response.data.articles.jquery.forEach(item => {
+        cards.appendChild(articleCreator(item));
+    })
+    response.data.articles.node.forEach(item => {
+        cards.appendChild(articleCreator(item));
+    })
+  }) 
+  .catch(err => {
+    console.log(err);
+  })
 
-function Header() {}
+function articleCreator(dataObj) {
+  let card = document.createElement("div");
+  let headline = document.createElement("div");
+  let author = document.createElement("div");
+  let imgDiv = document.createElement("div");
+  let img = document.createElement("img");
+  let span = document.createElement("span");
+
+  card.classList.add("card");
+  headline.classList.add("headline");
+  author.classList.add("author");
+  imgDiv.classList.add("img-container");
+
+  headline.textContent = dataObj.headline;
+  img.src = dataObj.authorPhoto;
+  span.textContent = `By ${dataObj.authorName}`;
+
+  card.appendChild(headline);
+  card.appendChild(author);
+  author.appendChild(imgDiv);
+  author.appendChild(span);
+  imgDiv.appendChild(img);
+
+  return card;
+}
+
+let cards = document.querySelector(".cards-container");
