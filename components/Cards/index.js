9a5717de929996d09697eba61860eb8b88569d6f
allan-pimble <allan.pimble@gmail.com>
2020-04-10 14:02:52 -0400

    created cards ready to post to DOM

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,84 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+{/* <div class="cards-container"> */}
+const articleCards = document.querySelector('cards-container')
+
+
+axios
+  .get('https://lambda-times-backend.herokuapp.com/articles')
+  .then((resolve) => {
+    getArticles(resolve.data.articles)
+  })
+  .catch((err) =>
+    console.error(err))
+
+// Retrieve and post the articles
+const getArticles = (articles) => {
+  // Get a list of the article names
+  articleNames = Object.keys(articles)
+
+  // Go through each article
+  articleNames.forEach(name => {
+    articles[name].forEach(article => {
+      console.log(article.headline)
+      console.log(article.authorPhoto)
+      console.log(article.authorName)
+      // fields headline, authorPhoto, authorName
+      let card = createCard(article)
+      articleCards.appendChild(card)
+
+    })
+  })
+}
+
+
+const createCard = (article) => {
+  const card = document.createElement('div')
+  const headline = document.createElement('div')
+  const author = document.createElement('div')
+  const imaageContainer = document.createElement('img')
+  const authorImage = document.createElement('img')
+  const authorName = document.createElement('span')
+
+  card.classList.add('card')
+  headline.classList.add('headline')
+  headline.textContent = article.headline
+  author.classList.add('author')
+  imaageContainer.classList.add('img-container')
+  authorImage.setAttribute('src', article.authorPhoto)
+  authorName.textContent = `By ${article.authorName}`
+
+  card.appendChild(headline)
+  card.appendChild(author)
+  card.appendChild(headline)
+
+  author.appendChild(imaageContainer)
+  author.appendChild(authorName)
+
+  imaageContainer.append(authorImage)
+  return card
+}
+
+// <div class="card">
+//   <div class="headline">{Headline of article}</div>
+//   <div class="author">
+//     <div class="img-container">
+//       <img src={url of authors image} />
+//     </div>
+//     <span>By {author's name}</span>
+//   </div>
+// </div>
+
+
+
+
+
+
+
+
+
+
+
+
