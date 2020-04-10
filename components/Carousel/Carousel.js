6313834012932957fe2b8f41dbf1ad7ad5e1e359
ReyWilly <57641631+ReyWilly@users.noreply.github.com>
2020-04-10 12:27:12 -0400

    Finished Cards attempted Carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,30 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+const carousel = document.createElement("div");
+carousel.classList.add("carousel");
+
+const left_button = document.createElement("div");
+left_button.classList.add("left-button");
+left_button.appendChild(carousel);
+
+const img1 = document.createElement("img");
+img1.src = "./assets/carousel/mountains.jpeg";
+img1.appendChild(carousel);
+
+const img2 = document.createElement("img");
+img2.src = "./assets/carousel/computer.jpeg";
+img2.appendChild(carousel);
+
+const img3 = document.createElement("img");
+img3.src = "./assets/carousel/trees.jpeg";
+img3.appendChild(carousel);
+
+const img4 = document.createElement("img");
+img4.src = "./assets/carousel/turntable.jpeg";
+img4.appendChild(carousel);
+
+const right_button = document.createElement("div");
+right_button.classList.add("left-button");
+right_button.appendChild(carousel);
