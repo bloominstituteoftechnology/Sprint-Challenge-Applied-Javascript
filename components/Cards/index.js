d3ec1a7e0260742c29d7169231e69ad1e05dfb76
matthew-heideman <heidemanmr@yahoo.com>
2020-04-10 10:43:21 -0500

    wip

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -19,11 +19,26 @@
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
 
-function createDogArticle() {
+axios
+    .get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response => {
+        console.log(response)
+    })
+    .catch(err => {
+        console.log(err, 'failed to retrieve data')
+    })
+
+
+function createDogArticle(data) {
     const card = document.createElement('div');
     const headline = document.createElement('div');
     const author = document.createElement('div');
     const imgContainer = document.createElement('div');
     const img = document.createElement('img');
     const authorName = document.createElement('span');
+    card.classList.add('.card');
+    headline.classList.add('.headline');
+    author.classList.add('.author');
+
+    return card;
 }
