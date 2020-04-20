e42700a8edcb5c13bd4501124b029ad135d2d350
Mustafa Hassan <60632959+MustafaCajib@users.noreply.github.com>
2020-04-19 18:48:55 -0700

    Mustafa-Hassan

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,33 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+// const entry = document.querySelector(".topics")
+// const Top =[];
+// Top.forEach( topics => {
+// axios.get(`https://lambda-times-backend.herokuapp.com/${topics}`)
+// .then( response =>{
+//     const newTabs = tabData(response.data)
+//     entry.appendChild(newTabs)
+//     console.log(response)
+// })
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then( response => {
+        response.data.topics.forEach((item) => {
+            const topicA = document.getElementsByClassName('topics');
+            let newTab = tabData(item);
+            topicA[0].appendChild(newTab);
+
+
+        });
+    })
+  .catch( error => {
+    console.log(error);
+  })
+
+function tabData(object){
+    const tab = document.createElement('div')
+    tab.classList.add('tab')
+    tab.textContent = object
+    return tab
+}
