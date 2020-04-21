abf6cd1a290d5085437a103c88078c96a69cd4ba
Lane <lanelovesugly@icloud.com>
2020-04-20 18:12:04 -0600

    trying to figure out article cards. can't get axios to show up on my terminal

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,39 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+function articleCard(headline, imgUrl, author){
+    const article = document.createElement('div');
+    const articleHeadLine = document.createElement('div');
+    const articleAuthor = document.createElement('div');
+    const articleImgContainer = document.createElement('div');
+    const articleImg = document.createElement('img');
+    const articleSpan = document.createElement('span');
+
+    axios.get("https://lambda-times-backend.herokuapp.com/articles")
+    .then( (response) => {
+    response.data.message.forEach( (imgUrl) => {
+    const newArticleCard = articleCard(headline, imgUrl, author);
+    entryPoint.appendChild(newArticleCard);
+        })
+    })
+    .catch( (err) => {
+    console.log("the data was not returned", err)
+    })
+
+    article.classList.add('card');
+    articleHeadLine.classList.add('headline');
+    articleAuthor.classList.add('author');
+    articleImgContainer.classList.add('img-container');
+    articleImg.src = imgUrl;
+
+    article.appendChild(articleHeadLine);
+    article.appendChild(articleAuthor);
+    articleAuthor.appendChild(articleImgContainer);
+    articleAuthor.appendChild(articleSpan);
+    articleImgContainer.appendChild(articleImg);
+
+    console.log('axios')
+}
+
+// const entryPoint = document.querySelector('.cards-container');
