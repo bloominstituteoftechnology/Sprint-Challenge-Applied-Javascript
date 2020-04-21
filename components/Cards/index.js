29abbfb45febae756ddcf3bce600d7170b298208
BryanPAnderson <AndersonBryan53@gmail.com>
2020-04-20 20:56:25 -0400

    done

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -1,85 +1,99 @@
-// STEP 3: Create article cards.
+// STEP 3: Create Article cards.
 // -----------------------
 // Send an HTTP GET request to the following address: https://lambda-times-backend.herokuapp.com/articles
-// Study the response data you get back, closely.
-// You will be creating a card for each 'article' in the response.
+// Stduy the response data you get back, closely.
+// You will be creating a component for each 'article' in the list.
 // This won't be as easy as just iterating over an array though.
+// Create a function that will programmatically create the following DOM component:
 //
-// Write a function that returns the following markup:
-//
-//
-// Use your function to create a card for each of the articles and add the card to the DOM.
-axios.get(`https://lambda-times-backend.herokuapp.com/articles`)
-    .then( function (response) 
-    {
-        console.log(response);
-        // Javascript
-         for(let i = 0; i < response.data.articles.javascript.length; i++)
-         {
-            cardCreator(response.data.articles.javascript[i]);
-         }
-        //bootstrap
-         for(let i = 0; i < response.data.articles.bootstrap.length; i++)
-         {
-            cardCreator(response.data.articles.bootstrap[i]);
-         }
-        //technology
-         for(let i = 0; i < response.data.articles.technology.length; i++)
-         {
-            cardCreator(response.data.articles.technology[i]);
-         }
-         //jquery
-         for(let i = 0; i < response.data.articles.jquery.length; i++)
-         {
-            cardCreator(response.data.articles.jquery[i]);
-         }
-         //node 
-         for(let i = 0; i < response.data.articles.node.length; i++)
-         {
-            cardCreator(response.data.articles.node[i]);
-         }
-
-    })
-
-    .catch( function (error) {
-        console.log(error);
-    })
-
-    // <div class="card">
+// <div class="card">
 //   <div class="headline">{Headline of article}</div>
 //   <div class="author">
 //     <div class="img-container">
 //       <img src={url of authors image} />
 //     </div>
-//     <span>By {author's name}</span>
+//     <span>By {authors name}</span>
 //   </div>
 // </div>
+//
+// Create a card for each of the articles and add the card to the DOM.
 
-function cardCreator(element) {
-    const card = document.createElement('div');
-    card.classList.add('card');
-    const headLine = document.createElement('div');
-    headLine.classList.add('headline');
-    headLine.textContent = element.headline;
-    const author = document.createElement('div');
-    author.classList.add('author');
-    const imageContainer = document.createElement('div');
-    imageContainer.classList.add('img-container');
-    const img = document.createElement('img');
-    image.src = element.authorPhoto;
-    const authorName = document.createElement('span');
-    authorName.textContent = `By ${element.authorName}`;
 
-    card.appendChild(headline);
-    card.appendChild(author);
-    author.appendChild(imageContainer);
-    imageContainer.appendChild(image);
-    author.appendChild(authorName);
+const cardContainer = document.querySelector('.cards-container');
 
-    const cardsContainer = document.querySelector('.cards-container');
-    cardsContainer.appendChild(card);
+let javascriptArticles = [];
+let bootstrapArticles = [];
+let technologyArticles = [];
+let jqueryArticles = [];
+let nodejsArticles = [];
 
-    return card;
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+  .then(response => {
 
+      response.data.articles.bootstrap.forEach((article) => {
+        articleCreator(bootstrapArticles, article, 'bootstrap');
+      });
+      bootstrapArticles.forEach((article)=>{
+          cardContainer.appendChild(article);
+      })
+      response.data.articles.javascript.forEach((article) => {
+        articleCreator(javascriptArticles, article, "javascript");
+      });
+      javascriptArticles.forEach((article)=>{
+          cardContainer.appendChild(article);
+      })
+      response.data.articles.technology.forEach((article) => {
+        articleCreator(technologyArticles, article, 'technology');
+      });
+      technologyArticles.forEach((article)=>{
+          cardContainer.appendChild(article);
+      })
+      response.data.articles.jquery.forEach((article) => {
+        articleCreator(jqueryArticles, article, 'jquery');
+      });
+      jqueryArticles.forEach((article)=>{
+          cardContainer.appendChild(article);
+      })
+      response.data.articles.node.forEach((article) => {
+        articleCreator(nodejsArticles, article, 'node.js');
+      });
+      nodejsArticles.forEach((article)=>{
+          cardContainer.appendChild(article);
+      })
+      console.log(nodejsArticles);
+
+})
+  .catch(error => {console.log('Error! : ' + error)});
+
+function articleCreator(array, obj, topic){
+    let cardDiv = document.createElement('div');
+    cardDiv.classList.add('card');
+    let headlineDiv = document.createElement('div');
+    headlineDiv.classList.add('headline');
+    headlineDiv.textContent = obj.headline;
+    let authorDiv = document.createElement('div');
+    authorDiv.classList.add('author');
+    let imageDiv = document.createElement('div');
+    imageDiv.classList.add('img-container');
+    let img = document.createElement('img');
+    img.src = obj.authorPhoto;
+    let span = document.createElement('span');
+    span.textContent = obj.authorName;
+
+    cardDiv.setAttribute('data-topic', topic);
+
+    imageDiv.appendChild(img);
+    authorDiv.appendChild(imageDiv);
+    authorDiv.appendChild(span);
+    cardDiv.appendChild(headlineDiv);
+    cardDiv.appendChild(authorDiv);
+    console.log(cardDiv);
+    array.push(cardDiv);
 }
-cardCreator();
+
+//   function tabCreator(data){
+//       let tabDiv = document.createElement('div');
+//       tabDiv.classList.add('tab');
+//       tabDiv.textContent = data;
+//       topics.appendChild(tabDiv);
+//   }
