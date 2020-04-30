c33e92d28f4bace7ff4b260e4ca2dcc034aa69b2
Mr. Jacky Charpentier <jcharp@fdu.edu>
2020-04-30 19:48:03 -0400

    created elements and class

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,20 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+
+    const header = document.createElement('div');
+    const date = document.createElement('span');
+    const tagH1 = document.createElement('h1');
+    const temp1 = dociment.createElement('span');
+
+
+    header.classList.add('header');
+    date.classList.add('date');
+    temp1.classList.add('temp');
+
+
+    
+
+
+}
