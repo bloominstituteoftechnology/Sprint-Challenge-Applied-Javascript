7e60a54c44d4b8fc2457a0cb8d7cd03a1c6d7056
Mr. Jacky Charpentier <jcharp@fdu.edu>
2020-04-30 21:28:19 -0400

    started stretch

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,14 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+function createCarousel() {
+
+  const carousel = document.createElement('div');
+  const leftButton = document.createElement('div');
+  const mountains = document.createElement('img');
+  const computer = document.createElement('img');
+  const trees = document.createElement('img');
+  const trntbl = document.createElement('img');
+  const rightButton = document.createElement('img');
+}
