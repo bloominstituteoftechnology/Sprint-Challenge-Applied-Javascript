b9264b2de552e694ed98988e75259ee5e2de2cad
Daniel Johnson <danielaugustusjohnson@gmail.com>
2020-05-08 11:53:26 -0400

    Finished Cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,81 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+axios.get("https://lambda-times-backend.herokuapp.com/articles").then(response => {
+
+    console.log(response.data["articles"]);
+
+    let newArr = [];
+
+    Object.keys(response.data["articles"]).forEach(item => {
+
+        //console.log(response.data["articles"][item]);
+
+        newArr.push(response.data["articles"][item]);
+
+    });
+
+    //console.log(newArr);
+
+    newArr.forEach(item => {
+
+        console.log(item);
+
+        item.forEach(item => {
+
+            //console.log(item);
+
+            //Creating elements
+
+            let card = document.createElement("div");
+
+            let headline = document.createElement("div");
+
+            let author = document.createElement("div");
+
+            let imgContainer = document.createElement("div");
+
+            let img = document.createElement("img");
+
+            let authorName = document.createElement("span");
+
+            //Adding classes
+
+            card.classList.add("card");
+
+            headline.classList.add("headline");
+
+            author.classList.add("author");
+
+            imgContainer.classList.add("img-container");
+
+            //Appending
+
+            card.appendChild(headline);
+
+            card.appendChild(author);
+
+            author.appendChild(imgContainer);
+
+            imgContainer.appendChild(img);
+
+            author.appendChild(authorName);
+
+            //Assigning values
+
+            headline.textContent = item.headline;
+
+            img.src = item.authorPhoto;
+
+            authorName.textContent = item.authorName;
+
+            //Appending card to existing element on screen
+
+            document.querySelector(".cards-container").appendChild(card);
+
+        });
+
+    });
+
+}).catch()
