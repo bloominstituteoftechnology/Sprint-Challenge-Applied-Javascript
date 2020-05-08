2b07e95bcf48c4780e564c38eebbc00efa61e2c5
arn-foto <38259824+arn-foto@users.noreply.github.com>
2020-05-08 09:17:31 -0700

    index.js is finished.

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,34 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+
+//elements created for card
+
+/* remember when it says "append" to the DOM it really means to PUT it in the header. */
+function Header() {
+    //create the elements
+    const headerContainer = document.createElement('div');
+    const date = document.createElement('span');
+    const headerOne = document.createElement('h1');
+    const temp = document.createElement('span');
+
+    //add classes to the elements
+    headerContainer.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+
+    //add content
+    date.textContent = 'SMARCH 28, 2019';
+    headerOne.textContent = 'Lambda Times';
+    temp.textContent = '98';
+
+    //append the children
+    headerContainer.appendChild(date);
+    headerContainer.appendChild(headerOne);
+    headerContainer.appendChild(temp);
+
+    return headerContainer;
+}
+
+const parentHeader = document.querySelector('.header-container');
+parentHeader.appendChild(Header());
