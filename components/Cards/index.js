1c3f45d8fdfeb59dd4ce048bf864fb25dfb4f881
JM <manzur-1990@hotmail.com>
2020-05-08 11:56:41 -0500

    Card component done

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,50 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then(res =>{
+    console.log(res)
+    const cardsComponent = document.querySelector('.cards-container')
+    const cardData = Object.values(res.data.articles)
+    // console.log(cardData)
+    cardData.forEach((articleSet)=>{
+        articleSet.forEach((article) =>{
+            cardsComponent.append(Cards(article))
+        })
+    })
+})
+.catch(err =>{
+    console.log(err)
+})
+
+
+function Cards(cardAtts) {
+    
+    const card = document.createElement('div')
+    const headline = document.createElement('div')
+    const author = document.createElement('div')
+    const imgCont = document.createElement('div')
+    const authImg = document.createElement('img')
+    const authName = document.createElement('span')
+
+    card.classList.add('card')
+    headline.classList.add('headline')
+    author.classList.add('author')
+    imgCont.classList.add('img-container')
+
+    card.appendChild(headline)
+    card.appendChild(author)
+    author.appendChild(imgCont)
+    imgCont.appendChild(authImg)
+    author.appendChild(authName)
+
+    headline.textContent = cardAtts.headline
+    authImg.src = cardAtts.authorPhoto
+    authName.textContent = `By: ${cardAtts.authorName}`
+
+
+    return card
+}
+
