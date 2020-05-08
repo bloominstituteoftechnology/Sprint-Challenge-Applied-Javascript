c5aa96537a7382cd1e1f2032850a62a42c379b8f
Daniel Johnson <danielaugustusjohnson@gmail.com>
2020-05-08 11:14:54 -0400

    Finished Header

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,44 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+
+    //Creating elements
+
+    let header = document.createElement("div");
+
+    let date = document.createElement("span");
+
+    let title = document.createElement("h1");
+
+    let temp = document.createElement("span");
+
+    //Adding classes
+
+    header.classList.add("header");
+
+    date.classList.add("date");
+
+    temp.classList.add("temp");
+
+    //Appending
+
+    header.appendChild(date);
+
+    header.appendChild(title);
+
+    header.appendChild(temp);
+
+    //Adding values
+
+    date.textContent = "SMARCH 28, 2019"; //Copied "SMARCH" straight from above
+
+    title.textContent = "Lambda Times";
+
+    temp.textContent = "98°";
+
+    return header;
+
+}
+
+document.querySelector("div.header-container").appendChild(Header());
