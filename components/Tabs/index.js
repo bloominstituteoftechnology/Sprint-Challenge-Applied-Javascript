3cffddb6547fd112811bf0cbd73fa2712dc604d2
Daniel Johnson <danielaugustusjohnson@gmail.com>
2020-05-08 11:22:04 -0400

    Finished Tabs

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,29 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+
+axios.get("https://lambda-times-backend.herokuapp.com/topics").then(response => {
+
+    console.log(response.data["topics"]);
+
+    response.data["topics"].forEach(item => {
+
+        //Creating an element
+
+        let tab = document.createElement("div");
+
+        //Adding a class to the element
+
+        tab.classList.add("tab");
+
+        //Assigning value to tab from data
+
+        tab.textContent = item;
+
+        //Appending to existing element on the screen
+
+        document.querySelector("div.topics").appendChild(tab);
+
+    });
+
+}).catch()
