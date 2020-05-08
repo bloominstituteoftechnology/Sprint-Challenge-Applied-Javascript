014a66f34c14ca1f931c3177e08bfa774d0ae969
arn-foto <38259824+arn-foto@users.noreply.github.com>
2020-05-08 09:42:15 -0700

    tabs.js is finished.

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,28 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+
+
+axios
+    .get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(response => {
+        console.log(response);
+        response.data.topics.forEach(item => {
+            const newTab = createTab(item);
+            parentElement.appendChild(newTab);
+        })
+    })
+    .catch(error => {
+        console.log('!!error!!');
+    })
+
+function createTab(data) {
+    const tab = document.createElement('div');
+    tab.classList.add('tab');
+    tab.textContent = data;
+
+    return tab;
+
+}
+
+const parentElement = document.querySelector('.tabs .topics');
