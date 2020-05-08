e9822775385045e347f80411dfb612d0c8936154
Nathan Cowley <nathancowley12@gmail.com>
2020-05-08 09:37:26 -0600

    done with the header

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,26 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(res => {
+        
+        res.data.topics.forEach(info => {
+          console.log(res)  
+            topicsDiv.append(tabMaker(info))
+        })
+    })
+    .catch(error => {
+        console.log('could not get request', error)
+    })
+
+
+const topicsDiv = document.querySelector('.topics')
+
+function tabMaker(array){
+    const tab = document.createElement('tab')
+          tab.classList.add('tab')
+          tab.textContent = array;
+    return tab
+}
+
