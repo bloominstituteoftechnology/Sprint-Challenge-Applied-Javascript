c8fbe338a64b370d3e1e58f15487e3b36427d345
Francisco Barrios <shighetari@gmail.com>
2020-05-08 13:56:10 -0400

    Done without stretch, will work on stretch later

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,32 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+
+            //start of consts\\
+    const header = document.createElement('div')
+    const headerDate = document.createElement('span')
+    const headerTitle = document.createElement('h1')
+    const headerTemp = document.createElement('span')
+
+            //start of appendChild\\
+    header.appendChild(headerDate)
+    header.appendChild(headerTitle)
+    header.appendChild(headerTemp)
+
+            //start of classList\\
+    header.classList.add('header')
+    headerDate.classList.add('date')
+    headerTemp.classList.add('temp')
+
+            //start of textContent\\
+    headerDate.textContent = 'MARCH 28, 2019'
+    headerTitle.textContent = 'Lambda Times'
+    headerTemp.textContent = '98°'
+    
+            //querySelector for appendChild\\
+    document.querySelector('.header-container').appendChild(header)
+}
+
+                //invoking the Header function\\
+                           Header()
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,26 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+
+                //start of promise to create tabs\\
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+.then( resolve => {
+//   debugger -> used to grab information from API object path
+//-> https://i.imgur.com/orusXHO.png
+        resolve.data.topics.forEach( element => {
+
+            //creating tab\\
+        const tab = document.createElement('div')
+        tab.classList.add('tab')
+        tab.textContent = element //topics here
+        document.querySelector('.topics').appendChild(tab)
+        //end of tab creation forEach\\
+    })
+})
+.catch( error => {
+    //adding insult to injury 
+    console.log('you failure')
+})
+.finally( () => {
+    console.log('Done')
+})
