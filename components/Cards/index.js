daf7cf9322a1285c45d26e722aecafaa58e6db2e
Impulse2020 <irishht@gmail.com>
2020-05-08 13:54:36 -0400

    Almost finishes task 3

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,9 +18,41 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+function cardMaker(object){
+    let card = document.createElement('div');
+    let headline = document.createElement('div');
+    let author = document.createElement('div');
+    let frame = document.createElement('div');
+    let picture = document.createElement('img');
+    let name = document.createElement('span');
+    //creates html elements of component
+    card.appendChild(headline);
+    card.appendChild(author);
+    author.appendChild(frame);
+    frame.appendChild(picture);
+    author.appendChild(name);
+    //structures component
+    card.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author');
+    frame.classList.add('img-container');
+    //adds classes to our component
+    headline.textContent = object.headline;
+    picture.src = object.authorPhoto;
+    name.textContent = object.authorName;
+    // sets content
 
+    return card;
+    // returns component
+}
 
 axios.get('https://lambda-times-backend.herokuapp.com/articles')
     .then(function(response){
+        let articles = response.data.articles.javascript;
         console.log(response);
+        articles.forEach(element => {
+           let card = cardMaker(element);
+            document.body.appendChild(card);
+        });
+    
     })
