551a100910fe09b2f48a5b3266927f0e4934df99
Marc de la Torre <mdelatorre1314@gmail.com>
2020-05-15 18:17:28 -0700

    Header built and structured

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,28 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    const container = document.createElement('div');
+    const date = document.createElement('span');
+    const title = document.createElement('h1');
+    const temp = document.createElement('span');
+
+    container.appendChild(date);
+    container.appendChild(title);
+    container.appendChild(temp);
+
+    container.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+
+    date.textContent = 'SMARCH 28, 2019'
+    title.textContent = 'Lambda Times';
+    temp.textContent = '98°';
+
+    return container;
+    
+}
+
+const domSelector = document.querySelector('.header-container');
+domSelector.appendChild(Header());
+
