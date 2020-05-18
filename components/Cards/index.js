9d02bcf0d20e06fe4d89315518b0718b5195ca97
slroberts <shomariroberts@gmail.comclear>
2020-05-17 23:51:10 -0400

    Completed MVP

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,43 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+const cardMaker = (obj, topic) => {
+  const card = document.createElement("div");
+  const headline = document.createElement("div");
+  const author = document.createElement("div");
+  const imgContainer = document.createElement("div");
+  const img = document.createElement("img");
+  const authorsName = document.createElement("span");
+
+  card.classList.add("card");
+  headline.classList.add("headline");
+  author.classList.add("author");
+  imgContainer.classList.add("img-container");
+
+  card.append(headline, author);
+  author.append(imgContainer, authorsName);
+  imgContainer.appendChild(img);
+
+  headline.textContent = obj.headline;
+  img.src = obj.authorPhoto;
+  authorsName.textContent = obj.authorName;
+
+  return card;
+};
+
+cardContainer = document.querySelector("div.cards-container");
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then((response) => {
+    console.log(response.data.articles);
+
+    const articles = Object.keys(response.data.articles);
+
+    articles.map((article) => {
+      response.data.articles[article].map((indArticle) => {
+        cardContainer.append(cardMaker(indArticle, article));
+      });
+    });
+  });
