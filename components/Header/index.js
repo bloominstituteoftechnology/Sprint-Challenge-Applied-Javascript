a418839a637cb35f38c9ae2205f16a34c0076c38
Isaac M Gorman <59988845+imgorman@users.noreply.github.com>
2020-06-05 10:31:48 -0500

    Header Done!

--- components/Header/index.js
+++ components/Header/index.js
@@ -3,7 +3,7 @@
 // Write a function that returns the markup you see below:
 //
 //  <div class="header">
-//    <span class="date">SMARCH 28, 2019</span>
+//    <span class="date">MARCH 28, 2019</span>
 //    <h1>Lambda Times</h1>
 //    <span class="temp">98°</span>
 //  </div>
@@ -11,4 +11,27 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+  const headerDiv = document.createElement("div");
+  const spanInDivDate = document.createElement("span");
+  const headingOne = document.createElement("h1");
+  const spanInDivTemp = document.createElement("span");
+
+  headerDiv.setAttribute("class", "header");
+  spanInDivDate.setAttribute("class", "date");
+  spanInDivTemp.setAttribute("class", "temp");
+
+  spanInDivDate.textContent = "Friday, June 5, 2020";
+  headingOne.textContent = "Lambda Times";
+  spanInDivTemp.textContent = "82°F";
+
+  headerDiv.appendChild(spanInDivDate);
+  headerDiv.appendChild(headingOne);
+  headerDiv.appendChild(spanInDivTemp);
+
+  const headerContainer = document.querySelector(".header-container")
+  
+  return headerContainer.append(headerDiv);
+}
+Header()
+
