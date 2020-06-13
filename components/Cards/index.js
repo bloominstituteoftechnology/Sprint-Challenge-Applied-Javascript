844647729e528bc216dfa476183b356203a90398
RJ <Argelisjiminian1@gmail.com>
2020-06-12 20:49:13 -0400

    finshed up the cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,48 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+
+
+const node = document.querySelector('.cards-container');
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles').then(response =>{
+    let articles = response.data.articles;
+    console.log(articles);
+    for(article in articles) {
+        articles[article].map(element =>{
+
+            let card = document.createElement('div');
+            card.classList.add('card');
+
+            let headline = document.createElement('div');
+            headline.classList.add('headline');
+            headline.textContent = element.headline;
+
+            let author = document.createElement('div');
+            author.classList.add('author');
+
+            let imgContainer = document.createElement('div');
+            imgContainer.classList.add('img-container');
+
+            let img = document.createElement('img');
+            img.src = element.authorPhoto;
+
+
+            let span = document.createElement('span');
+            span.textContent = element.authorName;
+
+
+
+            imgContainer.appendChild(img);
+            author.appendChild(imgContainer);
+            author.appendChild(span);
+            card.appendChild(headline);
+            card.appendChild(author);
+
+            node.appendChild(card);
+        });
+    }
+}).catch(err => {
+    console.log('An error has occurred!');
+})
