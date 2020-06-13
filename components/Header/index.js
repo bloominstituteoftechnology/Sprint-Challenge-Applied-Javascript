6e120cb2d41dd623b7201a64898877d66070a054
Terry Edwards <terryedwardsjr113@gmail.com>
2020-06-13 12:52:33 -0400

    header

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,28 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    // create
+    const header = document.createElement('div');
+    const date = document.createElement('span');
+    const title = document.createElement('h1');
+    const temp = document.createElement('span');
+
+    // append 
+    header.appendChild(date);
+    header.appendChild(title);
+    header.appendChild(temp);
+
+    // styling 
+    header.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+
+    // values
+    date.textContent='March 28, 2019'
+    temp.textContent='98°'
+
+    return header
+}
+const topBar=document.querySelector('.top-bar');
+topBar.appendChild(header);
