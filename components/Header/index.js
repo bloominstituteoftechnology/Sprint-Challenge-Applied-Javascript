8501cd0fe182b577307c9e3b8948f6e173b8c230
chqiu6 <chucky-qiu@lambdastudents.com>
2020-06-12 20:03:43 -0400

    i think cards require axios so we mightve messed up

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,25 +11,31 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
+
+//create elements
 function Header(date, h1, temp) {
 const headerdiv = document.createElement("div");
 const spand = document.createElement("span");
 const h1tag = document.createElement("h1");
 const spant = document.createElement("span");
 
+//append
 headerdiv.appendChild(spand);
 headerdiv.appendChild(h1tag);
 headerdiv.appendChild(spant);
 
+//make class
 headerdiv.classList.add("headerdiv");
 headerdiv.classList.add("spand");
 headerdiv.classList.add("spant");
 
+//add content
 spand.textContent = date;
 h1tag.textContent = h1;
 spant.textContent = temp;
 
 return headerdiv;
 }
+//append to headerContainer
 const headerContainer = document.querySelector(".header-container");
 headerContainer.appendChild(Header("MARCH 28, 2019", "Lambda Times", "98°"));
