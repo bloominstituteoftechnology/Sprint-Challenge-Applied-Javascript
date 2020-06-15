3ea87dd3d2b0e11995c8e58da0a224d03d70638e
Mr. Jacky Charpentier <jcharp@fdu.edu>
2020-06-14 20:11:21 -0400

    topics section done

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -9,10 +9,28 @@
 //    <div class="tab">topic here</div>
 
 
-axios.get('https://lambda-times-backend.herokuapp.com/topics')
- .then((resp) => {
-     console.log(resp);
+
+
+ function topics(someTopic) {
+     const tab = document.createElement('div');
+    tab.classList.add('tab');
+    tab.textContent = someTopic;
+    const topics = document.querySelector('topics');
+
+    return tab;
+ }
+
+ const entry = document.querySelector('.topics');
+
+
+ axios.get('https://lambda-times-backend.herokuapp.com/topics')
+ .then(response => {
+     response.data.topics.forEach(element => {
+         const newTopic = topics(element)
+         entry.appendChild(newTopic)
+         
+     });
  })
- .catch((err) => {
+ .catch(err => {
      console.log("Error on the card")
  })
