75d00418a6a90df0e32f00bc837f9458a5a05d5d
User <user@MacBook-Pro.local>
2020-06-14 17:52:32 -0700

    tasks 1-3 completed for mvp

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,25 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header(dates, title, temp) {
+    let header = document.createElement('div');
+    header.classList.add('header');
+
+    let date = document.createElement('span');
+    date.classList.add('date');
+    date.textContent = dates;
+    header.appendChild(date);
+
+    let h1 = document.createElement('h1');
+    h1.textContent = title;
+    header.appendChild(h1);
+
+    let temperature = document.createElement('span');
+    temperature.classList.add('temp');
+    temperature.textContent = temp;
+    header.appendChild(temperature);
+
+    return header;
+}
+
+document.querySelector('.header-container').append(Header('SMARCH 28, 2019', 'Lambda Times', '98°'));
