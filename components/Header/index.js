e9d5b5e000d440127fbd60644a88da64e10ec25e
Alex Vargas <yoralexvargas@gmai.com>
2020-06-14 20:22:49 -0400

    header is done

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,29 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+const headerContainer = document.querySelector('.header-container');
+
+function Header(todaysDate, headingContent, tempature)  {
+    //Top div
+    const header = document.createElement('div');
+    header.classList.add('header');
+    headerContainer.appendChild(header);
+    //Date span
+    const date = document.createElement('span');
+    date.textContent = todaysDate;
+    date.classList.add('date');
+    header.appendChild(date);
+    //h1 header
+    const heading = document.createElement('h1');
+    heading.textContent = headingContent;
+    header.appendChild(heading);
+    //Temp span
+    const temp = document.createElement('span');
+    temp.classList.add('temp');
+    temp.textContent = tempature;
+    header.appendChild(temp);
+
+    console.log(header);
+    return header;
+}
+ Header('June 14, 2020', 'Lamba Times', '78 degress')
