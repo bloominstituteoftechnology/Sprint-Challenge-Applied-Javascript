e50eb9b9951b1954dd629b7877508ef88292b866
Ahmad Wali Sayadi <sayadi@Ahmads-MacBook-Pro.local>
2020-06-14 17:14:30 -0700

    created the header component

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,35 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+const headerContainer = document.querySelector('.header-container');
+
+const headerContent = Header();
+
+headerContainer.appendChild(headerContent);
+
+function Header() {
+    // creating the elements
+    const header = document.createElement('div');
+    const headerDate = document.createElement('span');
+    const headerTitle = document.createElement('h1');
+    const headerTemp = document.createElement('span');
+
+    // appending to the header div
+    header.appendChild(headerDate);
+    header.appendChild(headerTitle);
+    header.appendChild(headerTemp);
+
+    // adding the classes
+    header.classList.add('header');
+    headerDate.classList.add('date');
+    headerTemp.classList.add('temp');
+
+    // adding attributes to the elements
+    headerDate.textContent = 'MARCH 28, 2019';
+    headerTitle.textContent = 'Lambda Times';
+    headerTemp.textContent = '98°';
+
+    // console.log(header);
+
+    return header;
+}
