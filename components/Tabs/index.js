4529d31f06fb3dc76c2911c549702d9cbd38ff05
unknown <clay2000lbs@gmail.com>
2020-08-16 18:50:44 -0500

    added axios request

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,44 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+
+let resArray = [];
+
+axios
+    .get('https://lambda-times-backend.herokuapp.com/topics')
+        .then ((res) => {
+            console.log('Here  is the res: ', res)
+            resArray = res.data;
+
+            res.data.message.forEach((url) => {
+            entrypoint.append(placeholder(url));
+        });
+    })
+        .catch((err) => (
+            console.log('Here is the err: ', err)
+        ));
+
+// function Header(date,title,temp) {
+//     let header0 = document.createElement('div');
+//     let headerDate =  document.createElement('span');
+//     let headerTitle = document.createElement('h1');
+//     let headerTemp = document.createElement('span');
+    
+//      header0.classList.add('header');
+//      headerDate.classList.add('date');
+//      headerTemp.classList.add('temp');
+
+
+//      headerDate.textContent = date;
+//      headerTitle.textContent =  title;
+//      headerTemp.textContent = temp;
+
+//      header0.appendChild(headerDate);
+//      header0.appendChild(headerTitle);
+//      header0.appendChild(headerTemp);
+
+//      return header0;
+// };
+// let headerContainer = document.querySelector('.header-container');
+
+// headerContainer.appendChild(Header("MARCH 28, 2019","Lambda Times","98°"));
