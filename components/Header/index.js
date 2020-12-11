c9af82a854af223270d3441abb4013a883102bab
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2020-12-11 12:13:45 -1000

    header complete
    grabbed header container, created elements to append to it, assigned classes, created inner text, appended elements and executed function

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,33 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+  
+    const headerContainer = document.querySelector('header-container');
+
+    //create elements
+    const  div  = document.createElement('div');
+    const span1 = document.createElement('span');
+    const hOne  = document.createElement('h1');
+    const span2 = document.createElement('span');
+
+    //add classes
+    div.classList.add('header');
+    span1.classList.add('date');
+    span2.classList.add('temp');
+
+    //create inner text
+    span1.innerText = "MARCH 28, 2019";
+    hOne.innerText  = "Lambda Times";
+    span2.innerText = "98°";
+
+    //append builder
+    headerContainer.appendChild('div');
+    headerContainer.appendChild('span1');
+    headerContainer.appendChild('hOne');
+    headerContainer.appendChild('span2');
+
+}
+
+Header();
+
