aa264b388148c6cf9243f16e78614d396b9e44af
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2020-12-11 12:33:58 -1000

    wrong branch
    fixed

--- components/Header/index.js
+++ components/Header/index.js
@@ -12,32 +12,4 @@
 // and append it to the DOM inside the div.header-container
 
 function Header() {
-  
-    const headerContainer = document.querySelector('header-container');
-
-    //create elements
-    const  div  = document.createElement('div');
-    const span1 = document.createElement('span');
-    const hOne  = document.createElement('h1');
-    const span2 = document.createElement('span');
-
-    //add classes
-    div.classList.add('header');
-    span1.classList.add('date');
-    span2.classList.add('temp');
-
-    //create inner text
-    span1.innerText = "MARCH 28, 2019";
-    hOne.innerText  = "Lambda Times";
-    span2.innerText = "98°";
-
-    //append builder
-    headerContainer.appendChild('div');
-    headerContainer.appendChild('span1');
-    headerContainer.appendChild('hOne');
-    headerContainer.appendChild('span2');
-
-}
-
-Header();
 
