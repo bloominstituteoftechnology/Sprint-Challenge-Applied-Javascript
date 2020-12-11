178a3d250d6e0c194b4c19dab28e6c68129ad786
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2020-12-11 13:08:59 -1000

    mvp complete
    FTW

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,61 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+// </div>
+//
+// Use your function to create a card for each of the articles and add the card to the DOM.
+
+
+function cardMaker(data) 
+{
+    //create elements
+    const cardDiv     = document.createElement('div');
+    const headlineDiv = document.createElement('div');
+    const authorDiv   = document.createElement('div');
+    const imgDiv      = document.createElement('div');
+    const makeImage   = document.createElement('img');
+    const makeSpan    = document.createElement('span');
+
+    //add classes
+    cardDiv.classList.add('card');
+    headlineDiv.classList.add('headline');
+    authorDiv.classList.add('author');
+    imgDiv.classList.add('img-container');
+
+    //add text
+    headlineDiv.textContent = data.headline;
+    imgDiv.textContent      = data.authorPhoto;
+    makeSpan.textContent    = data.authorName;
+
+    //add elements to the page
+    //div stacking build
+    cardDiv.appendChild(headlineDiv); 
+    headlineDiv.appendChild(authorDiv);
+            headlineDiv.appendChild(makeSpan); //nested content element
+    authorDiv.appendChild(imgDiv);
+            imgDiv.appendChild(makeImage);     // nested content element
+
+    
+    return cardDiv;
+
+}
+
+//grab card container 
+const getCardsContainer = document.querySelector('.cards-container');
+
+axios.get("https://lambda-times-backend.herokuapp.com/articles")
+.then((resolve) => {
+    const articles = resolve.data.articles;
+    const entries = Object.entries(articles);
+
+    for(const key of entries) {
+        const data = key[1]
+        data.forEach(item => {
+            cardsContainer.appendChild(createCard(item));
+        })
+    }
+})
+
+.catch(error => {
+    log("DOES NOT COMPUTE!" error);
+});
