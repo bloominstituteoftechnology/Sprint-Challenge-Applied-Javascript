8e25597df65b1188f053d2f802f420bb2349d9d1
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2020-12-11 12:41:04 -1000

    complete tabs section for mvp
    iterated tabsetup section, completed axios get .then .catch to handle the api request.

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,34 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+
+function tabSetup(data) 
+{
+    //create tab, add class list, add text content
+    const tab = document.createElement('div');
+    const topics = document.querySelector('topics');
+    tab.classList.add('tab');
+    tab.textContent = data;
+
+
+    return tab;
+}
+
+//grab topics
+const grabTopics = document.querySelector('.topics');
+
+
+//axios get request
+axios.get("https://lambda-times-backend.herokuapp.com/topics")
+.then(resolve => {
+
+    resolve.data.topics.array.forEach(element => {     
+        const createTopic = topics(element);
+        grabTopics.appendChild(createTopic);            
+    });        
+    
+})
+
+.catch(error => {
+    log("DOES NOT COMPUTE!");
+});
