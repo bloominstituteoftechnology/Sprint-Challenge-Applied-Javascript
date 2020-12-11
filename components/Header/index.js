eedd2b174eb42ce1d45edf91125d49df75133298
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2020-12-11 12:35:35 -1000

    finished header
    added all elements, created classes using class list, added text using innerText, and appended all children in the required function

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,3 +1,4 @@
+log = console.log;
 // STEP 1: Create a Header component.
 // -----------------------
 // Write a function that returns the markup you see below:
@@ -11,4 +12,46 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+
+// STEP 1: Create a Header component.
+// -----------------------
+// Write a function that returns the markup you see below:
+//
+//  <div class="header">
+//    <span class="date">SMARCH 28, 2019</span>
+//    <h1>Lambda Times</h1>
+//    <span class="temp">98°</span>
+//  </div>
+//
+// Use your function to create a header
+// and append it to the DOM inside the div.header-container
+
+function Header() {
+  
+    const headerContainer = document.querySelector('header-container');
+
+    //create elements
+    const  div  = document.createElement('div');
+    const span1 = document.createElement('span');
+    const hOne  = document.createElement('h1');
+    const span2 = document.createElement('span');
+
+    //add classes
+    div.classList.add('header');
+    span1.classList.add('date');
+    span2.classList.add('temp');
+
+    //create inner text
+    span1.innerText = "MARCH 28, 2019";
+    hOne.innerText  = "Lambda Times";
+    span2.innerText = "98°";
+
+    //append builder
+    headerContainer.appendChild('div');
+    headerContainer.appendChild('span1');
+    headerContainer.appendChild('hOne');
+    headerContainer.appendChild('span2');
+
+}
+
+Header();
