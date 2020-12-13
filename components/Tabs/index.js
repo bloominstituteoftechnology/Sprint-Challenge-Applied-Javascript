efcc461393efdcc1157469675972d6de055c0e92
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2020-12-12 16:36:29 -1000

    debug

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -2,9 +2,9 @@ log = console.log;
 //  STEP 2: Create tabs
 // -----------------------
 // Using axios send a GET request to the address: https://lambda-times-api.herokuapp.com/topics
-function getHerokuData() {
-  let herokuPromise = axios.get("https://lambda-times-api.herokuapp.com/topics");
-  return herokuPromise;
+function getData() {
+  let heroku = axios.get("https://lambda-times-api.herokuapp.com/topics");
+  return heroku;
 }
 // Once the data is resolved use console logs or breakpoints to review the structure.
 // console.log(getHerokuData()); // review the structure.  Target is dataObject.topics
@@ -16,11 +16,11 @@ function createNewTab(topicString) {
   tab.textContent = topicString;
   const topicsContainer = document.querySelector(".topics");
   topicsContainer.appendChild(tab);
-  log(topicsContainer);
+  //log(topicsContainer);
   return tab;
 };  
 
-let promise = getHerokuData();
+let promise = getData();
 promise.then(({data: {topics: topics}}) => {
   topics.forEach(element => {
     createNewTab(element);
