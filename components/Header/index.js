cee4aaf67761467c5e67a189b735d37e37dc2d51
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2020-12-12 16:58:01 -1000

    debug

--- components/Header/index.js
+++ components/Header/index.js
@@ -15,35 +15,37 @@ log = console.log;
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {
      
     const headContainer = document.querySelector(".header-container");
+     headContainer.appendChild(Header());
     //console.log(headCountainer);
     
+function Header() {
     //create elements
     const  div  = document.createElement('div');
     const span1 = document.createElement('span');
     const hOne  = document.createElement('h1');
     const span2 = document.createElement('span');
     
+    div.append(span1, hOne, span2);
+     
     //add classes
     div.classList.add('header');
     span1.classList.add('date');
     span2.classList.add('temp');
 
       //create inner text
-      span1.innerText = "MARCH 28, 2019";
-      hOne.innerText  = "Lambda Times";
-      span2.innerText = "98°";
+      span1.textContent = "MARCH 28, 2019";
+      hOne.textContent  = "Lambda Times";
+      span2.textContent = "98°";
 
         //append builder
-        headContainer.appendChild('div');
         div.appendChild('span1');
         div.appendChild('hOne');
         div.appendChild('span2');
      
-  return headContainer;
+  return div;
 }
 
-Header();
+
 
