c8327cbddd5deab67f68a5ce856532849fe4f2ec
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2020-12-12 16:22:38 -1000

    debug

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -1,30 +1,41 @@
 log = console.log;
-// STEP 2: Create tabs
+//  STEP 2: Create tabs
 // -----------------------
-// Using axios send a GET request to the address: https://lambda-times-backend.herokuapp.com/topics
+// Using axios send a GET request to the address: https://lambda-times-api.herokuapp.com/topics
+function getHerokuData() {
+  let herokuPromise = axios.get(‘https://lambda-times-api.herokuapp.com/topics’);
+  return herokuPromise;
+}
 // Once the data is resolved use console logs or breakpoints to review the structure.
+// console.log(getHerokuData()); // review the structure.  Target is dataObject.topics
 // Iterate over the topics creating a new tab for each topic, and appending it to the DOM
-// under the div.topics element.
-//
-//  Each tab should look like this:
-//    <div class="tab">topic here</div>
-
-axios.get("https://lambda-times-backend.herokuapp.com/topics")
-.then(res => {
-    console.log(res.data.topics)
-    const responseArray = res.data.topics
-    responseArray.forEach(element => {
-        const newTab = createTab(element)
-        topicsContainer.appendChild(newTab)
-    });
-})
-const topicsContainer = document.querySelector('.topics');
-function createTab(topic){
-    const tab = document.createElement("div")
-    tab.textContent = topic
-    tab.classList.add("tab")
-
-
-return tab
-} 
-
+function createNewTab(topicString) {
+  // create element
+  let tab = document.createElement(‘div’);
+  tab.classList.add(‘tab’);
+  tab.textContent = topicString;
+  let topicsContainer = document.querySelector(‘.topics’);
+  topicsContainer.appendChild(tab);
+  // console.log(“Appended Topics Container”, topicsContainer);
+  return tab;
+};  // this func creates tabs
+let promise = getHerokuData();
+promise.then(({data: {topics: topics}}) => { // object destructuring   {objectProperty: assignedToVar}
+  // console.log(“Topics the array variable: “, topics);
+  topics.forEach(e => {
+    createNewTab(e);
+  });
+}).catch((topics) => {
+  console.log(“Promise call unsuccessful.“);
+});  // end of the promise code
+// promise.then(({data: user}) => { // object destructuring   {objectProperty: assignedToVar}
+//   console.log(“User: “, user);
+//   let cardContainer =  document.querySelector(‘.cards’);
+//   cardContainer.appendChild(cardMaker(user)); // the append must go INSIDE the callback
+// }).catch((user) => {
+//   console.log(“Promise call unsuccessful.“)
+// });
+// test code, it works
+// createNewTab(“testtab”);
+// let topicsContainer = document.querySelector(‘.topics’);
+// console.log(“appended container”, topicsContainer)
